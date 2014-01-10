bc718fd3e251a74ee9afa24269ebb6e3ba6e6727
gardners <paul@servalproject.org>
2014-01-10 16:35:56 +1030

    update test container so that simulation of CPU+video controller works.

--- cpu_test.vhdl
+++ cpu_test.vhdl
@@ -9,6 +9,7 @@ end cpu_test;
 
 architecture behavior of cpu_test is
   signal clock : std_logic := '0';
+  signal cpuclock : std_logic := '0';
   signal reset : std_logic := '0';
   signal irq : std_logic := '1';
   signal nmi : std_logic := '1';
@@ -19,6 +20,65 @@ architecture behavior of cpu_test is
   signal monitor_y : std_logic_vector(7 downto 0);
   signal monitor_sp : std_logic_vector(7 downto 0);
   signal monitor_p : std_logic_vector(7 downto 0);
+
+  signal vsync : std_logic;
+  signal hsync : std_logic;
+  signal vgared : unsigned(3 downto 0);
+  signal vgagreen : unsigned(3 downto 0);
+  signal vgablue : unsigned(3 downto 0);
+
+  signal led0 : std_logic;
+  signal led1 : std_logic;
+  signal led2 : std_logic;
+  signal led3 : std_logic;
+  signal sw : std_logic_vector(15 downto 0) := (others => '0');
+  signal btn : std_logic_vector(4 downto 0) := (others => '0');
+  
+  
+  component vga is
+    Port (
+      ----------------------------------------------------------------------
+      -- 100MHz Nexys4 master clock from which we drive the dotclock
+      ----------------------------------------------------------------------
+      pixelclock : in  STD_LOGIC;
+      cpuclock : in std_logic;
+
+      ----------------------------------------------------------------------
+      -- VGA output
+      ----------------------------------------------------------------------
+      vsync : out  STD_LOGIC;
+      hsync : out  STD_LOGIC;
+      vgared : out  UNSIGNED (3 downto 0);
+      vgagreen : out  UNSIGNED (3 downto 0);
+      vgablue : out  UNSIGNED (3 downto 0);
+
+      -----------------------------------------------------------------------------
+      -- Interface to 128KB fastram
+      -----------------------------------------------------------------------------
+      ramaddress : OUT STD_LOGIC_VECTOR(13 DOWNTO 0);
+      ramdata : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
+      
+      -----------------------------------------------------------------------------
+      -- FastIO interface for accessing video registers
+      -----------------------------------------------------------------------------
+      fastio_addr : in std_logic_vector(19 downto 0);
+      fastio_read : in std_logic;
+      fastio_write : in std_logic;
+      fastio_wdata : in std_logic_vector(7 downto 0);
+      fastio_rdata : out std_logic_vector(7 downto 0);
+
+      ----------------------------------------------------------------------
+      -- Debug interfaces on Nexys4 board
+      ----------------------------------------------------------------------
+      led0 : out std_logic;
+      led1 : out std_logic;
+      led2 : out std_logic;
+      led3 : out std_logic;
+      sw : in std_logic_vector(15 downto 0);
+      btn : in std_logic_vector(4 downto 0)
+
+      );
+  end component;
   
   component cpu6502
     port (   clock : in STD_LOGIC;
@@ -38,8 +98,29 @@ architecture behavior of cpu_test is
              fastio_read : out std_logic;
              fastio_write : out std_logic;
              fastio_wdata : out std_logic_vector(7 downto 0);
-             fastio_rdata : in std_logic_vector(7 downto 0)
+             fastio_rdata : in std_logic_vector(7 downto 0);
+
+             -- fastram port
+             fastram_we : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+             fastram_address : OUT STD_LOGIC_VECTOR(13 DOWNTO 0);
+             fastram_datain : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);
+             fastram_dataout : IN STD_LOGIC_VECTOR(63 DOWNTO 0)
              );      
+  end component;
+
+ component ram64x16k
+    PORT (
+      clka : IN STD_LOGIC;
+      wea : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+      addra : IN STD_LOGIC_VECTOR(13 DOWNTO 0);
+      dina : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
+      douta : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);
+      clkb : IN STD_LOGIC;
+      web : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+      addrb : IN STD_LOGIC_VECTOR(13 DOWNTO 0);
+      dinb : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
+      doutb : OUT STD_LOGIC_VECTOR(63 DOWNTO 0)
+      );
   end component;
   
   component iomapper is
@@ -57,8 +138,57 @@ architecture behavior of cpu_test is
   signal fastio_write : std_logic;
   signal fastio_wdata : std_logic_vector(7 downto 0);
   signal fastio_rdata : std_logic_vector(7 downto 0);
-  
+
+  signal fastram_we : std_logic_vector(7 downto 0);
+  signal fastram_address : std_logic_vector(13 downto 0);
+  signal fastram_dataout : std_logic_vector(63 downto 0);
+  signal fastram_datain : std_logic_vector(63 downto 0);
+
+  signal vga_fastramaddress : std_logic_vector(13 downto 0);
+  signal vga_fastramdata : std_logic_vector(63 downto 0);
+
 begin
+  fastram1 : component ram64x16k
+    PORT MAP (
+      clka => clock,
+      wea => fastram_we,
+      addra => fastram_address,
+      dina => fastram_datain,
+      douta => fastram_dataout,
+      clkb => '0',
+      web => (others => '0'),
+      addrb => vga_fastramaddress,
+      dinb => (others => '0'),
+      doutb => vga_fastramdata
+      );
+
+  vga0: vga
+    port map (
+      pixelclock      => clock,
+      cpuclock        => cpuclock,
+      
+      vsync           => vsync,
+      hsync           => hsync,
+      vgared          => vgared,
+      vgagreen        => vgagreen,
+      vgablue         => vgablue,
+      
+      ramaddress      => vga_fastramaddress,
+      ramdata         => vga_fastramdata,
+      
+      fastio_addr     => fastio_addr,
+      fastio_read     => fastio_read,
+      fastio_write    => fastio_write,
+      fastio_wdata    => fastio_wdata,
+      fastio_rdata    => fastio_rdata,
+      
+      led0            => led0,
+      led1            => led1,
+      led2            => led2,
+      led3            => led3,
+      sw              => sw,
+      btn             => btn);
+  
   cpu0: cpu6502 port map(clock => clock,reset =>reset,irq => irq,
                          nmi => nmi,monitor_pc => monitor_pc,
                          monitor_opcode => monitor_opcode,
@@ -71,7 +201,11 @@ begin
                          fastio_read => fastio_read,
                          fastio_write => fastio_write,
                          fastio_wdata => fastio_wdata,
-                         fastio_rdata => fastio_rdata);
+                         fastio_rdata => fastio_rdata,
+                         fastram_we => fastram_we,
+                         fastram_address => fastram_address,
+                         fastram_dataout => fastram_dataout,
+                         fastram_datain => fastram_datain);
   iomapper0: iomapper port map (
     clk => clock, address => fastio_addr, r => fastio_read, w => fastio_write,
     data_i => fastio_wdata, data_o => fastio_rdata);
@@ -98,11 +232,12 @@ begin
     report "reset released" severity note;
     for i in 1 to 100 loop
       clock <= '1';
+      cpuclock <= not cpuclock;
       wait for 10 ns;     
       -- report "clock=1" severity note;
       clock <= '0';
       -- report "clock=0 (run)" severity note;
-      wait for 10 ns;      
+      wait for 10 ns;
     end loop;  -- i
     assert false report "End of simulation" severity failure;
   end process;
