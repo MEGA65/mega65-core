233cc0ef490d164cd92dc707114b3ae31ad318b1
gardners <paul@servalproject.org>
2014-01-13 15:53:31 +1030

    fix uart TX timer calculation.

--- UART_TX_CTRL.vhd
+++ UART_TX_CTRL.vhd
@@ -52,8 +52,8 @@ architecture Behavioral of UART_TX_CTRL is
 type TX_STATE_TYPE is (RDY, LOAD_BIT, SEND_BIT);
 
 -- Input clock = 192MHz, baudrate = 230400
--- 192MHz/230400 -1 = 832
-constant BIT_TMR_MAX : std_logic_vector(13 downto 0) := "00011010000000";
+-- 192MHz/2/230400 -1 = 416
+constant BIT_TMR_MAX : std_logic_vector(13 downto 0) := "00001101000000";
 constant BIT_INDEX_MAX : natural := 10;
 
 --Counter that keeps track of the number of clock cycles the current bit has been held stable over the
