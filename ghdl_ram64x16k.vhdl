968e3cc3e54238a350141f5d9329ac051bf5d3f3
gardners <paul@servalproject.org>
2014-01-18 11:58:44 +1030

    work work on making fastram simulable.

--- ghdl_ram64x16k.vhdl
+++ ghdl_ram64x16k.vhdl
@@ -23,40 +23,35 @@ END ram64x16k;
 
 architecture behavioural of ram64x16k is
 
-  type ram_t is array (0 to 65535) of std_logic_vector(63 downto 0);
-  signal ram : ram_t;
+  type ram_t is array (0 to 65535) of std_logic_vector(7 downto 0);
+  signal ram(0 to 7) : ram_t  := (others => x"bd");
 
 begin  -- behavioural
 
   process(clka)
+    variable therame : ram_t;
+    variable thevalue : std_logic_vector(63 downto 0) := dina;
   begin
     if(rising_edge(Clka)) then 
       if true then
-        if wea(0)='1' then
-          ram(to_integer(unsigned(addra)))(7 downto 0) <= dina(7 downto 0);
-        end if;
-        if wea(1)='1' then
-          ram(to_integer(unsigned(addra)))(15 downto 8) <= dina(15 downto 8);
-        end if;
-        if wea(2)='1' then
-          ram(to_integer(unsigned(addra)))(23 downto 16) <= dina(23 downto 16);
-        end if;
-        if wea(3)='1' then
-          ram(to_integer(unsigned(addra)))(31 downto 24) <= dina(31 downto 24);
-        end if;
-        if wea(4)='1' then
-          ram(to_integer(unsigned(addra)))(39 downto 32) <= dina(39 downto 32);
-        end if;
-        if wea(5)='1' then
-          ram(to_integer(unsigned(addra)))(47 downto 40) <= dina(47 downto 40);
-        end if;
-        if wea(6)='1' then
-          ram(to_integer(unsigned(addra)))(55 downto 48) <= dina(55 downto 48);
-        end if;
-        if wea(7)='1' then
-          ram(to_integer(unsigned(addra)))(63 downto 56) <= dina(63 downto 56);
-        end if;
+        for i in 0 to 7 loop
+          if wea(i)='1' then
+            report "writing $" & to_hstring(thevalue(7 downto 0))
+              & " to $" & to_hstring("000"&addra&std_logic_vector(to_unsigned(i,3))) severity note;
+            theram:=ram(i);
+            theram(to_integer(unsigned(addra))) <= thevalue(7 downto 0);
+          end if;
+          thevalue(55 downto 0) := thevalue(63 downto 8);
+        end loop;  -- i
+
+        for i in 0 to 7 loop
+          
+        end loop;  -- i
         douta <= ram(to_integer(unsigned(addra)));
+        report "reading fastram at $" & to_hstring("000"&addra&"111")
+          & ", contains $" & to_hstring(ram(to_integer(unsigned(addra))))
+          severity note;
+      
       else
         douta <= (others => 'Z');
       end if;
@@ -68,27 +63,43 @@ begin  -- behavioural
     if(rising_edge(Clkb)) then 
       if true then
         if web(0)='1' then
+          report "writing $" & to_hstring(dinb(7 downto 0))
+            & " to $" & to_hstring("000"&addrb&"000") severity note;
           ram(to_integer(unsigned(addrb)))(7 downto 0) <= dinb(7 downto 0);
         end if;
         if web(1)='1' then
+          report "writing $" & to_hstring(dinb(15 downto 8))
+            & " to $" & to_hstring("000"&addrb&"001") severity note;
           ram(to_integer(unsigned(addrb)))(15 downto 8) <= dinb(15 downto 8);
         end if;
         if web(2)='1' then
+          report "writing $" & to_hstring(dinb(23 downto 16))
+            & " to $" & to_hstring("000"&addrb&"010") severity note;
           ram(to_integer(unsigned(addrb)))(23 downto 16) <= dinb(23 downto 16);
         end if;
         if web(3)='1' then
+          report "writing $" & to_hstring(dinb(31 downto 24))
+            & " to $" & to_hstring("000"&addrb&"011") severity note;
           ram(to_integer(unsigned(addrb)))(31 downto 24) <= dinb(31 downto 24);
         end if;
         if web(4)='1' then
+          report "writing $" & to_hstring(dinb(39 downto 32))
+            & " to $" & to_hstring("000"&addrb&"100") severity note;
           ram(to_integer(unsigned(addrb)))(39 downto 32) <= dinb(39 downto 32);
         end if;
         if web(5)='1' then
+          report "writing $" & to_hstring(dinb(47 downto 40))
+            & " to $" & to_hstring("000"&addrb&"101") severity note;
           ram(to_integer(unsigned(addrb)))(47 downto 40) <= dinb(47 downto 40);
         end if;
         if web(6)='1' then
+          report "writing $" & to_hstring(dinb(55 downto 48))
+            & " to $" & to_hstring("000"&addrb&"110") severity note;
           ram(to_integer(unsigned(addrb)))(55 downto 48) <= dinb(55 downto 48);
         end if;
         if web(7)='1' then
+          report "writing $" & to_hstring(dinb(63 downto 56))
+            & " to $" & to_hstring("000"&addrb&"111") severity note;
           ram(to_integer(unsigned(addrb)))(63 downto 56) <= dinb(63 downto 56);
         end if;
         doutb <= ram(to_integer(unsigned(addrb)));
