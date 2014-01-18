06ce9eb64a4fa8ede1d75994b9a31d7ef3a619f9
gardners <paul@servalproject.org>
2014-01-18 11:26:39 +1030

    work towards making fastram simulatable.

--- ghdl_ram64x16k.vhdl
+++ ghdl_ram64x16k.vhdl
@@ -32,8 +32,29 @@ begin  -- behavioural
   begin
     if(rising_edge(Clka)) then 
       if true then
-        if(wea="1") then
-          ram(to_integer(unsigned(addra))) <= dina;
+        if wea(0)='1' then
+          ram(to_integer(unsigned(addra)))(7 downto 0) <= dina(7 downto 0);
+        end if;
+        if wea(1)='1' then
+          ram(to_integer(unsigned(addra)))(15 downto 8) <= dina(15 downto 8);
+        end if;
+        if wea(2)='1' then
+          ram(to_integer(unsigned(addra)))(23 downto 16) <= dina(23 downto 16);
+        end if;
+        if wea(3)='1' then
+          ram(to_integer(unsigned(addra)))(31 downto 24) <= dina(31 downto 24);
+        end if;
+        if wea(4)='1' then
+          ram(to_integer(unsigned(addra)))(39 downto 32) <= dina(39 downto 32);
+        end if;
+        if wea(5)='1' then
+          ram(to_integer(unsigned(addra)))(47 downto 40) <= dina(47 downto 40);
+        end if;
+        if wea(6)='1' then
+          ram(to_integer(unsigned(addra)))(55 downto 48) <= dina(55 downto 48);
+        end if;
+        if wea(7)='1' then
+          ram(to_integer(unsigned(addra)))(63 downto 56) <= dina(63 downto 56);
         end if;
         douta <= ram(to_integer(unsigned(addra)));
       else
@@ -46,8 +67,29 @@ begin  -- behavioural
   begin
     if(rising_edge(Clkb)) then 
       if true then
-        if(web="1") then
-          ram(to_integer(unsigned(addrb))) <= dinb;
+        if web(0)='1' then
+          ram(to_integer(unsigned(addrb)))(7 downto 0) <= dinb(7 downto 0);
+        end if;
+        if web(1)='1' then
+          ram(to_integer(unsigned(addrb)))(15 downto 8) <= dinb(15 downto 8);
+        end if;
+        if web(2)='1' then
+          ram(to_integer(unsigned(addrb)))(23 downto 16) <= dinb(23 downto 16);
+        end if;
+        if web(3)='1' then
+          ram(to_integer(unsigned(addrb)))(31 downto 24) <= dinb(31 downto 24);
+        end if;
+        if web(4)='1' then
+          ram(to_integer(unsigned(addrb)))(39 downto 32) <= dinb(39 downto 32);
+        end if;
+        if web(5)='1' then
+          ram(to_integer(unsigned(addrb)))(47 downto 40) <= dinb(47 downto 40);
+        end if;
+        if web(6)='1' then
+          ram(to_integer(unsigned(addrb)))(55 downto 48) <= dinb(55 downto 48);
+        end if;
+        if web(7)='1' then
+          ram(to_integer(unsigned(addrb)))(63 downto 56) <= dinb(63 downto 56);
         end if;
         doutb <= ram(to_integer(unsigned(addrb)));
       else
