8a8b3ca1195440e9ca1fc0b7db3d4bc518dc0559
gardners <paul@servalproject.org>
2014-01-21 11:56:04 +1030

    fix various bugs in CIA implementation that prevented effective reading and writing of registers.

--- cia6526.vhdl
+++ cia6526.vhdl
@@ -121,15 +121,19 @@ architecture behavioural of cia6526 is
 
 begin  -- behavioural
   
-  process(cpuclock,fastio_addr,flagin,cs) is
+  process(cpuclock,fastio_addr,flagin,cs,portain,portbin) is
     -- purpose: use DDR to show either input or output bits
-  function ddr_pick (
-    ddr                            : in std_logic_vector(7 downto 0);
-    i                              : in std_logic_vector(7 downto 0);
-    o                              : in std_logic_vector(7 downto 0))
+    function ddr_pick (
+      ddr                            : in std_logic_vector(7 downto 0);
+      i                              : in std_logic_vector(7 downto 0);
+      o                              : in std_logic_vector(7 downto 0))
     return unsigned is
     variable result : unsigned(7 downto 0);
   begin  -- ddr_pick
+    report "determining read value for CIA port." &
+      "  DDR=$" & to_hstring(ddr) &
+      ", out_value=$" & to_hstring(o) &
+      ", in_value=$" & to_hstring(i) severity note;
     for b in 0 to 7 loop
       if ddr(b)='1' then
         -- XXX implement external pull down of output bits?
@@ -141,11 +145,71 @@ begin  -- behavioural
     return result;
   end ddr_pick;
 
-    variable register_number : unsigned(3 downto 0) := fastio_addr(3 downto 0);
+  variable register_number : unsigned(3 downto 0);
   begin
+    register_number := fastio_addr(3 downto 0);
     if cs='0' then
+      -- Tri-state read lines if not selected
       fastio_rdata <= (others => 'Z');
     else
+      -- Reading of registers
+      if fastio_write='1' then
+        -- Tri-state read lines if writing
+        fastio_rdata <= (others => 'Z');
+      else
+        report "reading from CIA register $" & to_hstring(register_number) severity note;
+        case register_number is
+          when x"0" => fastio_rdata <= ddr_pick(reg_porta_ddr,portain,reg_porta_out);
+          when x"1" => fastio_rdata <= ddr_pick(reg_portb_ddr,portbin,reg_portb_out);
+          when x"2" => fastio_rdata <= unsigned(reg_porta_ddr);
+          when x"3" => fastio_rdata <= unsigned(reg_portb_ddr);
+          when x"4" => fastio_rdata <= reg_timera(7 downto 0);
+          when x"5" => fastio_rdata <= reg_timera(15 downto 8);
+          when x"6" => fastio_rdata <= reg_timerb(7 downto 0);
+          when x"7" => fastio_rdata <= reg_timerb(15 downto 8);
+          when x"8" =>
+            if read_tod_latched='1' then
+              fastio_rdata <= read_tod_dsecs;
+            else
+              fastio_rdata <= reg_tod_dsecs;
+            end if;
+          when x"9" =>   
+            if read_tod_latched='1' then
+              fastio_rdata <= read_tod_secs;
+            else
+              fastio_rdata <= reg_tod_secs;
+            end if;
+          when x"a" =>   
+            if read_tod_latched='1' then
+              fastio_rdata <= read_tod_mins;
+            else
+              fastio_rdata <= reg_tod_mins;
+            end if;
+          when x"b" =>   
+            fastio_rdata <= reg_tod_ampm & reg_tod_hours;
+          when x"c" => fastio_rdata <= unsigned(reg_read_sdr);
+          when x"d" => fastio_rdata <= reg_isr;
+          when x"e" => 
+            fastio_rdata <= reg_60hz
+                            & reg_serialport_direction
+                            & reg_timera_pulse_source
+                            & '0'
+                            & reg_timera_oneshot
+                            & reg_timera_toggle_or_pulse
+                            & reg_timera_pb6_out
+                            & reg_timera_start;            
+          when x"f" =>
+            fastio_rdata <= unsigned(reg_tod_alarm_edit
+                                     & reg_timerb_tick_source
+                                     & '0'  -- strobe always reads as 0
+                                     & reg_timerb_oneshot
+                                     & reg_timerb_toggle_or_pulse
+                                     & reg_timerb_pb7_out
+                                     & reg_timerb_start);
+          when others => fastio_rdata <= (others => 'Z');
+        end case;
+      end if;
+      
       if rising_edge(cpuclock) then
 
         -- Check for negative edge on FLAG
@@ -166,6 +230,8 @@ begin  -- behavioural
 
         -- Check for register read side effects
         if fastio_write='0' then
+          report "Performing side-effects of reading from CIA register $" & to_hstring(register_number) severity note;
+
           case register_number is
             when x"1" =>
               -- Reading or writing port B strobes PC high for 1 cycle
@@ -182,154 +248,99 @@ begin  -- behavioural
               reg_isr<= x"00"; irq<='1';
             when others => null;
           end case;
-
-          -- Check for register writing
-          if fastio_write='1' then
-            case register_number is
-              when x"0" => portaout<=std_logic_vector(fastio_wdata);
-                           reg_porta_out<=std_logic_vector(fastio_wdata);
-              when x"1" =>
-                portbout<=std_logic_vector(fastio_wdata);
-                reg_portb_out<=std_logic_vector(fastio_wdata);
-                pcout<='1';
-                strobe_pc<='1';
-              when x"2" => reg_porta_ddr<=std_logic_vector(fastio_wdata);
-              when x"3" => reg_portb_ddr<=std_logic_vector(fastio_wdata);
-              when x"4" => reg_timera_latch(7 downto 0) <= fastio_wdata;
-              when x"5" => reg_timera_latch(15 downto 8) <= fastio_wdata;
-              when x"6" => reg_timerb_latch(7 downto 0) <= fastio_wdata;
-              when x"7" => reg_timerb_latch(15 downto 8) <= fastio_wdata;
-              when x"8" =>
-                if reg_tod_alarm_edit ='0' then
-                  reg_tod_dsecs <= fastio_wdata; tod_running<='1';
-                else
-                  reg_alarm_dsecs <= fastio_wdata;
-                end if;
-              when x"9" => 
-                if reg_tod_alarm_edit ='0' then
-                  reg_tod_secs <= fastio_wdata;
-                else
-                  reg_alarm_secs <= fastio_wdata;
-                end if;
-              when x"a" => 
-                if reg_tod_alarm_edit ='0' then
-                  reg_tod_mins <= fastio_wdata;
-                else
-                  reg_alarm_mins <= fastio_wdata;
-                end if;
-              when x"b" => 
-                if reg_tod_alarm_edit ='0' then
-                  tod_running <= '0';
-                  reg_tod_hours <= fastio_wdata(6 downto 0);
-                  reg_tod_ampm <= fastio_wdata(7);
-                else
-                  reg_alarm_hours <= fastio_wdata(6 downto 0);
-                  reg_alarm_ampm <= fastio_wdata(7);
-                end if;
-              when x"c" =>
-                reg_sdr_data <= std_logic_vector(fastio_wdata);
-                sdr_loaded <= '1';
-              when x"d" =>
-                if fastio_wdata(7)='1' then
-                  -- Set interrupt mask bits
-                  imask_flag <= imask_flag or fastio_wdata(4);
-                  imask_serialport <= imask_flag or fastio_wdata(3);
-                  imask_alarm <= imask_flag or fastio_wdata(2);
-                  imask_tb <= imask_flag or fastio_wdata(1);
-                  imask_ta <= imask_flag or fastio_wdata(0);
-                else
-                  -- Clear interrupt mask bits if a bit is 1.
-                  imask_flag <= imask_flag and (not fastio_wdata(4));
-                  imask_serialport <= imask_flag and (not fastio_wdata(3));
-                  imask_alarm <= imask_flag and (not fastio_wdata(2));
-                  imask_tb <= imask_flag and (not fastio_wdata(1));
-                  imask_ta <= imask_flag and (not fastio_wdata(0));                 
-                end if;
-              when x"e" =>
-                reg_60hz <= fastio_wdata(7);
-                reg_serialport_direction <= fastio_wdata(6);
-                reg_timera_pulse_source <= fastio_wdata(5);
-                if fastio_wdata(4)='1' then
-                  -- Force loading of timer A now from latch
-                  reg_timera <= reg_timera_latch;
-                end if;
-                reg_timera_oneshot <= fastio_wdata(3);
-                reg_timera_toggle_or_pulse <= fastio_wdata(2);
-                reg_timera_pb6_out <= fastio_wdata(1);
-                reg_timera_start <= fastio_wdata(0);
-              when x"f" =>
-                reg_tod_alarm_edit <= std_logic(fastio_wdata(7));
-                reg_timerb_tick_source <= std_logic_vector(fastio_wdata(6 downto 5));
-                if fastio_wdata(4)='1' then
-                  -- Force loading of timer A now from latch
-                  reg_timerb <= reg_timerb_latch;
-                end if;
-                reg_timerb_oneshot <= std_logic(fastio_wdata(3));
-                reg_timerb_toggle_or_pulse <= std_logic(fastio_wdata(2));
-                reg_timerb_pb7_out <= std_logic(fastio_wdata(1));
-                reg_timerb_start <= std_logic(fastio_wdata(0));                  
-              when others => null;
-            end case;
-          end if;
         end if;
+        
+        -- Check for register writing
+        if fastio_write='1' then
+          report "writing $" & to_hstring(fastio_wdata)
+            & " to CIA register $" & to_hstring(register_number) severity note;
 
-        -- Reading of registers
-        if fastio_write='0' then
-          fastio_rdata <= (others => 'Z');
-        else
           case register_number is
-            when x"0" => fastio_rdata <= ddr_pick(reg_porta_ddr,portain,reg_porta_out);
-            when x"1" => fastio_rdata <= ddr_pick(reg_portb_ddr,portbin,reg_portb_out);
-            when x"2" => fastio_rdata <= unsigned(reg_porta_ddr);
-            when x"3" => fastio_rdata <= unsigned(reg_portb_ddr);
-            when x"4" => fastio_rdata <= reg_timera(7 downto 0);
-            when x"5" => fastio_rdata <= reg_timera(15 downto 8);
-            when x"6" => fastio_rdata <= reg_timerb(7 downto 0);
-            when x"7" => fastio_rdata <= reg_timerb(15 downto 8);
+            when x"0" => portaout<=std_logic_vector(fastio_wdata);
+                         reg_porta_out<=std_logic_vector(fastio_wdata);
+            when x"1" =>
+              portbout<=std_logic_vector(fastio_wdata);
+              reg_portb_out<=std_logic_vector(fastio_wdata);
+            when x"2" => reg_porta_ddr<=std_logic_vector(fastio_wdata);
+            when x"3" => reg_portb_ddr<=std_logic_vector(fastio_wdata);
+            when x"4" => reg_timera_latch(7 downto 0) <= fastio_wdata;
+            when x"5" => reg_timera_latch(15 downto 8) <= fastio_wdata;
+            when x"6" => reg_timerb_latch(7 downto 0) <= fastio_wdata;
+            when x"7" => reg_timerb_latch(15 downto 8) <= fastio_wdata;
             when x"8" =>
-              if read_tod_latched='1' then
-                fastio_rdata <= read_tod_dsecs;
+              if reg_tod_alarm_edit ='0' then
+                reg_tod_dsecs <= fastio_wdata; tod_running<='1';
               else
-                fastio_rdata <= reg_tod_dsecs;
+                reg_alarm_dsecs <= fastio_wdata;
               end if;
-            when x"9" =>   
-              if read_tod_latched='1' then
-                fastio_rdata <= read_tod_secs;
+            when x"9" => 
+              if reg_tod_alarm_edit ='0' then
+                reg_tod_secs <= fastio_wdata;
               else
-                fastio_rdata <= reg_tod_secs;
+                reg_alarm_secs <= fastio_wdata;
               end if;
-            when x"a" =>   
-              if read_tod_latched='1' then
-                fastio_rdata <= read_tod_mins;
+            when x"a" => 
+              if reg_tod_alarm_edit ='0' then
+                reg_tod_mins <= fastio_wdata;
               else
-                fastio_rdata <= reg_tod_mins;
+                reg_alarm_mins <= fastio_wdata;
               end if;
-            when x"b" =>   
-              fastio_rdata <= reg_tod_ampm & reg_tod_hours;
-            when x"c" => fastio_rdata <= unsigned(reg_read_sdr);
-            when x"d" => fastio_rdata <= reg_isr;
-            when x"e" => 
-              fastio_rdata <= reg_60hz
-                              & reg_serialport_direction
-                              & reg_timera_pulse_source
-                              & '0'
-                              & reg_timera_oneshot
-                              & reg_timera_toggle_or_pulse
-                              & reg_timera_pb6_out
-                              & reg_timera_start;            
+            when x"b" => 
+              if reg_tod_alarm_edit ='0' then
+                tod_running <= '0';
+                reg_tod_hours <= fastio_wdata(6 downto 0);
+                reg_tod_ampm <= fastio_wdata(7);
+              else
+                reg_alarm_hours <= fastio_wdata(6 downto 0);
+                reg_alarm_ampm <= fastio_wdata(7);
+              end if;
+            when x"c" =>
+              reg_sdr_data <= std_logic_vector(fastio_wdata);
+              sdr_loaded <= '1';
+            when x"d" =>
+              if fastio_wdata(7)='1' then
+                -- Set interrupt mask bits
+                imask_flag <= imask_flag or fastio_wdata(4);
+                imask_serialport <= imask_flag or fastio_wdata(3);
+                imask_alarm <= imask_flag or fastio_wdata(2);
+                imask_tb <= imask_flag or fastio_wdata(1);
+                imask_ta <= imask_flag or fastio_wdata(0);
+              else
+                -- Clear interrupt mask bits if a bit is 1.
+                imask_flag <= imask_flag and (not fastio_wdata(4));
+                imask_serialport <= imask_flag and (not fastio_wdata(3));
+                imask_alarm <= imask_flag and (not fastio_wdata(2));
+                imask_tb <= imask_flag and (not fastio_wdata(1));
+                imask_ta <= imask_flag and (not fastio_wdata(0));                 
+              end if;
+            when x"e" =>
+              reg_60hz <= fastio_wdata(7);
+              reg_serialport_direction <= fastio_wdata(6);
+              reg_timera_pulse_source <= fastio_wdata(5);
+              if fastio_wdata(4)='1' then
+                -- Force loading of timer A now from latch
+                reg_timera <= reg_timera_latch;
+              end if;
+              reg_timera_oneshot <= fastio_wdata(3);
+              reg_timera_toggle_or_pulse <= fastio_wdata(2);
+              reg_timera_pb6_out <= fastio_wdata(1);
+              reg_timera_start <= fastio_wdata(0);
             when x"f" =>
-              fastio_rdata <= unsigned(reg_tod_alarm_edit
-                                       & reg_timerb_tick_source
-                                       & '0'  -- strobe always reads as 0
-                                       & reg_timerb_oneshot
-                                       & reg_timerb_toggle_or_pulse
-                                       & reg_timerb_pb7_out
-                                       & reg_timerb_start);
-            when others => fastio_rdata <= (others => 'Z');
+              reg_tod_alarm_edit <= std_logic(fastio_wdata(7));
+              reg_timerb_tick_source <= std_logic_vector(fastio_wdata(6 downto 5));
+              if fastio_wdata(4)='1' then
+                -- Force loading of timer A now from latch
+                reg_timerb <= reg_timerb_latch;
+              end if;
+              reg_timerb_oneshot <= std_logic(fastio_wdata(3));
+              reg_timerb_toggle_or_pulse <= std_logic(fastio_wdata(2));
+              reg_timerb_pb7_out <= std_logic(fastio_wdata(1));
+              reg_timerb_start <= std_logic(fastio_wdata(0));                  
+            when others => null;
           end case;
         end if;
       end if;
-    end if;
+    end if;      
   end process;
   
 end behavioural;
