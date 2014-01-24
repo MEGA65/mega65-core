aea27b4703e7728aee39ddd64a8e33dbfed366ea
gardners <paul@servalproject.org>
2014-01-24 22:19:41 +1030

    now works to load programs by trapping LOAD.

--- monitor_drive.c
+++ monitor_drive.c
@@ -33,6 +33,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <string.h>
 #include <ctype.h>
 
+int process_char(unsigned char c,int live);
+
 int slow_write(int fd,char *d,int l)
 {
   // UART is at 230400bps, but reading commands has no FIFO, and echos
@@ -53,13 +55,15 @@ int state=0;
 int name_len,name_lo,name_hi,name_addr=-1;
 char filename[17];
 FILE *f=NULL;
+char *search_path=".";
 
-int process_line(char *line)
+int process_line(char *line,int live)
 {
   int pc,a,x,y,sp,p;
   int addr;
   char filename[17];
-  printf("[%s]\n",line);
+  // printf("[%s]\n",line);
+  if (!live) return 0;
   if (sscanf(line,"%04x %02x %02x %02x %02x %02x",
 	     &pc,&a,&x,&y,&sp,&p)==6) {
     printf("PC=$%04x\n",pc);
@@ -92,7 +96,9 @@ int process_line(char *line)
       // Invert case
       int i; for(i=0;i<name_len;i++) if (isalpha(filename[i])) filename[i]^=0x20;
       printf("Filename is %s\n",filename);
-      f=fopen(filename,"r");
+      char fullname[1024];
+      snprintf(fullname,1024,"%s/%s",search_path,filename);
+      f=fopen(fullname,"r");
       if (f==NULL) {
 	// file not found error
 	usleep(50000);
@@ -100,8 +106,59 @@ int process_line(char *line)
 	usleep(50000);
 	slow_write(fd,"t0\r",3);
 	usleep(50000);
+      } else {
+	int load_addr=fgetc(f);
+	load_addr|=fgetc(f)<<8;
+	printf("Load address is $%04x\n",load_addr);
+	usleep(50000);
+	unsigned char buf[1024];
+	int b=fread(buf,1,1024,f);
+	while(b>0) {
+	  int i;
+	  int n;
+	  for(i=0;i<b;i+=16) {
+	    if ((i+16)>b) n=b-i; else n=16;
+	    char cmd[64];
+	    printf("Read to $%04x\r",load_addr);
+	    fflush(stdout);
+	    // XXX - writes 16 bytes even if there are less bytes ready.
+	    sprintf(cmd,"s%x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\r",
+		    load_addr,
+		    buf[i+0],buf[i+1],buf[i+2],buf[i+3],buf[i+4],buf[i+5],buf[i+6],buf[i+7],
+		    buf[i+8],buf[i+9],buf[i+10],buf[i+11],buf[i+12],buf[i+13],buf[i+14],buf[i+15]);
+
+	    slow_write(fd,cmd,strlen(cmd));
+	    usleep(10000);
+	    load_addr+=n;
+
+	    {
+	      unsigned char read_buff[1024];
+	      int b=read(fd,read_buff,1024);
+	      if (b>0) {
+		int i;
+		for(i=0;i<b;i++) {
+		  process_char(read_buff[i],0);
+		}
+	      }
+	    }
+	  }
+	  b=fread(buf,1,1024,f);
+	}
+	fclose(f); f=NULL;
+	printf("\n");
+	// loaded ok.
+	char cmd[64];
+	snprintf(cmd,64,"s9ff0 18 a9 00 a2 %02x a0 %02x 4c a9 f5\r",
+		 load_addr&0xff,(load_addr>>8)&0xff);
+	usleep(50000);
+	slow_write(fd,cmd,strlen(cmd));
+	usleep(50000);
+	slow_write(fd,"g9ff0\r",6);
+	usleep(50000);
+	slow_write(fd,"t0\r",3);
+	usleep(50000);
+
       }
-      fclose(f); f=NULL;
     }
   }
   return 0;
@@ -111,11 +168,11 @@ int process_line(char *line)
 char line[1024];
 int line_len=0;
 
-int process_char(unsigned char c)
+int process_char(unsigned char c, int live)
 {
   if (c=='\r'||c=='\n') {
     line[line_len]=0;
-    if (line_len>0) process_line(line);
+    if (line_len>0) process_line(line,live);
     line_len=0;
   } else {
     if (line_len<1023) line[line_len++]=c;
@@ -125,6 +182,7 @@ int process_char(unsigned char c)
 
 int main(int argc,char **argv)
 {
+  search_path=argv[2];
   fd=open(argv[1],O_RDWR);
   if (fd==-1) perror("open");
   fcntl(fd,F_SETFL,fcntl(fd, F_GETFL, NULL)|O_NONBLOCK);
@@ -142,6 +200,7 @@ int main(int argc,char **argv)
   if (tcsetattr(fd, TCSANOW, &t)) perror("Failed to set terminal parameters");
 
   // Send ^U r <return> to print registers and get into a known state.
+  usleep(50000);
   slow_write(fd,"\025r\r",3);
   usleep(50000);
   slow_write(fd,"bf4a2\r",6);   // Also setup breakpoint
@@ -163,7 +222,7 @@ int main(int argc,char **argv)
 	if (b>0) {
 	  int i;
 	  for(i=0;i<b;i++) {
-	    process_char(read_buff[i]);
+	    process_char(read_buff[i],1);
 	  }
 	} else usleep(10000);
 	if (time(0)>last_check) {
