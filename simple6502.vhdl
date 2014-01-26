0e2abf970d60f784a3efd68ad4afa3b66f93260c
gardners <paul@servalproject.org>
2014-01-26 16:52:54 +1030

    update copyright to LGPL for compatibility with Xilinx modules. This function contains code that is based on algorithms from VICE that are available in C source under the GPL.  As we have not used their source, but rather reimplemented their algorithms, we are not obliged to use the GPL.  We would rather use the GPL, but the Xilinx modules are incompatible, including the clock generator.

--- simple6502.vhdl
+++ simple6502.vhdl
@@ -3,30 +3,22 @@
 -- Written by
 --    Paul Gardner-Stephen <hld@c64.org>
 --
--- * Portions derived from  6510core.c - MOS6510 emulation core.
--- *
--- * Written by
--- *  Ettore Perazzoli <ettore@comm2000.it>
--- *  Andreas Boose <viceteam@t-online.de>
--- *
--- * DTV sections written by
--- *  M.Kiesel <mayne@users.sourceforge.net>
--- *  Hannu Nuotio <hannu.nuotio@tut.fi>
--- *
--- * This file is part of VICE, the Versatile Commodore Emulator.
--- * See README for copyright notice.
+-- * ADC/SBC algorithm derived from  6510core.c - WICE MOS6510 emulation core.
+-- *   Written by
+-- *    Ettore Perazzoli <ettore@comm2000.it>
+-- *    Andreas Boose <viceteam@t-online.de>
 -- *
 -- *  This program is free software; you can redistribute it and/or modify
--- *  it under the terms of the GNU General Public License as published by
--- *  the Free Software Foundation; either version 2 of the License, or
--- *  (at your option) any later version.
+-- *  it under the terms of the GNU Lesser General Public License as
+-- *  published by the Free Software Foundation; either version 2 of the
+-- *  License, or (at your option) any later version.
 -- *
 -- *  This program is distributed in the hope that it will be useful,
 -- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- *  GNU General Public License for more details.
 -- *
--- *  You should have received a copy of the GNU General Public License
+-- *  You should have received a copy of the GNU Lesser General Public License
 -- *  along with this program; if not, write to the Free Software
 -- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 -- *  02111-1307  USA.
