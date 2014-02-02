05bdbd81b47e204502045cb16b2ee57b00441cc9
gardners <paul@servalproject.org>
2014-02-02 20:15:58 +1030

    auto generate addressing mode list and byte count table

--- 4510tables.c
+++ 4510tables.c
@@ -6,6 +6,9 @@
 char *opnames[256]={NULL};
 char *modes[256]={NULL};
 
+char *modelist[256]={NULL};
+int modecount=0;
+
 int main(int argc,char **argv)
 {
   FILE *f=fopen("64net.opc","r");
@@ -62,11 +65,18 @@ int main(int argc,char **argv)
     for(j=0;modes[i][j];j++) {
       switch(modes[i][j]) {
       case '(': mode[o++]='I'; break;
+      case '#': mode[o++]='i'; mode[o++]='m'; mode[o++]='m'; break;
       case 'n': case 'r': case 'X': case 'Y': case 'Z': case 'S': case 'P': case 'A':
 	mode[o++]=modes[i][j]; break;
       }
     }
     mode[o]=0; if (o==2) strcpy(mode,"M_impl"); o=strlen(mode);
+    int m;
+    for(m=0;m<modecount;m++) if (!strcasecmp(mode,modelist[m])) break;
+    if (m>=modecount) {
+      modelist[modecount++]=strdup(mode);
+    }
+
     printf("%s",mode);
     if (i<255) { printf(",");
       while(o<8) { printf(" "); o++; }
@@ -75,5 +85,25 @@ int main(int argc,char **argv)
     else printf(");\n");
     if ((i&7)==7) printf("\n    ");
   }
+
+  printf("\n  type addressingmode is (\n    ");
+  int m;
+  for(m=0;m<modecount;m++) {
+    if (m&&(m!=modecount-1)) printf(",");
+    if ((m&7)==7) printf("\n    ");
+    printf("%s",modelist[m]);
+  }
+  printf(");\n\n");
+
+  printf("  type mode_list is array(addressingmode'low to addressingmode'high) of integer;\n  constant mode_bytes_lut : mode_list := (\n");
+  for(m=0;m<modecount;m++) {
+    int c=0;
+    for(i=0;modelist[m][i];i++) {
+      if (modelist[m][i]=='n'||modelist[m][i]=='r') c++;
+    }
+    if (m>0) printf(",\n");
+    printf("    %s => %d",modelist[m],c/2);    
+  }
+  printf(");\n\n");
 
 }
