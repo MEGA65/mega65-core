022cb73b7baf2bee96d856709972ab1f5518a4d9
gardners <paul@servalproject.org>
2014-02-12 20:44:34 +1030

    fix compile error introduced while fixing synthesis warnings.

--- vga/vga.vhd
+++ vga/vga.vhd
@@ -491,7 +491,7 @@ begin
               data_o => chardata
               );
 
-  process(cpuclock,fastio_addr) is
+  process(cpuclock,fastio_addr,) is
     variable register_bank : unsigned(7 downto 0);
     variable register_page : unsigned(3 downto 0);
     variable register_num : unsigned(7 downto 0);
@@ -621,7 +621,7 @@ begin
           std_logic_vector(screen_ram_base(13 downto 10))
           & std_logic_vector(character_set_address(13 downto 10));
       elsif register_number=25 then          -- $D019 compatibility IRQ bits
-        fastio_rdata(7) <= irq_asserted;
+        fastio_rdata(7) <= irq_drive;
         fastio_rdata(6) <= '1';       -- NC
         fastio_rdata(5) <= '1';       -- NC
         fastio_rdata(4) <= '1';       -- NC
