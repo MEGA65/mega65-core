c19c1f241465ef58d218ab41b9c1108083fcc39f
gardners <paul@servalproject.org>
2014-02-13 21:24:27 +1030

    reveal SD/SDHC mode in $D680

--- sdcardio.vhdl
+++ sdcardio.vhdl
@@ -369,7 +369,7 @@ begin  -- behavioural
               fastio_rdata(7) <= '0';
               fastio_rdata(6) <= sdio_error;
               fastio_rdata(5) <= sdio_fsm_error;
-              fastio_rdata(4) <= '0';
+              fastio_rdata(4) <= sdhc_mode;
               fastio_rdata(3) <= sector_buffer_mapped;
               fastio_rdata(2) <= sd_reset;
               fastio_rdata(1) <= sdio_busy;
