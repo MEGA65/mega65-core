ec418b5f9e31ba021a358fbd7f52202a0be51eb3
gardners <paul@servalproject.org>
2014-02-24 21:19:56 +1030

    try tighter fix for MAP instruction bug since previous fix didn't do it. Issue #30

--- gs4510.vhdl
+++ gs4510.vhdl
@@ -391,7 +391,7 @@ begin
     if short_address(15)='1' then
       if reg_map_high(blocknum)='1' then
         temp_address(27 downto 20) := reg_mb_high;
-        temp_address(19 downto 8) := reg_offset_high+(128+(blocknum*32))+short_address(14 downto 8);
+        temp_address(19 downto 8) := reg_offset_high+(128+(blocknum*32))+to_integer(short_address(14 downto 8));
         temp_address(7 downto 0) := short_address(7 downto 0);
 
       else
@@ -401,7 +401,7 @@ begin
     else
       if reg_map_low(blocknum)='1' then
         temp_address(27 downto 20) := reg_mb_low;
-        temp_address(19 downto 8) := reg_offset_low+(0+(blocknum*32))+short_address(14 downto 8);
+        temp_address(19 downto 8) := reg_offset_low+(0+(blocknum*32))+to_integer(short_address(14 downto 8));
         temp_address(7 downto 0) := short_address(7 downto 0);
       else
         temp_address(27 downto 16) := (others => '0');
