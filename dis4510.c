512ae8829a587ac2ca10118a3ca9ce472a9816b7
gardners <paul@servalproject.org>
2014-02-26 02:08:56 +1030

    improvements to 4510 disassembler for examining C65 ROMs.

--- dis4510.c
+++ dis4510.c
@@ -8,6 +8,9 @@ char *modes[256]={NULL};
 
 int main(int argc,char **argv)
 {
+  char *annotations[65536]={0};
+  char is_data[65536]={0};
+
   FILE *f=fopen("64net.opc","r");
   if (!f) {
     perror("Could not open 64net.opc");
@@ -33,9 +36,50 @@ int main(int argc,char **argv)
     }
   }
 
-  if (argc!=3) {
-    fprintf(stderr,"usage: dis4510 <binary file> <load address (hex)>\n");
+  if (argc<3||argc>4) {
+    fprintf(stderr,"usage: dis4510 <binary file> <load address (hex)> [address annotations]\n");
     return -1;
+  }
+
+  if (argv[3]) {
+    f=fopen(argv[3],"r");
+    if (f) {
+      char line[1024];
+      int address,addresshi;
+      char note[1024];
+      int count=0;
+      line[0]=0; fgets(line,1024,f);
+      while(line[0]) {
+	if (sscanf(line,"data %x %x",&address,&addresshi)==2)
+	  {
+	    int i;
+	    for(i=address;i<=addresshi;i++) is_data[i]=1;
+	    printf("Data from %x - %x\n",address,addresshi);
+	  }
+	else if (sscanf(line,"word %x %x",&address,&addresshi)==2)
+	  {
+	    int i;
+	    for(i=address;i<=addresshi;i++) is_data[i]=2;
+	    printf("Words from %x - %x\n",address,addresshi);
+	  }
+	else if (sscanf(line,"text %x %x",&address,&addresshi)==2)
+	  {
+	    int i;
+	    for(i=address;i<=addresshi;i++) is_data[i]=3;
+	    printf("Text from %x - %x\n",address,addresshi);
+	  }
+	else if (sscanf(line,"%x %[^\n\r]",&address,note)==2)
+	  {
+	    if (address>=0&&address<65536) {
+	      annotations[address]=strdup(note);
+	      count++;
+	    }
+	  }
+	line[0]=0; fgets(line,1024,f);
+      }
+      fclose(f);
+      fprintf(stderr,"Read %d annotations.\n",count);
+    }
   }
 
   f=fopen(argv[1],"r");
@@ -56,83 +100,135 @@ int main(int argc,char **argv)
       char args[1024];
       int opcode=mem[i];
       int c=0;
-
-      if (mem[i]==mem[i+1]&&mem[i]==mem[i+2]&&mem[i]==mem[i+3]) {
-	// run of identical bytes, assume data.
-	int j=i;
-	for(j=i;mem[j]==mem[i];j++) continue;
-	j-=1;
-	printf("%04X - %04X .BY $%02X ; repeated byte\n\n",
-	       load_address+i,load_address+j,mem[i]);
-
-	i=j+1;
-	continue;
+      
+      /*
+      if (is_data[load_address+i-1]==0) {
+	if (mem[i]==mem[i+1]&&mem[i]==mem[i+2]&&mem[i]==mem[i+3]) {
+	  // run of identical bytes, assume data.
+	  int j=i;
+	  for(j=i;mem[j]==mem[i];j++) continue;
+	  j-=1;
+	  printf("%04X - %04X .BY $%02X ; repeated byte\n\n",
+		 load_address+i,load_address+j,mem[i]);
+	  
+	  i=j+1;
+	  continue;
+	}
       }
+      */
 
       // printf("op=%02X, mode=%s\n",opcode,modes[opcode]);
 
+      // blank line before and after data block
+      if (is_data[load_address+i-1]!=is_data[load_address+i-1-1])
+	printf("\n");
+
       // Print address and opcode byte
+      if (annotations[load_address+i]) {
+	printf("; %s\n",annotations[load_address+i]);
+      }
       printf("%04X  %02X",load_address+i,mem[i]);
       i++;
+
       int value;
+      int annotation_address=-1;
       int j,digits;
-      for(j=0;modes[opcode][j];) {
-	args[o]=0;
-	// printf("j=%d, args=[%s], template=[%s]\n",j,args,modes[opcode]);
-	switch(modes[opcode][j]) {
-	case 'n': // normal argument
-	  digits=0;
-	  while (modes[opcode][j++]=='n') digits++; j--;
-	  if (digits==2) {
-	    value=mem[i];
-	    printf(" %02X",mem[i++]);
-	    sprintf(&args[o],"%02X",value); o+=2;
-	    c+=3;
+
+      if (is_data[load_address+i-1]==1) {
+	printf("         .BY $%02X",mem[i-1]);	
+      } else if (is_data[load_address+i-1]==2) {
+	printf(" %02X      .W  $%04X",mem[i],mem[i-1]|(mem[i]<<8));
+	annotation_address=mem[i-1]|(mem[i]<<8);
+	i++;
+      } else if (is_data[load_address+i-1]==3) {
+	printf("         .text ");
+	i--;
+	j=i;
+	while(is_data[load_address+i]==3&&(i-j<8)) {
+	  printf("%02X ",mem[i++]);
+	}
+	printf(" '");
+	i=j;
+	while(is_data[load_address+i]==3&&(i-j<8)) {
+	  if (mem[i]>=' '&&mem[i]<=0x7c)
+	    printf("%c",mem[i++]);
+	  else
+	    printf("<%02X>",mem[i++]);
+	}
+	printf("'");
+      } else {
+	for(j=0;modes[opcode][j];) {
+	  args[o]=0;
+	  // printf("j=%d, args=[%s], template=[%s]\n",j,args,modes[opcode]);
+	  switch(modes[opcode][j]) {
+	  case 'n': // normal argument
+	    digits=0;
+	    while (modes[opcode][j++]=='n') digits++; j--;
+	    if (digits==2) {
+	      value=mem[i];
+	      annotation_address=value;
+	      printf(" %02X",mem[i++]);
+	      sprintf(&args[o],"%02X",value); o+=2;
+	      c+=3;
+	    }
+	    if (digits==4) {
+	      value=mem[i]+(mem[i+1]<<8);
+	      annotation_address=value;
+	      printf(" %02X",mem[i++]);
+	      printf(" %02X",mem[i++]);
+	      sprintf(&args[o],"%04X",value); o+=4;
+	      c+=6;
+	    }
+	    break;
+	  case 'r': // relative argument
+	    digits=0;
+	    while (modes[opcode][j++]=='r') digits++; j--;
+	    if (digits==2) {
+	      value=mem[i];
+	      if (value&0x80) value-=0x100;
+	      printf(" %02X",mem[i++]);
+	      value+=load_address+i;
+	      sprintf(&args[o],"%04X",value); o+=4;
+	      annotation_address=value;
+	      c+=3;
+	    }
+	    if (digits==4) {
+	      value=mem[i]+(mem[i+1]<<8);
+	      if (value&0x8000) value-=0x10000;
+	      printf(" %02X",mem[i++]);
+	      // 16 bit branches are still relative to the same point as 8-bit ones,
+	      // i.e., after the 2nd of the 3 bytes
+	      value+=load_address+i; 	    
+	      annotation_address=value;
+	      printf(" %02X",mem[i++]);
+	      sprintf(&args[o],"%04X",value); o+=4;
+	      c+=6;
+	    }
+	    break;
+	  default: 
+	    args[o++]=modes[opcode][j++];
+	    break;
 	  }
-	  if (digits==4) {
-	    value=mem[i]+(mem[i+1]<<8);
-	    printf(" %02X",mem[i++]);
-	    printf(" %02X",mem[i++]);
-	    sprintf(&args[o],"%04X",value); o+=4;
-	    c+=6;
-	  }
-	  break;
-	case 'r': // relative argument
-	  digits=0;
-	  while (modes[opcode][j++]=='r') digits++; j--;
-	  if (digits==2) {
-	    value=mem[i];
-	    if (value&0x80) value-=0x100;
-	    printf(" %02X",mem[i++]);
-	    value+=load_address+i;
-	    sprintf(&args[o],"%04X",value); o+=4;
-	    c+=3;
-	  }
-	  if (digits==4) {
-	    value=mem[i]+(mem[i+1]<<8);
-	    if (value&0x8000) value-=0x10000;
-	    printf(" %02X",mem[i++]);
-	    // 16 bit branches are still relative to the same point as 8-bit ones,
-	    // i.e., after the 2nd of the 3 bytes
-	    value+=load_address+i; 	    
-	    printf(" %02X",mem[i++]);
-	    sprintf(&args[o],"%04X",value); o+=4;
-	    c+=6;
-	  }
-	  break;
-	default: 
-	  args[o++]=modes[opcode][j++];
-	  break;
+	  args[o]=0;
+	  // printf("[%s]\n",args);
 	}
 	args[o]=0;
-	// printf("[%s]\n",args);
+	while(c<9) { printf(" "); c++; }
+	printf("%s %s",opnames[opcode],args);      
+	c+=strlen(opnames[opcode])+1+strlen(args);
+      } 
+      if (annotation_address>65536) annotation_address&=0xffff;
+      if (annotation_address!=-1&&annotations[annotation_address]) {
+	while(c<40) { printf(" "); c++; }
+	printf("; %s\n",annotations[annotation_address]);
+      } else printf("\n");
+      if (!is_data[load_address+i-1]) {
+	if (!strcasecmp(opnames[opcode],"RTI")) printf("\n");
+	if (!strcasecmp(opnames[opcode],"RTS")) printf("\n");
+	if (!strcasecmp(opnames[opcode],"JMP")) printf("\n");
+	if (!strcasecmp(opnames[opcode],"BRA")) printf("\n");
       }
-      args[o]=0;
-      while(c<9) { printf(" "); c++; }
-      printf("%s %s\n",opnames[opcode],args);
-      if (!strcasecmp(opnames[opcode],"RTS")) printf("\n");
-      if (!strcasecmp(opnames[opcode],"JMP")) printf("\n");
-      if (!strcasecmp(opnames[opcode],"BRA")) printf("\n");
+
       fflush(stdout);
     }
 
