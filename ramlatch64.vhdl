4cdd54422b52f0d58293a20f94532afd2579b1a6
gardners <paul@servalproject.org>
2014-04-10 14:42:29 +0930

    add 2nd layer of synchronisation to try to fix writing glitches.

--- ramlatch64.vhdl
+++ ramlatch64.vhdl
@@ -22,19 +22,27 @@ entity ramlatch64 is
 end ramlatch64;
 
 architecture behavioural of ramlatch64 is
-
+  signal address_internal : std_logic_vector(13 downto 0);
+  signal wea_internal : std_logic_vector(7 downto 0);
+  signal data_internal : std_logic_vector(63 downto 0);
 begin  -- behavioural
 
   -- purpose: copy address, write enable and write data lines on rising edge of slow clock
   -- type   : combinational
   -- inputs : slowclock, address_in, wea_in, data_in
   -- outputs: *_out
+  
   process (slowclock, address_in, wea_in, data_in)
   begin  -- process
     if rising_edge(slowclock) then
-      address_out <= address_in;
-      wea_out <= wea_in;
-      data_out <= data_in;
+      address_internal <= address_in;
+      wea_internal <= wea_in;
+      data_internal <= data_in;
+    end if;
+    if rising_edge(fastclock) then
+      address_out <= address_internal;
+      wea_out <= wea_internal;
+      data_out <= data_internal;
     end if;
   end process;
 
