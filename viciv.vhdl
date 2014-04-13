0496d5b382c9db75584167fdbf8b7a1bec196f18
gardners <paul@servalproject.org>
2014-04-13 21:54:30 +0930

    fix width of expression in another spot

--- viciv.vhdl
+++ viciv.vhdl
@@ -1667,7 +1667,7 @@ begin
 
               -- Compute the address for the screen row.
               screen_row_address <= screen_ram_base(16 downto 0) + first_card_of_row;
-              screen_row_current_address <= screen_ram_base + first_card_of_row;
+              screen_row_current_address <= screen_ram_base(16 downto 0) + first_card_of_row;
             end if;
             chargen_y_sub <= (others => '0');
           else
