ffbf21e2a9457e9f9e2b59ae5b4c3656cb15565f
gardners <paul@servalproject.org>
2014-06-30 21:24:14 +0930

    tweak IO waitstates to work on the assumption that all dual-port buffer memories will be clocked at cpuclock and all other io registers will be at IO clock.  This means we can eventually reduce waitstates down to 1.

--- machine.vhdl
+++ machine.vhdl
@@ -553,12 +553,12 @@ begin
       -- rising edge of IOclock.
       -- This might need a bit of adjustment, but the principle should be sound.
       case clock_phase is
-        when 0 => cpuclock <= '1'; ioclock<='0'; io_wait_states <= x"03";
-        when 1 => cpuclock <= '0'; ioclock<='0'; io_wait_states <= x"03";
-        when 2 => cpuclock <= '0'; ioclock<='0'; io_wait_states <= x"03";
-        when 3 => cpuclock <= '1'; ioclock<='1'; io_wait_states <= x"04";
-        when 4 => cpuclock <= '0'; ioclock<='1'; io_wait_states <= x"04";
-        when 5 => cpuclock <= '0'; ioclock<='1'; io_wait_states <= x"04";
+        when 0 => cpuclock <= '1'; ioclock<='0'; io_wait_states <= x"02";
+        when 1 => cpuclock <= '0'; ioclock<='0'; io_wait_states <= x"02";
+        when 2 => cpuclock <= '0'; ioclock<='0'; io_wait_states <= x"02";
+        when 3 => cpuclock <= '1'; ioclock<='1'; io_wait_states <= x"03";
+        when 4 => cpuclock <= '0'; ioclock<='1'; io_wait_states <= x"03";
+        when 5 => cpuclock <= '0'; ioclock<='1'; io_wait_states <= x"03";
       end case;
       --case clock_phase is
       --  when 0 => cpuclock <= '1'; ioclock<='0'; io_wait_states <= x"05";
