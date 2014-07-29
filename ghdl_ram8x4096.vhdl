ffcc7fb2dc6582852b534af19e01c5d8477147d7
gardners <paul@servalproject.org>
2014-07-30 02:34:59 +0930

    add 4KB memory to model.

---
+++ ghdl_ram8x4096.vhdl
@@ -0,0 +1,46 @@
+use WORK.ALL;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use Std.TextIO.all;
+use work.debugtools.all;
+
+ENTITY ram8x4096 IS
+  PORT (
+    clk : IN STD_LOGIC;
+    cs : IN STD_LOGIC;
+    w : IN std_logic;
+    write_address : IN integer range 0 to 4095;
+    wdata : IN unsigned(7 DOWNTO 0);
+    address : IN integer range 0 to 4095;
+    rdata : OUT unsigned(7 DOWNTO 0)
+    );
+END ram8x4096;
+
+architecture behavioural of ram8x4096 is
+
+  type ram_t is array (0 to 4095) of unsigned(7 downto 0);
+  signal ram : ram_t := (
+    others => x"00");
+
+begin  -- behavioural
+
+  process(clk,cs,address)
+  begin
+    if cs='1' then
+      rdata <= ram(address);
+    else
+      rdata <= (others => 'Z');
+    end if;
+
+    if(rising_edge(Clk)) then
+      if w='1' then
+        ram(write_address) <= wdata;
+        report "writing $" & to_hstring(wdata) & " to sector buffer offset $"
+          & to_hstring(to_unsigned(write_address,12)) severity note;
+      end if;
+    end if;    
+  end process;
+
+end behavioural;
