a636a486426b6be3ce630cf0d2507fe7fcb6f7ef
gardners <paul@servalproject.org>
2014-09-19 17:27:10 +0930

    fix compile error.

--- ethernet.vhdl
+++ ethernet.vhdl
@@ -851,7 +851,7 @@ begin  -- behavioural
           when x"b" =>
             fastio_rdata <= eth_tx_size_padded(7 downto 0);
           when x"c" =>
-            fastio_rdata(7 downto 4) <= x"00";
+            fastio_rdata(7 downto 4) <= x"0";
             fastio_rdata(3 downto 0) <= eth_tx_size_padded(11 downto 8);
           when x"d" =>
             fastio_rdata <= rrnet_debug(7 downto 0);
