0822f003318acbbfe6b0824ff85f3eb26aa72277
gardners <paul@servalproject.org>
2014-10-21 18:56:31 +1030

    document thumbnail generator ports.

--- framepacker.vhdl
+++ framepacker.vhdl
@@ -188,7 +188,12 @@ begin  -- behavioural
             thumbnail_read_address <= thumbnail_read_address + 1;
           end if;
         elsif fastio_addr = x"D367E" and hypervisor_mode='1' then
+          -- @IO:GS $D67D-$D67E - Read-only hardware-generated thumbnail of display (accessible only in hypervisor mode)
           -- @IO:GS $D67E - Write to reset port address for thumbnail generator
+          -- @IO:GS $D67E - Read to obtain status of thumbnail generator.
+          -- @IO:GS $D67E.7 - Thumbnail is valid if 1.  Else there has not been a complete frame since elapsed without a trap to hypervisor mode, in which case the thumbnail may not reflect the current process.
+          -- @IO:GS $D67E.6 - Thumbnail drawing was in progress.
+          
           thumbnail_read_address <= (others => '0');
           fastio_rdata(7) <= thumbnail_valid;
           fastio_rdata(6) <= thumbnail_started;
