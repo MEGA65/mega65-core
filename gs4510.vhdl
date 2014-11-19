6501bb4c8efa0e8847d131d3bfc372c68e7708d4
gardners <paul@servalproject.org>
2014-11-19 20:15:26 +1030

    add IO documentation marker for RAM expansion controller address space.

--- gs4510.vhdl
+++ gs4510.vhdl
@@ -23,6 +23,8 @@
 -- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 -- *  02111-1307  USA.
 
+-- @IO:65 $D0A0-$D0FF - Reserved for C65 RAM Expansion Controller.
+
 use WORK.ALL;
 
 library IEEE;
