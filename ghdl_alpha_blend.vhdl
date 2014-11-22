5266f6bdbd0f4ccec7b9484ae427c92449d54a6d
gardners <paul@servalproject.org>
2014-11-22 17:27:07 +1030

    add extra cycle latency to alpha blender to meet timing.

--- ghdl_alpha_blend.vhdl
+++ ghdl_alpha_blend.vhdl
@@ -35,27 +35,35 @@ end alpha_blend_top;
 
 architecture behavioural of alpha_blend_top is
 begin
+
+  signal r0  : integer;
+  signal r1  : integer;
+  signal g0  : integer;
+  signal g1  : integer;
+  signal b0  : integer;
+  signal b1  : integer;
+  
   process (clk1x) is
     variable temp : integer;
   begin
     if rising_edge(clk1x) then
-      temp := to_integer(unsigned(r_strm0))
-              *to_integer(unsigned(alpha_strm))
-              +to_integer(unsigned(r_strm1))
-              *(1024-to_integer(unsigned(alpha_strm)));
-      r_blnd <= std_logic_vector(to_unsigned(temp/1024,10));
+      r0 <= to_integer(unsigned(r_strm0))
+            *to_integer(unsigned(alpha_strm));
+      r1 <= to_integer(unsigned(r_strm1))
+            *(1024-to_integer(unsigned(alpha_strm)));
+      r_blnd <= std_logic_vector(to_unsigned((r0+r1)/1024,10));
 
-      temp := to_integer(unsigned(g_strm0))
-              *to_integer(unsigned(alpha_strm))
-              +to_integer(unsigned(g_strm1))
-              *(1024-to_integer(unsigned(alpha_strm)));
-      g_blnd <= std_logic_vector(to_unsigned(temp/1024,10));
+      g0 <= to_integer(unsigned(g_strm0))
+            *to_integer(unsigned(alpha_strm));
+      g1 <= to_integer(unsigned(g_strm1))
+            *(1024-to_integer(unsigned(alpha_strm)));
+      g_blnd <= std_logic_vector(to_unsigned((g0+g1)/1024,10));
       
-      temp := to_integer(unsigned(b_strm0))
-              *to_integer(unsigned(alpha_strm))
-              +to_integer(unsigned(b_strm1))
-              *(1024-to_integer(unsigned(alpha_strm)));
-      b_blnd <= std_logic_vector(to_unsigned(temp/1024,10));
+      b0 <= to_integer(unsigned(b_strm0))
+            *to_integer(unsigned(alpha_strm));
+      b1 <= to_integer(unsigned(b_strm1))
+            *(1024-to_integer(unsigned(alpha_strm)));
+      b_blnd <= std_logic_vector(to_unsigned((b0+b1)/1024,10));
     end if;
   end process;
 end behavioural;
