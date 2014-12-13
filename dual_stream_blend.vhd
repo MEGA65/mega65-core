cf597db495a26076f104ff88fba2b2347398cda6
gardners <paul@servalproject.org>
2014-12-13 21:11:15 +1030

    switch to unix encoding

--- dual_stream_blend.vhd
+++ dual_stream_blend.vhd
@@ -76,12 +76,15 @@ signal vid0_in :     std_logic_vector(17 downto 0);  -- Registers for inputs
 signal vid1_in :     std_logic_vector(17 downto 0);
 signal alpha_in :    std_logic_vector(17 downto 0);
 signal one_minus_in :std_logic_vector(17 downto 0);
-signal mux_sel :     std_logic;           -- Mux select for DDR mux
 signal blend_pass :  std_logic;           -- Mux select for DSP 48 Z mux
-signal dspA :        std_logic_vector(17 downto 0);     -- DSP48 A input 
-signal dspB :        std_logic_vector(17 downto 0);     -- DSP48 B input
-signal dspP :        std_logic_vector(47 downto 0);     -- DSP48 output
-signal opmode :      std_logic_vector(6 downto 0);      -- OPMODE input to DSP48
+signal dsp0A :        std_logic_vector(17 downto 0);     -- DSP48 A input 
+signal dsp0B :        std_logic_vector(17 downto 0);     -- DSP48 B input
+signal dsp1A :        std_logic_vector(17 downto 0);     -- DSP48 A input 
+signal dsp1B :        std_logic_vector(17 downto 0);     -- DSP48 B input
+signal dsp0P :        std_logic_vector(47 downto 0);     -- DSP48 output
+signal dsp1P :        std_logic_vector(47 downto 0);     -- DSP48 output
+signal opmode0 :      std_logic_vector(6 downto 0);      -- OPMODE input to DSP48
+signal opmode1 :      std_logic_vector(6 downto 0);      -- OPMODE input to DSP48
 signal zero:         std_logic;   -- logic levels for connecting to DSP48
 signal one:          std_logic;   -- verilog model for Modelsim simulation
 signal zero_2:       std_logic_vector(1 downto 0);
@@ -94,9 +97,13 @@ begin
   zero_18 <= "00" & x"0000";  
   zero_48 <= x"000000000000" ;
   -- define input muxes
-  dspA <= vid1_in when mux_sel = '1' else vid0_in;
-  dspB <= one_minus_in when mux_sel = '1' else  alpha_in;
-  opmode <=   '0' &  '1' & not blend_pass  & "0101";
+  dsp0A <= vid0_in;
+  dsp0B <= alpha_in;
+  dsp1A <= vid1_in;
+  dsp1B <= one_minus_in;
+
+  opmode0 <=   '0' &  '1' & (not '0')  & "0101";
+  opmode1 <=   '0' &  '1' & (not '1')  & "0101";
   
   -- define clk1x registers
   process (clk1x, reset) begin
@@ -126,7 +133,7 @@ begin
     end if; -- not reset
   end process;
   -- instance DSP 48 
-  alpha_blend : DSP48
+  alpha_blend0 : DSP48
     generic map (
       AREG => 2,
       BREG => 2,
@@ -141,12 +148,12 @@ begin
       LEGACY_MODE =>  "MULT18X18S" 
     )
     port map(
-      A                 => dspA,           -- Input A to Multiplier
-      B                 => dspB,           -- Input B to Multiplier
+      A                 => dsp0A,           -- Input A to Multiplier
+      B                 => dsp0B,           -- Input B to Multiplier
       C                 => round,          -- Input C to Adder, round to 17 buts
       BCIN              => zero_18,          
       PCIN              => zero_48,       
-      OPMODE            => opmode,  
+      OPMODE            => opmode0,  
       SUBTRACT          => zero,    
       CARRYIN           => zero,     
       CARRYINSEL        => zero_2, 
@@ -167,7 +174,51 @@ begin
       RSTCTRL           => reset,    
       RSTCARRYIN        => reset, 
       BCOUT             => open, 
-      P                 => dspP,           
+      P                 => dsp0P,           
+      PCOUT             => open 
+  );
+  alpha_blend1 : DSP48
+    generic map (
+      AREG => 2,
+      BREG => 2,
+      CREG => 0,
+      CARRYINREG => 0,
+      MREG => 1,
+      PREG => 1,
+      OPMODEREG => 1,
+      SUBTRACTREG => 0,
+      CARRYINSELREG => 0,
+      B_INPUT => "DIRECT",
+      LEGACY_MODE =>  "MULT18X18S" 
+    )
+    port map(
+      A                 => dsp1A,           -- Input A to Multiplier
+      B                 => dsp1B,           -- Input B to Multiplier
+      C                 => round,          -- Input C to Adder, round to 17 buts
+      BCIN              => zero_18,          
+      PCIN              => zero_48,       
+      OPMODE            => opmode1,  
+      SUBTRACT          => zero,    
+      CARRYIN           => zero,     
+      CARRYINSEL        => zero_2, 
+      CLK               => clk2x,        
+      CEA               => one,         
+      CEB               => one,         
+      CEC               => one,         
+      CEP               => one,         
+      CEM               => one,         
+      CECTRL            => one,      
+      CECARRYIN         => one,   
+	  CECINSUB          => one,    
+      RSTA              => reset,       
+      RSTB              => reset,       
+      RSTC              => reset,       
+      RSTP              => reset,       
+      RSTM              => reset,       
+      RSTCTRL           => reset,    
+      RSTCARRYIN        => reset, 
+      BCOUT             => open, 
+      P                 => dsp1P,           
       PCOUT             => open 
   );
 end synth;	
