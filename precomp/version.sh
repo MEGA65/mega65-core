9c057ab9f145877ab4f4e2da19cf2f8116859fb3
Ben-401 <gerb0002@flinders.edu.au>
2016-05-12 15:45:57 +0930

    moved "Makefile" and related files into "precomp" dir, deleted "iomap.txt" because it is generated to compile in "diskmenu.a65" commented-out ".scope", moved files in "tests" dir back one level, deleted "tests/Makefile", renamed "dejavus" to "textmodetest-dejavus" to group related files, in "textmodetest.a65" changed spaces to tabs, deleted "version-template" and put its contents in version.sh, NEED TO CHECK WITH PGS the following: created 911001.bin with zeros because it didnt exist created c65-rom-910111.bin with zeros because it didnt exist

--- precomp/version.sh
+++ precomp/version.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+
+# try git describe --always --abbrev=40 --dirty=+DIRTY
 version=`git log | head | grep commit | head -1 | cut -f2 -d" "``[[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]] && echo "+DIRTY"`
 echo $version
-cat version-template.vhdl | sed -e 's/GITCOMMIT/'${version}'/g' > version.vhdl
+cat > version.vhdl <<ENDTEMPLATE
+library ieee;
+use Std.TextIO.all;
+use ieee.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
 
+package version is
+
+  constant gitcommit : string := "${version}";
+
+end version;
+ENDTEMPLATE
+
+# try git describe --always --abbrev=7 --dirty=*+DIRTY
 version=`git log | head | grep commit | head -1 | cut -f2 -d" " | tr "abcdef" "ABCDEF" | cut -c1-15`\*`[[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]] && echo "+DIRTY"`
 echo $version
 echo 'msg_gitcommit: .byte "GIT COMMIT: '${version}'",0' > version.a65
