156f7b73f8a466fd4da68a0081f6e43ab23947a0
gardners <paul@servalproject.org>
2017-05-06 15:45:01 +0930

    fix remaining merge conflict

--- compile.sh
+++ compile.sh
@@ -216,16 +216,11 @@ for filename in ./sdcard-files/*.bit; do
 done
 
 # now copy the bit-file to the sdcard-output directory, and timestamp it with time and git-status
-<<<<<<< HEAD
 echo "cp ./isework/container.bit ./sdcard-files/bit${datetime2}_${branch2}_${gitstring}.bit"
 cp       ./isework/container.bit ./sdcard-files/bit${datetime2}_${branch2}_${gitstring}.bit
 echo "Generating .MCS SPI flash file from .BIT file..."
 promgen -spi -p mcs -w -o ./sdcard-files/bit${datetime2}_${branch2}_${gitstring}.mcs -s 16384 -u 0 isework/container.bit
 
-=======
-echo "cp ./isework/container.bit ./sdcard-files/bit${datetime2}_${branch2}_${gitstring}${NONDDR_TAG}.bit"
-cp       ./isework/container.bit ./sdcard-files/bit${datetime2}_${branch2}_${gitstring}${NONDDR_TAG}.bit
->>>>>>> kirb/secureMode
 # # and the KICKUP file
 # echo "cp ./src/KICKUP.M65 ./sdcard-files"
 #       cp ./src/KICKUP.M65 ./sdcard-files
