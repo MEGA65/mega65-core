6c1389f8771c4404cabba1ed1f34332d6f93dd26
Paul Gardner-Stephen <paul@servalproject.org>
2017-07-31 16:53:21 +0930

    work on debugging video mode issues

--- src/modeline.c
+++ src/modeline.c
@@ -3,7 +3,53 @@
 
   Takes mode lines in the format of the mythtv database (https://www.mythtv.org/wiki/Modeline_Database), e.g.:
   ModeLine "1920x1080" 148.50 1920 2008 2052 2200 1080 1084 1088 1125 -HSync -VSync
+
+This is almost right, but hsync adjustment needs to go negative  
+:FFD3075 80 AE 87 38 65 44 00 E0 00 00 00 08 40 04 A8 27
+
+
   Modeline "1920x1200" 151.138 1920 1960 1992 2040 1200 1201 1204 1232 -hsync
+
+
+xrandr suggests the following:
+
+1920x1200 (0x2a1) 154.000MHz +HSync -VSync *current +preferred
+        h: width  1920 start 1968 end 2000 total 2080 skew    0 clock  74.04KHz
+        v: height 1200 start 1203 end 1209 total 1235           clock  59.95Hz
+  1600x1200 (0x2a2) 162.000MHz +HSync +VSync
+        h: width  1600 start 1664 end 1856 total 2160 skew    0 clock  75.00KHz
+        v: height 1200 start 1201 end 1204 total 1250           clock  60.00Hz
+  1280x1024 (0x2a3) 135.000MHz +HSync +VSync
+        h: width  1280 start 1296 end 1440 total 1688 skew    0 clock  79.98KHz
+        v: height 1024 start 1025 end 1028 total 1066           clock  75.02Hz
+  1280x1024 (0x2a4) 108.000MHz +HSync +VSync
+        h: width  1280 start 1328 end 1440 total 1688 skew    0 clock  63.98KHz
+        v: height 1024 start 1025 end 1028 total 1066           clock  60.02Hz
+  1152x864 (0x2a5) 108.000MHz +HSync +VSync
+        h: width  1152 start 1216 end 1344 total 1600 skew    0 clock  67.50KHz
+        v: height  864 start  865 end  868 total  900           clock  75.00Hz
+  1024x768 (0x2a6) 78.750MHz +HSync +VSync
+        h: width  1024 start 1040 end 1136 total 1312 skew    0 clock  60.02KHz
+        v: height  768 start  769 end  772 total  800           clock  75.03Hz
+  1024x768 (0x4c) 65.000MHz -HSync -VSync
+        h: width  1024 start 1048 end 1184 total 1344 skew    0 clock  48.36KHz
+        v: height  768 start  771 end  777 total  806           clock  60.00Hz
+  800x600 (0x2a7) 49.500MHz +HSync +VSync
+        h: width   800 start  816 end  896 total 1056 skew    0 clock  46.88KHz
+        v: height  600 start  601 end  604 total  625           clock  75.00Hz
+  800x600 (0x53) 40.000MHz +HSync +VSync
+        h: width   800 start  840 end  968 total 1056 skew    0 clock  37.88KHz
+        v: height  600 start  601 end  605 total  628           clock  60.32Hz
+  640x480 (0x2a8) 31.500MHz -HSync -VSync
+        h: width   640 start  656 end  720 total  840 skew    0 clock  37.50KHz
+        v: height  480 start  481 end  484 total  500           clock  75.00Hz
+  640x480 (0x5c) 25.175MHz -HSync -VSync
+        h: width   640 start  656 end  752 total  800 skew    0 clock  31.47KHz
+        v: height  480 start  490 end  492 total  525           clock  59.94Hz
+  720x400 (0x2a9) 28.320MHz -HSync +VSync
+        h: width   720 start  738 end  846 total  900 skew    0 clock  31.47KHz
+        v: height  400 start  412 end  414 total  449           clock  70.08Hz
+
 
 
 */
@@ -34,29 +80,37 @@ void parse_video_mode(int b[16+5])
 	  hsync_polarity ? '-' : '+',
 	  vsync_polarity ? '-' : '+',
 	  rasters_per_vicii_raster,
-	  frame_hertz,hfreq);  
+	  frame_hertz,hfreq);
+  fprintf(stderr,"   hpixels=$%04x (%d) $D075,$D077.0-3\n",
+	  hpixels,hpixels);
+  fprintf(stderr,"   hwidth=$%04x (%d) $D076,$D077.7-4\n",
+	  hwidth,hwidth);
+  fprintf(stderr,"   vpixels=$%04x (%d) $D078,$D07A.0-3\n",
+	  vpixels,vpixels);
+  fprintf(stderr,"   vheight=$%04x (%d) $D079,$D07A.7-4\n",
+	  vheight,vheight);
+  
   return;
 }
 
 
 int main(int argc,char **argv)
 {
-  if (argc<12) {
-    fprintf(stderr,"usage: modeline <modeline information>\n");
-    exit(-1);
-  }
-
   if (!strcasecmp(":ffd3075",argv[1]))
     {
       // Decode current mode info
       // e.g. from :FFD3075 80 80 97 20 30 44 02 E7 00 00 00 00 00 01 00 01
       int b[16+5];
-      for(int i=0;i<16;i++) b[i+5]=strtoll(argv[i+2],NULL,16);
+      for(int i=0;i<16&&(i<(argc-2));i++) b[i+5]=strtoll(argv[i+2],NULL,16);
       parse_video_mode(b);
 
       return 0;
     }
 
+  if (argc<12) {
+    fprintf(stderr,"usage: modeline <modeline information>\n");
+    exit(-1);
+  }
   
   if (strcasecmp("modeline",argv[1])) { fprintf(stderr,"No modeline keyword.\n"); exit(-1); }
   char *modename=argv[2];
