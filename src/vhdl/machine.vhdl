9b761538e91fc2b0e1fb27f6c50455f1e09a8581
Paul Gardner-Stephen <paul@servalproject.org>
2017-08-05 07:51:47 +0930

    fix type

--- src/vhdl/machine.vhdl
+++ src/vhdl/machine.vhdl
@@ -476,10 +476,10 @@ begin
       elsif segled_counter(17 downto 15)=3 then
         digit := std_logic_vector(seg_led_data(15 downto 12));
       elsif segled_counter(17 downto 15)=4 then
-        digit := uart_char(3 downto 0);
+        digit := std_logic_vector(uart_char(3 downto 0));
       elsif segled_counter(17 downto 15)=5 then
         --digit := std_logic_vector(seg_led_data(23 downto 20));
-        digit := uart_char(7 downto 4);
+        digit := std_logic_vector(uart_char(7 downto 4));
       elsif segled_counter(17 downto 15)=6 then
         digit := std_logic_vector(seg_led_data(27 downto 24));
       elsif segled_counter(17 downto 15)=7 then
