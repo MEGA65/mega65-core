1b1d79e6bb071dc0d5c77e90a2b96d55d1d12724
Paul Gardner-Stephen <paul@servalproject.org>
2017-08-22 14:13:35 +0930

    adjust matrix mode start position.

--- src/vhdl/matrix_compositor.vhdl
+++ src/vhdl/matrix_compositor.vhdl
@@ -61,14 +61,14 @@ architecture Behavioral of matrix_compositor is
 
 --Mode0 Frame
 --640x320
-  constant mode0_startx  : unsigned(13 downto 0):= to_unsigned(1,14);
+  constant mode0_startx  : unsigned(13 downto 0):= to_unsigned(32,14);
   constant mode0_starty : unsigned(11 downto 0):=x"07C";--x"1B8";--x"07C"; --x07C 124
   constant mode0_endx  : unsigned(13 downto 0):=mode0_startx+647;--x"313";--x"507";--x"313"; --x814 d780  -1 +8
   constant mode0_endy : unsigned(11 downto 0):=x"1BB";--x"2F8";--x"1BB"; --124+320 = 444, x1BC -1 
   constant mode0_garbage_end_offset : unsigned(13 downto 0):=to_unsigned(8,14);
 --Mode1 Frame
---1280x640
-  constant mode1_startx  : unsigned(13 downto 0):=to_unsigned(1,14);
+--640x640
+  constant mode1_startx  : unsigned(13 downto 0):=to_unsigned(32,14);
   constant mode1_starty : unsigned(11 downto 0):=x"07C";--x"118";--x"07C"; --07C 124 
   constant mode1_endx  : unsigned(13 downto 0):=mode1_startx+1295;--x"590";--x"64F";--x"590"; --x581 d1409 -1
   constant mode1_endy : unsigned(11 downto 0):=mode1_starty+640;--x"2FB";--x"398";--x"2FB"; --x2FC d764 -1
