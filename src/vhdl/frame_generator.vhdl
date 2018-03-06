89f1b3e7f80fa734a1125e09747608747acd2801
Paul Gardner-Stephen <paul@servalproject.org>
2018-03-07 10:07:39 +1030

    begin implementing minimal lcd/vga frame test

---
+++ src/vhdl/frame_generator.vhdl
@@ -0,0 +1,115 @@
+--
+-- Written by
+--    Paul Gardner-Stephen <hld@c64.org>  2018
+--
+-- *  This program is free software; you can redistribute it and/or modify
+-- *  it under the terms of the GNU Lesser General Public License as
+-- *  published by the Free Software Foundation; either version 3 of the
+-- *  License, or (at your option) any later version.
+-- *
+-- *  This program is distributed in the hope that it will be useful,
+-- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- *  GNU General Public License for more details.
+-- *
+-- *  You should have received a copy of the GNU Lesser General Public License
+-- *  along with this program; if not, write to the Free Software
+-- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+-- *  02111-1307  USA.
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use Std.TextIO.all;
+use work.victypes.all;
+
+entity frame_generator is
+  generic (
+    frame_width : integer := 1065;
+    display_width : integer := 800;
+    frame_height : integer := 625;
+    display_height : integer := 600;
+    vsync_start : integer := 618;
+    hsync_start : integer := 921;
+    hsync_end : integer := 1033
+    )
+  port (
+    clock : in std_logic;
+    hsync : out std_logic := '0';
+    vsync : out std_logic := '0';
+    inframe : out std_logic := '0';
+
+    red_o : out unsigned(7 downto 0) := x"00";
+    green_o : out unsigned(7 downto 0) := x"00";
+    blue_o : out unsigned(7 downto 0) := x"00";
+    
+    );
+
+end frame_generator;
+
+architecture brutalist of frame_generator is
+
+  signal x : integer := 0;
+  signal y : integer := 0;
+  
+begin
+
+  process (clock) is
+  begin
+
+    if rising_edge(clock) then
+      if x < frame_width then
+        x <= x + 1;
+      else
+        x <= 0;
+        if y < frame_height then
+          y <= y + 1;
+        else
+          y <= 0;
+        end if;
+      end if;
+
+      if x = hsync_start then
+        hsync <= '1';
+      end if;
+      if x = hsync_end then
+        hsync <= '0';
+      end if;
+      if y = vsync_start then
+        vsync <= '1';
+      end if;
+      if y = 0 then
+        vsync <= '0';
+      end if;
+
+      -- Draw white edge on frame
+      if x = 0 and y < vsync_start then
+        inframe <= '1';
+        red_o <= x"FF";
+        green_o <= x"FF";
+        blue_o <= x"FF";
+      end if;
+      if (x = ( display_width - 1 ))
+         or (y = 0) or (y = (display_height - 1)) then
+        red_o <= x"FF";
+        green_o <= x"FF";
+        blue_o <= x"FF";
+      end if;
+      if inframe = '1' then
+        -- Inside frame, draw a test pattern
+        red_o <= to_unsigned(x,8);
+        green_o <= to_unsigned(y,8);
+        blue_o <= to_unsigned(x+y,8);
+      end if;
+      -- Black outside of frame
+      if x = display_width then
+        inframe <= '0';
+        red_o <= x"00";
+        green_o <= x"00";
+        blue_o <= x"00";        
+      end if;
+    end if;
+
+  end process;
+  
+end brutalist;
---
+++ src/vhdl/lcdtest.vhdl
@@ -0,0 +1,218 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:30:37 12/10/2013 
+-- Design Name: 
+-- Module Name:    container - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use Std.TextIO.all;
+
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity container is
+  Port ( CLK_IN : STD_LOGIC;         
+         btnCpuReset : in  STD_LOGIC;
+--         irq : in  STD_LOGIC;
+--         nmi : in  STD_LOGIC;
+         
+         ----------------------------------------------------------------------
+         -- CIA1 ports for keyboard/joystick 
+         ----------------------------------------------------------------------
+--         porta_pins : inout  std_logic_vector(7 downto 0);
+--         portb_pins : inout  std_logic_vector(7 downto 0);
+         
+         ----------------------------------------------------------------------
+         -- VGA output
+         ----------------------------------------------------------------------
+         vsync : out STD_LOGIC;
+         hsync : out  STD_LOGIC;
+         vgared : buffer  UNSIGNED (3 downto 0);
+         vgagreen : buffer  UNSIGNED (3 downto 0);
+         vgablue : buffer  UNSIGNED (3 downto 0);
+
+         ----------------------------------------------------------------------
+         -- PS/2 keyboard interface
+         ----------------------------------------------------------------------
+         ps2clk : in std_logic;
+         ps2data : in std_logic;
+
+         jblo : inout std_logic_vector(4 downto 1) := (others => 'Z');
+         jbhi : inout std_logic_vector(10 downto 7) := (others => 'Z');
+         jalo : inout std_logic_vector(4 downto 1) := (others => 'Z');
+         jahi : inout std_logic_vector(10 downto 7) := (others => 'Z');
+         jdlo : inout std_logic_vector(4 downto 1) := (others => 'Z');
+         jdhi : inout std_logic_vector(10 downto 7) := (others => 'Z');
+         jclo : inout std_logic_vector(4 downto 1) := (others => 'Z');
+         jchi : inout std_logic_vector(10 downto 7) := (others => 'Z');
+                  
+         ----------------------------------------------------------------------
+         -- Debug interfaces on Nexys4 board
+         ----------------------------------------------------------------------
+         led : out std_logic_vector(15 downto 0) := (others => '1');
+         sw : in std_logic_vector(15 downto 0);
+         btn : in std_logic_vector(4 downto 0);
+
+         UART_TXD : out std_logic := '1';
+         RsRx : in std_logic;
+         
+         sseg_ca : out std_logic_vector(7 downto 0) := x"00";
+         sseg_an : out std_logic_vector(7 downto 0) := x"00"
+         );
+end container;
+
+architecture Behavioral of container is
+
+  component fpgatemp is
+    Generic ( DELAY_CYCLES : natural := 480 ); -- 10us @ 48 Mhz
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           temp : out  STD_LOGIC_VECTOR (11 downto 0));
+  end component;
+    
+  signal pixelclock : std_logic;
+  signal cpuclock : std_logic;
+  signal clock200 : std_logic;
+  signal clock40 : std_logic;
+  signal clock33 : std_logic;
+  signal clock30 : std_logic;
+
+  signal hsync_i : std_logic := '0';
+  signal vsync_i : std_logic := '0';
+  signal hsync_o : std_logic := '0';
+  signal vsync_o : std_logic := '0';
+  
+  signal segled_counter : unsigned(31 downto 0) := (others => '0');
+
+  signal lcd_clk_i : std_logic;
+  signal lcd_clk : std_logic;
+  signal lcd_hsync_i : std_logic;
+  signal lcd_vsync : std_logic;
+  signal lcd_hsync : std_logic;
+  signal lcd_vsync : std_logic;
+  signal lcd_de_i : std_logic;
+  signal lcd_de : std_logic;
+
+  signal red_i : unsigned(7 downto 0);
+  signal green_i : unsigned(7 downto 0);
+  signal blue_i : unsigned(7 downto 0);
+
+  signal red_o : unsigned(7 downto 0);
+  signal green_o : unsigned(7 downto 0);
+  signal blue_o : unsigned(7 downto 0);
+  
+begin
+  
+  dotclock1: entity work.dotclock100
+    port map ( clk_in1 => CLK_IN,
+               clock100 => pixelclock, -- 100MHz
+               clock50 => cpuclock, -- 50MHz
+               clock40 => clock40,
+               clock33 => clock33,
+               clock30 => clock30,
+               clock200 => clock200
+               );
+
+  frame50: entity work.frame_generator
+    generic map ( frame_width => 1065,
+                  display_width => 800,
+                  frame_height => 625,
+                  display_height => 600,
+                  vsync_start => 618,
+                  hsync_start => 921,
+                  hsync_end => 1033
+                  )                  
+    port map ( clock => clock33,
+               hsync => hsync_pal50,
+               vsync => vsync_pal50,
+               inframe => inframe_pal50,
+
+               -- Get test pattern
+               red_o => red_i,
+               green_o => green_i,
+               blue_o => blue_i
+               );
+               
+               
+  
+  pixel0: entity work.pixel_driver
+    port map (
+      pixelclock_select => sw(7 downto 0),
+      
+      clock200 => clock200,
+      clock100 => pixelclock,
+      clock50 => cpuclock,
+      clock40 => clock40,
+      clock33 => clock33,
+      clock30 => clock30,
+
+      red_i => red_,
+      green_i => green_i,
+      blue_i => blue_i,
+
+      red_o => red_o,
+      green_o => green_o,
+      blue_o => blue_o,
+
+      hsync_i => hsync_i,
+      hsync_o => hsync_o,
+      vsync_i => vsync_i,
+      vsync_o => vsync_o,
+
+      lcd_hsync_i => lcd_hsync_i,
+      lcd_hsync_o => lcd_hsync,
+      lcd_vsync_i => lcd_vsync_i,
+      lcd_vsync_o => lcd_vsync,
+
+      viciv_outofframe_i => oof_i,
+      viciv_outofframe_o => oof_o,
+      
+      lcd_display_enable_i => lcd_de_i,
+      lcd_display_enable_o => lcd_de_o,
+
+      lcd_pixel_strobe_i => lcd_clk_i,
+      lcd_pixel_strobe_o => lcd_clk_o
+
+      );
+
+  vgared <= red_o;
+  vgagreen <= green_o;
+  vgablue <= blue_o;
+    
+  jalo <= std_logic_vector(blue_o);
+  jahi <= std_logic_vector(red_o);
+  jblo <= std_logic_vector(green_o);
+  jbhi(7) <= lcd_clk;
+  jbhi(8) <= lcd_hsync;
+  jbhi(9) <= lcd_vsync;
+  jbhi(10) <= lcd_de;
+  
+  process (cpuclock)
+  begin
+    if rising_edge(cpuclock) then
+    end if;
+  end process;
+  
+end Behavioral;
