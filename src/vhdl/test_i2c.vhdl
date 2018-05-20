03a9f62d617399f81e10ecb3ce3e4e1d020b8b7f
Paul Gardner-Stephen <paul@servalproject.org>
2018-05-20 18:44:57 +0930

    refactor touch pad code out, and begin implementing calibrated reading

--- src/vhdl/test_i2c.vhdl
+++ src/vhdl/test_i2c.vhdl
@@ -11,59 +11,62 @@ architecture foo of test_i2c is
 
   signal clock50mhz : std_logic := '1';
 
-  signal i2c0_address : unsigned(6 downto 0) := to_unsigned(0,7);
-  signal i2c0_address_internal : unsigned(6 downto 0) := to_unsigned(0,7);
-  signal i2c0_rdata : unsigned(7 downto 0) := to_unsigned(0,8);
-  signal i2c0_wdata : unsigned(7 downto 0) := to_unsigned(0,8);
-  signal i2c0_wdata_internal : unsigned(7 downto 0) := to_unsigned(0,8);
-  signal i2c0_busy : std_logic := '0';
-  signal i2c0_busy_last : std_logic := '0';
-  signal i2c0_rw : std_logic := '0';
-  signal i2c0_rw_internal : std_logic := '0';
-  signal i2c0_error : std_logic := '0';  
-  signal i2c0_reset : std_logic := '1';
-  signal i2c0_reset_internal : std_logic := '1';
-  signal i2c0_command_en : std_logic := '0';  
-  signal i2c0_command_en_internal : std_logic := '0';  
-
   signal sda : std_logic := '1';
   signal scl : std_logic := '1';
   signal sda_last : std_logic := '1';
   signal scl_last : std_logic := '1';
-
-  signal last_busy : std_logic := '1';
-  signal busy_count : integer := 0;
-
-  subtype uint8 is unsigned(7 downto 0);
-  type byte_array is array (0 to 15) of uint8;
-  signal bytes : byte_array := (others => x"00");
-
-  signal touch_enabled : std_logic := '1';
 
   signal read_req : std_logic := '0';
   signal data_to_master : std_logic_vector(7 downto 0) := x"00";
   signal data_valid : std_logic := '0';
   signal data_from_master : std_logic_vector(7 downto 0) := x"00";
   signal next_value : integer := 0;
+
+  signal touch_enabled : std_logic := '1';
+  signal x_invert : std_logic := '0';
+  signal y_invert : std_logic := '0';
+  signal x_mult : unsigned(15 downto 0) := to_unsigned(256,16);
+  signal y_mult : unsigned(15 downto 0) := to_unsigned(256,16);
+  signal x_delta : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal y_delta : unsigned(15 downto 0) := to_unsigned(0,16);
+    
+    -- The touch events we have received
+  signal touch1_active : std_logic := '0';
+  signal touch1_status : std_logic_vector(1 downto 0) := "11";
+  signal x1 : unsigned(9 downto 0) := to_unsigned(0,10);
+  signal y1 : unsigned(9 downto 0) := to_unsigned(0,10);
+
+  signal touch2_active : std_logic := '0';
+  signal touch2_status : std_logic_vector(1 downto 0) := "11";
+  signal x2 : unsigned(9 downto 0) := to_unsigned(0,10);
+  signal y2 : unsigned(9 downto 0) := to_unsigned(0,10);
+ 
   
 begin
 
-
-  i2c0: entity work.i2c_master
+  touch0: entity work.touch
     port map (
-      clk => clock50mhz,
-      reset_n => i2c0_reset,
-      ena => i2c0_command_en,
-      addr => std_logic_vector(i2c0_address),
-      rw => i2c0_rw,
-      data_wr => std_logic_vector(i2c0_wdata),
-      busy => i2c0_busy,
-      unsigned(data_rd) => i2c0_rdata,
-      ack_error => i2c0_error,
+      clock50mhz => clock50mhz,
       sda => sda,
-      scl => scl
-      );
+      scl => scl,
+      touch_enabled => touch_enabled,
 
+      x_invert => x_invert,
+      y_invert => y_invert,
+      x_mult => x_mult,
+      y_mult => y_mult,
+
+      touch1_active => touch1_active,
+      touch1_status => touch1_status,
+      x1 => x1,
+      y1 => y1,
+
+      touch2_active => touch2_active,
+      touch2_status => touch2_status,
+      x2 => x2,
+      y2 => y2
+      );
+ 
   i2cslave: entity work.i2c_slave
     generic map (
       SLAVE_ADDR => "0111000"
@@ -116,50 +119,4 @@ begin
     end if;
   end process;
 
-  process (clock50mhz) is
-  begin
-    if rising_edge(clock50mhz) then
-      if i2c0_busy /= 'U' then
-        last_busy <= i2c0_busy;
-      end if;
---      report "busy=" & std_logic'image(i2c0_busy) & "last_busy = " & std_logic'image(last_busy);
-      if i2c0_busy='0' and last_busy='1' then
-        report "busy de-asserted: dispatching next command";
-        busy_count <= busy_count + 1;
-        case busy_count is
-          when 0 =>
-            if touch_enabled='1' then
-              report "Beginning touch panel scan";
-              -- send initial command
-              i2c0_command_en <= '1';
-              i2c0_address <= "0111000";  -- 0x70 = I2C address of touch panel
-              -- Write register zero to set starting point for read
-              i2c0_wdata <= x"00";
-              i2c0_rw <= '0';
-            else
-              busy_count <= 0;
-            end if;
-          when 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 =>
-            if i2c0_error='1' then
-              i2c0_command_en <= '0';
-              busy_count <= 0;
-              report "I2C error: Restarting job.";
-            else
-              i2c0_rw <= '1';
-              i2c0_command_en <= '1';
-            end if;
-            if busy_count>3 then
-              report "Setting byte(" & integer'image(busy_count - 4) & ") to $" & to_hstring(i2c0_rdata);
-              bytes(busy_count - 4) <= i2c0_rdata;
-            end if;
-          when others =>
-            report "Setting byte(" & integer'image(busy_count - 4) & ") to $" & to_hstring(i2c0_rdata);
-            bytes(busy_count - 4) <= i2c0_rdata;
-            i2c0_command_en <= '0';
-            busy_count <= 0;
-        end case;
-      end if;
-    end if;
-  end process;  
-  
 end foo;
---
+++ src/vhdl/touch.vhdl
@@ -0,0 +1,188 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use STD.textio.all;
+use work.debugtools.all;
+
+entity touch is
+  port (
+    clock50mhz : in std_logic;
+    sda : inout std_logic;
+    scl : inout std_logic;
+    touch_enabled : in std_logic := '1';
+
+    -- Calibration settings
+    -- XXX Doesn't cater for non-linearity, which we do see in
+    -- the horizontal axis.
+    x_invert : in std_logic := '0';
+    y_invert : in std_logic := '0';
+    x_mult : in unsigned(15 downto 0) := to_unsigned(256,16);
+    y_mult : in unsigned(15 downto 0) := to_unsigned(256,16);
+    x_delta : in unsigned(15 downto 0) := to_unsigned(0,16);
+    y_delta : in unsigned(15 downto 0) := to_unsigned(0,16);
+    
+    -- The touch events we have received
+    touch1_active : out std_logic := '0';
+    touch1_status : out std_logic_vector(1 downto 0) := "11";
+    x1 : out unsigned(9 downto 0) := to_unsigned(0,10);
+    y1 : out unsigned(9 downto 0) := to_unsigned(0,10);
+
+    touch2_active : out std_logic := '0';
+    touch2_status : out std_logic_vector(1 downto 0) := "11";
+    x2 : out unsigned(9 downto 0) := to_unsigned(0,10);
+    y2 : out unsigned(9 downto 0) := to_unsigned(0,10)
+    
+    );
+end entity;
+
+architecture foo of touch is
+
+  signal i2c0_address : unsigned(6 downto 0) := to_unsigned(0,7);
+  signal i2c0_address_internal : unsigned(6 downto 0) := to_unsigned(0,7);
+  signal i2c0_rdata : unsigned(7 downto 0) := to_unsigned(0,8);
+  signal i2c0_wdata : unsigned(7 downto 0) := to_unsigned(0,8);
+  signal i2c0_wdata_internal : unsigned(7 downto 0) := to_unsigned(0,8);
+  signal i2c0_busy : std_logic := '0';
+  signal i2c0_busy_last : std_logic := '0';
+  signal i2c0_rw : std_logic := '0';
+  signal i2c0_rw_internal : std_logic := '0';
+  signal i2c0_error : std_logic := '0';  
+  signal i2c0_reset : std_logic := '1';
+  signal i2c0_reset_internal : std_logic := '1';
+  signal i2c0_command_en : std_logic := '0';  
+  signal i2c0_command_en_internal : std_logic := '0';  
+
+  signal last_busy : std_logic := '1';
+  signal busy_count : integer := 0;
+
+  subtype uint8 is unsigned(7 downto 0);
+  type byte_array is array (0 to 15) of uint8;
+  signal bytes : byte_array := (others => x"00");
+
+  signal parse_touch : integer := 0;
+
+  signal x1_int : integer := 0;
+  signal x2_int : integer := 0;
+  signal y1_int : integer := 0;
+  signal y2_int : integer := 0;
+
+  signal x1_mult : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal x2_mult : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal y1_mult : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal y2_mult : unsigned(15 downto 0) := to_unsigned(0,16);
+  
+begin
+
+
+  i2c0: entity work.i2c_master
+    port map (
+      clk => clock50mhz,
+      reset_n => i2c0_reset,
+      ena => i2c0_command_en,
+      addr => std_logic_vector(i2c0_address),
+      rw => i2c0_rw,
+      data_wr => std_logic_vector(i2c0_wdata),
+      busy => i2c0_busy,
+      unsigned(data_rd) => i2c0_rdata,
+      ack_error => i2c0_error,
+      sda => sda,
+      scl => scl
+      );
+
+  process (clock50mhz) is
+    variable r : unsigned(15 downto 0) := to_unsigned(0,16);
+  begin
+    if rising_edge(clock50mhz) then
+      if i2c0_busy /= 'U' then
+        last_busy <= i2c0_busy;
+      end if;
+--      report "busy=" & std_logic'image(i2c0_busy) & "last_busy = " & std_logic'image(last_busy);
+      if i2c0_busy='0' and last_busy='1' then
+        report "busy de-asserted: dispatching next command";
+        busy_count <= busy_count + 1;
+        case busy_count is
+          when 0 =>
+            if touch_enabled='1' then
+              report "Beginning touch panel scan";
+              -- send initial command
+              i2c0_command_en <= '1';
+              i2c0_address <= "0111000";  -- 0x70 = I2C address of touch panel
+              -- Write register zero to set starting point for read
+              i2c0_wdata <= x"00";
+              i2c0_rw <= '0';
+            else
+              busy_count <= 0;
+            end if;
+          when 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 =>
+            if i2c0_error='1' then
+              i2c0_command_en <= '0';
+              busy_count <= 0;
+              report "I2C error: Restarting job.";
+            else
+              i2c0_rw <= '1';
+              i2c0_command_en <= '1';
+            end if;
+            if busy_count>3 then
+              report "Setting byte(" & integer'image(busy_count - 4) & ") to $" & to_hstring(i2c0_rdata);
+              bytes(busy_count - 4) <= i2c0_rdata;
+            end if;
+          when others =>
+            report "Setting byte(" & integer'image(busy_count - 4) & ") to $" & to_hstring(i2c0_rdata);
+            bytes(busy_count - 4) <= i2c0_rdata;
+            i2c0_command_en <= '0';
+            busy_count <= 0;
+            parse_touch <= 1;
+        end case;
+      end if;
+
+      case parse_touch is
+        when 0 =>
+          -- No touch event to parse
+          null;
+        when 1 =>
+          -- Begin parsing
+          if bytes(2) /= x"00" and bytes(3+2)(7 downto 4) /= x"f" then
+            if bytes(3+2)(7 downto 4) = "0001" then
+              touch1_status <= std_logic_vector(bytes(3+0)(7 downto 6));
+              x1_int <= to_integer(bytes(3+2)(3 downto 0) & bytes(3+3));
+              y1_int <= to_integer(bytes(3+0)(3 downto 0) & bytes(3+1));
+            elsif bytes(3+2)(7 downto 4) = "0010" then
+              touch2_status <= std_logic_vector(bytes(3+0)(7 downto 6));
+              x2_int <= to_integer(bytes(3+2)(3 downto 0) & bytes(3+3));
+              y2_int <= to_integer(bytes(3+0)(3 downto 0) & bytes(3+1));
+            end if;
+          end if;
+
+          if bytes(2) > x"01" and bytes(9+2)(7 downto 4) /= x"f" then
+            if bytes(3+2)(7 downto 4) = "0001" then
+              touch1_status <= std_logic_vector(bytes(9+0)(7 downto 6));
+              x1_int <= to_integer(bytes(9+2)(3 downto 0) & bytes(9+3));
+              y1_int <= to_integer(bytes(9+0)(3 downto 0) & bytes(9+1));
+            elsif bytes(3+2)(7 downto 4) = "0010" then
+              touch2_status <= std_logic_vector(bytes(9+0)(7 downto 6));
+              x2_int <= to_integer(bytes(9+2)(3 downto 0) & bytes(9+3));
+              y2_int <= to_integer(bytes(9+0)(3 downto 0) & bytes(9+1));
+            end if;
+          end if;
+          parse_touch <= 0;
+        when others =>
+          parse_touch <= 0;
+      end case;
+
+      -- We ignore the MSB, so that it is possible for the multiplier to
+      -- both scale up and down versus the input from the panel
+      x1_mult <= to_unsigned(x1_int * to_integer(x_mult),12+16)(26 downto 11);
+      x2_mult <= to_unsigned(x2_int * to_integer(x_mult),12+16)(26 downto 11);
+      y1_mult <= to_unsigned(y1_int * to_integer(y_mult),12+16)(26 downto 11);
+      y2_mult <= to_unsigned(y2_int * to_integer(y_mult),12+16)(26 downto 11);
+
+      r := x1_mult + x_delta; x1 <= r(15 downto 6);
+      r := x2_mult + x_delta; x2 <= r(15 downto 6);
+      r := y1_mult + y_delta; y1 <= r(15 downto 6);
+      r := y2_mult + y_delta; y2 <= r(15 downto 6);
+      
+      
+    end if;
+  end process;  
+  
+end foo;
