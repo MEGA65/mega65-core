d3590556637019bfa14f40916e997d876808f3ba
Paul Gardner-Stephen <paul@servalproject.org>
2018-05-30 10:31:01 +0930

    remove debug output

--- src/tools/videoproxy.c
+++ src/tools/videoproxy.c
@@ -143,7 +143,7 @@ int main(int argc,char **argv)
       const unsigned char *packet = pcap_next(descr,&hdr);
       if (packet) {
 	if (hdr.caplen == 2132) {
-	  printf("."); fflush(stdout);
+	  //	  printf("."); fflush(stdout);
 	  // probably a C65GS compressed video frame.
 	  if (client_sock!=-1) write(client_sock,packet,2132);
 	}
