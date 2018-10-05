910e2849ed8888496e748e359dc1bb761174144d
Paul Gardner-Stephen <paul@servalproject.org>
2018-10-05 17:38:07 +0930

    fix problem with disappearing matrix mode

--- src/vhdl/rain.vhdl
+++ src/vhdl/rain.vhdl
@@ -821,9 +821,12 @@ begin  -- rtl
             else
               vgablue_out <= x"00";
             end if;
-          elsif (row_counter >= (te_header_line_count + te_screen_height))
+          elsif (secure_mode_flag='0' and (row_counter = 3 or row_counter = 4)) then
             -- Hide the secure compartment instructions when not in secure mode
-              or (secure_mode_flag='0' and (row_counter = 3 or row_counter = 4)) then
+            vgared_out <= x"00";
+            vgagreen_out <= x"00";
+            vgablue_out <= x"00";
+          elsif (row_counter >= (te_header_line_count + te_screen_height)) then
             -- Beyond the end of display, so don't display text of overlay.
             vgared_out(7 downto 6) <= "00";
             vgagreen_out(7 downto 6) <= "00";
