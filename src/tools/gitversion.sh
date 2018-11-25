a07648795f6929460229950054446a6eafb0a639
Paul Gardner-Stephen <paul@servalproject.org>
2018-11-25 13:20:05 +1030

    fix more removal of brackets from git version string

--- src/tools/gitversion.sh
+++ src/tools/gitversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # status of 'B'ranch in 'S'hort format
-branch=`git status -b -s | head -n 1`
+branch=`git status -b -s | head -n 1 | sed -e 's/(//g' -e 's/)//g'`
 # get from charpos3, for 6 chars
 branch2=${branch:3:6}
 version=`git describe --always --abbrev=7 --dirty=+DIRTY | sed -e 's/(//g' -e 's/)//g'`
