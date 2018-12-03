3dc3e44abdc6109ca34841f36e68e389c4ac9430
Paul Gardner-Stephen <paul@servalproject.org>
2018-12-03 15:31:08 +1030

    considerable work on instructing timing program display

--- src/tests/instructiontiming.c
+++ src/tests/instructiontiming.c
@@ -24,6 +24,265 @@
 #define POKE(a,v) *((uint8_t *)a)=(uint8_t)v
 #define PEEK(a) ((uint8_t)(*((uint8_t *)a)))
 
+char *instruction_descriptions[256]={
+"BRK",
+"ORA ($nn,X)",
+"CLE",
+"SEE",
+"TSB $nn",
+"ORA $nn",
+"ASL $nn",
+"RMB0 $nn",
+"PHP",
+"ORA #$nn",
+"ASL A",
+"TSY",
+"TSB $nnnn",
+"ORA $nnnn",
+"ASL $nnnn",
+"BBR0 $nn,$rr",
+"BPL $rr",
+"ORA ($nn),Y",
+"ORA ($nn),Z",
+"BPL $rrrr",
+"TRB $nn",
+"ORA $nn,X",
+"ASL $nn,X",
+"RMB1 $nn",
+"CLC",
+"ORA $nnnn,Y",
+"INC",
+"INZ",
+"TRB $nnnn",
+"ORA $nnnn,X",
+"ASL $nnnn,X",
+"BBR1 $nn,$rr",
+"JSR $nnnn",
+"AND ($nn,X)",
+"JSR ($nnnn)",
+"JSR ($nnnn,X)",
+"BIT $nn",
+"AND $nn",
+"ROL $nn",
+"RMB2 $nn",
+"PLP",
+"AND #$nn",
+"ROL A",
+"TYS",
+"BIT $nnnn",
+"AND $nnnn",
+"ROL $nnnn",
+"BBR2 $nn,$rr",
+"BMI $rr",
+"AND ($nn),Y",
+"AND ($nn),Z",
+"BMI $rrrr",
+"BIT $nn,X",
+"AND $nn,X",
+"ROL $nn,X",
+"RMB3 $nn",
+"SEC",
+"AND $nnnn,Y",
+"DEC",
+"DEZ",
+"BIT $nnnn,X",
+"AND $nnnn,X",
+"ROL $nnnn,X",
+"BBR3 $nn,$rr",
+"RTI",
+"EOR ($nn,X)",
+"NEG",
+"ASR",
+"ASR $nn",
+"EOR $nn",
+"LSR $nn",
+"RMB4 $nn",
+"PHA",
+"EOR #$nn",
+"LSR A",
+"TAZ",
+"JMP $nnnn",
+"EOR $nnnn",
+"LSR $nnnn",
+"BBR4 $nn,$rr",
+"BVC $rr",
+"EOR ($nn),Y",
+"EOR ($nn),Z",
+"BVC $rrrr",
+"ASR $nn,X",
+"EOR $nn,X",
+"LSR $nn,X",
+"RMB5 $nn",
+"CLI",
+"EOR $nnnn,Y",
+"PHY",
+"TAB",
+"MAP",
+"EOR $nnnn,X",
+"LSR $nnnn,X",
+"BBR5 $nn,$rr",
+"RTS",
+"ADC ($nn,X)",
+"RTS #$nn",
+"BSR $rrrr",
+"STZ $nn",
+"ADC $nn",
+"ROR $nn",
+"RMB6 $nn",
+"PLA",
+"ADC #$nn",
+"ROR A",
+"TZA",
+"JMP ($nnnn)",
+"ADC $nnnn",
+"ROR $nnnn",
+"BBR6 $nn,$rr",
+"BVS $rr",
+"ADC ($nn),Y",
+"ADC ($nn),Z",
+"BVS $rrrr",
+"STZ $nn,X",
+"ADC $nn,X",
+"ROR $nn,X",
+"RMB7 $nn",
+"SEI",
+"ADC $nnnn,Y",
+"PLY",
+"TBA",
+"JMP ($nnnn,X)",
+"ADC $nnnn,X",
+"ROR $nnnn,X",
+"BBR7 $nn,$rr",
+"BRA $rr",
+"STA ($nn,X)",
+"STA ($nn,SP),Y",
+"BRA $rrrr",
+"STY $nn",
+"STA $nn",
+"STX $nn",
+"SMB0 $nn",
+"DEY",
+"BIT #$nn",
+"TXA",
+"STY $nnnn,X",
+"STY $nnnn",
+"STA $nnnn",
+"STX $nnnn",
+"BBS0 $nn,$rr",
+"BCC $rr",
+"STA ($nn),Y",
+"STA ($nn),Z",
+"BCC $rrrr",
+"STY $nn,X",
+"STA $nn,X",
+"STX $nn,Y",
+"SMB1 $nn",
+"TYA",
+"STA $nnnn,Y",
+"TXS",
+"STX $nnnn,Y",
+"STZ $nnnn",
+"STA $nnnn,X",
+"STZ $nnnn,X",
+"BBS1 $nn,$rr",
+"LDY #$nn",
+"LDA ($nn,X)",
+"LDX #$nn",
+"LDZ #$nn",
+"LDY $nn",
+"LDA $nn",
+"LDX $nn",
+"SMB2 $nn",
+"TAY",
+"LDA #$nn",
+"TAX",
+"LDZ $nnnn",
+"LDY $nnnn",
+"LDA $nnnn",
+"LDX $nnnn",
+"BBS2 $nn,$rr",
+"BCS $rr",
+"LDA ($nn),Y",
+"LDA ($nn),Z",
+"BCS $rrrr",
+"LDY $nn,X",
+"LDA $nn,X",
+"LDX $nn,Y",
+"SMB3 $nn",
+"CLV",
+"LDA $nnnn,Y",
+"TSX",
+"LDZ $nnnn,X",
+"LDY $nnnn,X",
+"LDA $nnnn,X",
+"LDX $nnnn,Y",
+"BBS3 $nn,$rr",
+"CPY #$nn",
+"CMP ($nn,X)",
+"CPZ #$nn",
+"DEW $nn",
+"CPY $nn",
+"CMP $nn",
+"DEC $nn",
+"SMB4 $nn",
+"INY",
+"CMP #$nn",
+"DEX",
+"ASW $nnnn",
+"CPY $nnnn",
+"CMP $nnnn",
+"DEC $nnnn",
+"BBS4 $nn,$rr",
+"BNE $rr",
+"CMP ($nn),Y",
+"CMP ($nn),Z",
+"BNE $rrrr",
+"CPZ $nn",
+"CMP $nn,X",
+"DEC $nn,X",
+"SMB5 $nn",
+"CLD",
+"CMP $nnnn,Y",
+"PHX",
+"PHZ",
+"CPZ $nnnn",
+"CMP $nnnn,X",
+"DEC $nnnn,X",
+"BBS5 $nn,$rr",
+"CPX #$nn",
+"SBC ($nn,X)",
+"LDA ($nn,SP),Y",
+"INW $nn",
+"CPX $nn",
+"SBC $nn",
+"INC $nn",
+"SMB6 $nn",
+"INX",
+"SBC #$nn",
+"EOM",
+"ROW $nnnn",
+"CPX $nnnn",
+"SBC $nnnn",
+"INC $nnnn",
+"BBS6 $nn,$rr",
+"BEQ $rr",
+"SBC ($nn),Y",
+"SBC ($nn),Z",
+"BEQ $rrrr",
+"PHW #$nnnn",
+"SBC $nn,X",
+"INC $nn,X",
+"SMB7 $nn",
+"SED",
+"SBC $nnnn,Y",
+"PLX",
+"PLZ",
+"PHW $nnnn",
+"SBC $nnnn,X",
+"INC $nnnn,X",
+"BBS7 $nn,$rr"
+};
+
 unsigned char opcode;
 
 // Measured cycle counts. Values are 256ths of a cycle
@@ -46,11 +305,13 @@ unsigned char colour;
 
 unsigned char selected_opcode=0;
 
+unsigned short addr;
+
 void indicate_display_mode(void)
 {
   printf("%c%c%c",0x13,0x11,0x11);
   if (display_mode==0) printf("Showing cycles per instruction         ");
-  if (display_mode==9) printf("Showing difference from expected cycles");
+  if (display_mode==1) printf("Showing difference from expected cycles");
   
 }
 
@@ -61,6 +322,35 @@ void main(void)
   indicate_display_mode();
   
   while(1) {
+
+    __asm__("jsr $ffe4");
+    __asm__("sta %v",v);
+    switch(v) {
+    case ' ': display_mode^=1; indicate_display_mode(); break;
+    case 0x11: case 0x91: case 0x1d: case 0x9d:
+      addr=0x0400 + 40 * 4 + selected_opcode + selected_opcode;
+      POKE(addr,PEEK(addr)&0x7f);
+      POKE(addr+1,PEEK(addr+1)&0x7f);
+      if (v==0x11) selected_opcode+=20;
+      if (v==0x91) selected_opcode-=20;
+      if (v==0x1d) selected_opcode+=1;
+      if (v==0x9d) selected_opcode-=1;
+      addr=0x0400 + 40 * 4 + selected_opcode + selected_opcode;
+      POKE(addr,PEEK(addr)|0x80);
+      POKE(addr+1,PEEK(addr+1)|0x80);
+
+      printf("\023\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"); // home cursor + 20x down
+      // blank row, and cursor back up onto it
+      addr=0x0400 + 40 * 21;
+      for(v=0;v<40;v++) POKE(addr+v,' ');
+      // Display expected cycle count
+      printf("                 %d cycles\n%c",expected_cycles_6502[selected_opcode]>>8,0x91);
+      // print the opcode description, and cursor back up
+      printf("$%02x %s\n",selected_opcode,instruction_descriptions[selected_opcode]);
+      
+      break;
+    }
+    
     if (!opcode) screen_addr=0x0400 + 40 * 4;
     
     expected_cycles=expected_cycles_6502[opcode];
@@ -142,3 +432,4 @@ void main(void)
     
   }
 }
+
