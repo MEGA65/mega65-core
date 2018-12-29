8190d341cba720121488abbe77a979f0d393a29e
Paul Gardner-Stephen <paul@servalproject.org>
2018-12-29 20:57:43 +1030

    begin displaying sprites at head of trails

--- src/tests/unicorns.c
+++ src/tests/unicorns.c
@@ -11,6 +11,8 @@
 #define POKE(a,v) *((uint8_t *)a)=(uint8_t)v
 #define PEEK(a) ((uint8_t)(*((uint8_t *)a)))
 
+#include "horses.h"
+
 unsigned short i;
 
 struct dmagic_dmalist {
@@ -112,9 +114,59 @@ void lfill(long destination_address, unsigned char value,
   return;
 }
 
+unsigned char flipped[256];
+
+unsigned char frame_count=0;
+
+unsigned char game_grid[80][50];
+unsigned char a,b;
+unsigned char player_x[4];
+unsigned char player_y[4];
+unsigned int player_tiles[4];
+unsigned int player_features[4];
+unsigned int player_animation_frame[4]; 
+#define FEATURE_FAST 0x01
+#define FEATURE_SUPERFAST 0x02
+
+void prepare_sprites(void)
+{
+  // Enable all sprites.
+  POKE(0xD015U,0xFF);
+  
+  // Make horizontally flipped sprites
+  a=0;
+  do {
+    flipped[a]=0;
+    if (a&0x01) flipped[a]^=0x80;
+    if (a&0x02) flipped[a]^=0x40;
+    if (a&0x04) flipped[a]^=0x20;
+    if (a&0x08) flipped[a]^=0x10;
+    if (a&0x10) flipped[a]^=0x08;
+    if (a&0x20) flipped[a]^=0x04;
+    if (a&0x40) flipped[a]^=0x02;
+    if (a&0x80) flipped[a]^=0x01;
+  } while(++a);
+
+  // Make horizontally flipped sprites
+  for(a=0;a<16;a++) {
+    for(b=0;b<21;b++) {
+      POKE(0xC000U+32*64
+	   +a*64+b*3+0,flipped[PEEK(0xC000U+a*64+b*3+2)]);
+      POKE(0xC000U+32*64
+	   +a*64+b*3+1,flipped[PEEK(0xC000U+a*64+b*3+1)]);
+      POKE(0xC000U+32*64
+	   +a*64+b*3+2,flipped[PEEK(0xC000U+a*64+b*3+0)]);
+    }
+  }
+
+  // XXX Now make 90 degree rotated versions
+  
+  
+}
 
 void videomode_game(void)
 {
+
   // Enable C65 VIC-III IO registers
   POKE(0xD02FU,0xA5);
   POKE(0xD02FU,0x96);
@@ -122,48 +174,10 @@ void videomode_game(void)
   // 80 column text mode, 3.5MHz CPU
   POKE(0xD031U,0xE0);
 
-  lpoke(0x8000U,1);
-  
-  // Clear screen
-  lfill(0x8000U,0x20,2000);
-  // Set colour of characters to yellow
-  lfill(0x1f800U,0x07,2000);
-
-  // Copy charrom to $A000
-  lcopy(0x29000U,0xa000U,2048);
-  // Patch char $1F to be half block (like char $62 in normal char ROM)
-  POKE(0xa000U+0x1f*8+0,0);
-  POKE(0xa000U+0x1f*8+1,0);
-  POKE(0xa000U+0x1f*8+2,0);
-  POKE(0xa000U+0x1f*8+3,0);
-  POKE(0xa000U+0x1f*8+4,0xff);
-  POKE(0xa000U+0x1f*8+5,0xff);
-  POKE(0xa000U+0x1f*8+6,0xff);
-  POKE(0xa000U+0x1f*8+7,0xff);
-  // Patch char $1E to be inverted half block
-  POKE(0xa000U+0x1e*8+0,0xff);
-  POKE(0xa000U+0x1e*8+1,0xff);
-  POKE(0xa000U+0x1e*8+2,0xff);
-  POKE(0xa000U+0x1e*8+3,0xff);
-  POKE(0xa000U+0x1e*8+4,0);
-  POKE(0xa000U+0x1e*8+5,0);
-  POKE(0xa000U+0x1e*8+6,0);
-  POKE(0xa000U+0x1e*8+7,0);
-  // Patch char $1D to be solid block
-  POKE(0xa000U+0x1d*8+0,0xff);
-  POKE(0xa000U+0x1d*8+1,0xff);
-  POKE(0xa000U+0x1d*8+2,0xff);
-  POKE(0xa000U+0x1d*8+3,0xff);
-  POKE(0xa000U+0x1d*8+4,0xff);
-  POKE(0xa000U+0x1d*8+5,0xff);
-  POKE(0xa000U+0x1d*8+6,0xff);
-  POKE(0xa000U+0x1d*8+7,0xff);
-
-  
-  // Put screen at $8000, get charrom from $A000
+  // Put screen at $F000, get charrom from $E800
   // Will be 2K for 80x25 text mode
-  POKE(0xDD00U,0x01);
-  POKE(0xD018U,0x08);
+  POKE(0xDD00U,0x00);
+  POKE(0xD018U,0xCB);
   
   // Pink border, black background
   POKE(0xD020U,0x0a);
@@ -176,7 +190,56 @@ void videomode_game(void)
   
   // Extended background colour mode
   // (so we can do fake 80x50 graphics mode with character 0x62 (half filled block)
-  POKE(0xD011U,0x5b);
+  POKE(0xD011U,0x5b);  
+  
+  // Clear screen
+  lfill(0xf000U,0x20,2000);
+  // Set colour of characters to yellow
+  lfill(0x1f800U,0x07,2000);
+
+  // $C000-$E7FF is free for sprites.
+  // This is $2800 bytes total = 10KB, enough for 160 sprites.
+  // We need 11 sprites for the horses facing each directions,
+  // so a total of 44 sprites.
+  // It would also be good to be able to make the horses hollow,
+  // and have 2nd sprite that overlays each frame to have a coloured
+  // body that matches the player's colour.
+  // So that makes 88 sprites, which easily fits
+  // We copy the sprites into place, and then make the horizontally flipped
+  // and 90 degree rotated versions
+  lcopy((long)&horse_sprites[0],0xC000U,sizeof(horse_sprites));  
+  prepare_sprites();
+  
+  // Copy charrom to $F000-$F7FF
+  lcopy(0x29000U,0xe800U,2048);
+  // Patch char $1F to be half block (like char $62 in normal char ROM)
+  POKE(0xe800U+0x1f*8+0,0);
+  POKE(0xe800U+0x1f*8+1,0);
+  POKE(0xe800U+0x1f*8+2,0);
+  POKE(0xe800U+0x1f*8+3,0);
+  POKE(0xe800U+0x1f*8+4,0xff);
+  POKE(0xe800U+0x1f*8+5,0xff);
+  POKE(0xe800U+0x1f*8+6,0xff);
+  POKE(0xe800U+0x1f*8+7,0xff);
+  // Patch char $1E to be inverted half block
+  POKE(0xe800U+0x1e*8+0,0xff);
+  POKE(0xe800U+0x1e*8+1,0xff);
+  POKE(0xe800U+0x1e*8+2,0xff);
+  POKE(0xe800U+0x1e*8+3,0xff);
+  POKE(0xe800U+0x1e*8+4,0);
+  POKE(0xe800U+0x1e*8+5,0);
+  POKE(0xe800U+0x1e*8+6,0);
+  POKE(0xe800U+0x1e*8+7,0);
+  // Patch char $1D to be solid block
+  POKE(0xe800U+0x1d*8+0,0xff);
+  POKE(0xe800U+0x1d*8+1,0xff);
+  POKE(0xe800U+0x1d*8+2,0xff);
+  POKE(0xe800U+0x1d*8+3,0xff);
+  POKE(0xe800U+0x1d*8+4,0xff);
+  POKE(0xe800U+0x1d*8+5,0xff);
+  POKE(0xe800U+0x1d*8+6,0xff);
+  POKE(0xe800U+0x1d*8+7,0xff);
+
   
 }
 
@@ -193,30 +256,19 @@ void draw_pixel_char(unsigned char x,unsigned char y,
   if (c_upper<4) {
     // Use half-char block with active pixels in bottom half.
     // Set colour of upper half using extended background
-    POKE(0x8000U+screen_offset,(c_upper<<6)+0x1f);
+    POKE(0xF000U+screen_offset,(c_upper<<6)+0x1f);
     lpoke(0x1f800U+screen_offset,colour_lookup[c_lower]);
   } else {
     if (c_lower<4) {
-      POKE(0x8000U+screen_offset,(c_lower<<6)+0x1e);
+      POKE(0xF000U+screen_offset,(c_lower<<6)+0x1e);
       lpoke(0x1f800U+screen_offset,colour_lookup[c_upper]);
     } else {
       // Both halves are the last colour, so just use a solid char
-      POKE(0x8000U+screen_offset,0x1d);
+      POKE(0xF000U+screen_offset,0x1d);
       lpoke(0x1f800U+screen_offset,colour_lookup[c_upper]);
     }
   }
 }
-
-unsigned char frame_count=0;
-
-unsigned char game_grid[80][50];
-unsigned char a,b;
-unsigned char player_x[4];
-unsigned char player_y[4];
-unsigned int player_tiles[4];
-unsigned int player_features[4];
-#define FEATURE_FAST 0x01
-#define FEATURE_SUPERFAST 0x02
 
 void redraw_game_grid(void)
 {
@@ -225,6 +277,9 @@ void redraw_game_grid(void)
     for(b=0;b<25;b++)
       draw_pixel_char(a,b,game_grid[a][b<<1],game_grid[a][1+(b<<1)]);
 }
+
+unsigned char sprite_y;
+unsigned short sprite_x;
 
 void main(void)
 {
@@ -236,7 +291,9 @@ void main(void)
     // Player initially has no tiles allocated
     player_tiles[a]=0;
     // Players have no special feature initially
-    player_features[a]=0;    
+    player_features[a]=0;
+    // Begin with unicorns in stationary pose
+    player_animation_frame[a]=11;
   }
   // Clear game grid
   for(a=0;a<80;a++)
@@ -281,6 +338,41 @@ void main(void)
 	  if (b&2) { if (player_y[a]<49) player_y[a]++; }
 	  if (b&4) { if (player_x[a]) player_x[a]--; }
 	  if (b&8) { if (player_x[a]<79) player_x[a]++; }
+	  if (b&0xf) {
+	    // Player is being moved, so update animation frame
+	    player_animation_frame[a]++;
+	    if (player_animation_frame[a]>10) player_animation_frame[a]=0;
+	  } else
+	    // Stationary player, so show standing unicorn
+	    player_animation_frame[a]=11;
+	  // Work out which direction, and from that, the position of the
+	  // sprite
+	  if (b&1) {
+	    // Moving up
+	  }
+	  if (b&2) {
+	    // Moving down
+	  }
+	  if (b&4) {
+	    // Moving left
+	    POKE(0xF7F8U+a,32+player_animation_frame[a]);      // outline sprite
+	    POKE(0xF7F8U+4+a,32+16+player_animation_frame[a]); // colour sprite
+	    sprite_x=8+player_x[a]*4;
+	    sprite_y=40+player_y[a]*4;
+	  }
+	  if (b&8) {
+	    // Moving right
+	    POKE(0xF7F8U+a,player_animation_frame[a]);      // outline sprite
+	    POKE(0xF7F8U+4+a,16+player_animation_frame[a]); // colour sprite
+	    sprite_x=19+player_x[a]*4;
+	    sprite_y=40+player_y[a]*4;
+	  }
+	  POKE(0xD000U+a*2,sprite_x&0xff);
+	  POKE(0xD001U+a*2,sprite_y);
+	  POKE(0xD008U+a*2,sprite_x&0xff);
+	  POKE(0xD009U+a*2,sprite_y);
+	  if (sprite_x&0x100) POKE(0xD010U,PEEK(0xD010U)|(0x11<<a));
+	  else POKE(0xD010U,PEEK(0xD010U)&(0xFF-(0x11<<a)));
 	}
 
       // Leave unicorn rainbow trail behind us
