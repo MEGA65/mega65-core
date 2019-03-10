178f4d28063b1b69ac5c5faafb33fd94a9de2340
Falk Rehwagen <frehwagen@bluewaysw.de>
2019-03-10 19:12:30 +0100

    Fix that only one pixel is skiped in column 0.

--- src/vhdl/bitplanes.vhdl
+++ src/vhdl/bitplanes.vhdl
@@ -396,7 +396,9 @@ begin  -- behavioural
         and (y_top='1' or bitplane_drawing='1') then
         x_left <= '1';
         x_in_bitplanes <= '1';
-        bitplanes_advance_pixel <= "11111111";
+        if (v_x_in /= x_last)
+          bitplanes_advance_pixel <= "11111111";
+        end if;
         report "asserting x_left and x_in_bitplanes";
       else
         x_left <= '0';
