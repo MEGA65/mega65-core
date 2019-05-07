5b142608573363f323cd2a330788195755f8741f
Paul Gardner-Stephen <paul@servalproject.org>
2019-05-07 17:27:43 +0930

    get the i2clist program minimally running.

--- src/utilities/i2clist.c
+++ src/utilities/i2clist.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
 
-const volatile unsigned char *i2c_master=(unsigned char *)0xd6d0L;
+volatile unsigned char *i2c_master=(unsigned char *)0xd6d0L;
 
-int main(void)
+unsigned char bus,addr;
+
+void probe_address(const unsigned char bus,const unsigned char addr)
 {
-  printf("Hello world\r");
+
+  // Set up bus and address
+  i2c_master[0]=bus;
+  i2c_master[2]=addr;
+
+  // Reset I2C bus
+  i2c_master[1]=0x08;
+  i2c_master[1]=0x09;
+
+  __asm__("inc $0410");  
+  
+#pragma optimize(push, off)
+  // Wait for bus to go ready?
+  do {
+    if ((i2c_master[1]&0xc7)==0x01) break;
+    *(unsigned char *)0x0400 = i2c_master[1];
+  } while(1);
+
+  __asm__("inc $0411");  
+  
+  // Ask bus to do something?
+  i2c_master[1]=0x0f;
+
+  // Wait for busy to assert
+  //  while(!(i2c_master[1]&0x40)) continue;
+
+  __asm__("inc $0412");  
+  
+  // Wait for error flag to clear
+  //  while((i2c_master[1]&0x80)) continue;
+
+  __asm__("inc $0413");  
+  
+  // Wait for busy to clear
+  while((i2c_master[1]&0x40)) continue;
+
+  __asm__("inc $0414");  
+  
+  // Check success by checking error bit
+  if (i2c_master[1]&0x80) {
+    // Error -- so do no more on this address;
+    __asm__("inc $0415");  
+    return;
+  }
+#pragma optimize(pop)
+
+  printf("  found device $%02x (%d)\n",addr,addr);
+}
+
+
+void main(void)
+{
+#pragma optimize(push,off)
+  *(unsigned char *)0xD02fL=0x47;
+  *(unsigned char *)0xD02fL=0x53;
+#pragma optimize(pop)
+  
+  for(bus=1;bus<2;bus++)
+    {
+      printf("Scanning bus %d\n",bus);
+      addr=0;
+      do {
+	probe_address(bus,addr);
+	addr+=2;
+      } while(addr);
+    }
 }
