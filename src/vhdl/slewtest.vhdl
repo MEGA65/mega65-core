0c297b4453bc7fcd2e3db59fa6ef832b1f724840
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-05 17:04:52 +0930

    add LCD test pattern output as begining of debugging LCD brightness problem.

--- src/vhdl/slewtest.vhdl
+++ src/vhdl/slewtest.vhdl
@@ -41,13 +41,30 @@ entity container is
          ----------------------------------------------------------------------
          -- PMODs for LCD screen and associated things during testing
          ----------------------------------------------------------------------
-         lcd_dclk : out std_logic := '0'
-         
+
+         power_down : out std_logic := '0';
+
+         ----------------------------------------------------------------------
+         -- LCD output
+         ----------------------------------------------------------------------
+         lcd_vsync : out STD_LOGIC;
+         lcd_hsync : out  STD_LOGIC;
+         lcd_display_enable : out std_logic;
+         lcd_pwm : out std_logic;
+         lcd_dclk : out std_logic;
+         lcd_red : out  UNSIGNED (5 downto 0);
+         lcd_green : out  UNSIGNED (5 downto 0);
+         lcd_blue : out  UNSIGNED (5 downto 0)
+      
          );
 end container;
 
 architecture Behavioral of container is
 
+  signal dummy_red : unsigned(1 downto 0);
+  signal dummy_green : unsigned(1 downto 0);
+  signal dummy_blue : unsigned(1 downto 0);
+  
   signal pixelclock : std_logic;
   signal cpuclock : std_logic;
   signal clock240 : std_logic;
@@ -60,6 +77,8 @@ architecture Behavioral of container is
 
   signal pal50_select : std_logic := '0';
 
+  signal pixel_strobe : std_logic := '0';
+  
 begin
   
   dotclock1: entity work.dotclock100
@@ -80,6 +99,53 @@ begin
    (O => clock30,
     I => clock30in);
 
+  pixel0: entity work.pixel_driver
+    port map (
+      clock80 => pixelclock,
+      clock120 => clock120,
+      clock240 => clock240,
+
+      pixel_strobe80_out => pixel_strobe,
+      
+      -- Configuration information from the VIC-IV
+      hsync_invert => '0',
+      vsync_invert => '0',
+      pal50_select =>  pal50_select,
+      test_pattern_enable => '1',
+      
+      -- Framing information for VIC-IV
+--      x_zero => external_frame_x_zero,     
+--      y_zero => external_frame_y_zero,     
+
+      -- Pixel data from the video pipeline
+      -- (clocked at 100MHz pixel clock)
+      pixel_strobe_in => pixel_strobe,
+      red_i => x"FF",
+      green_i => x"FF",
+      blue_i => x"FF",
+
+      -- The pixel for direct output to VGA pins
+      -- It is clocked at the correct pixel
+      red_o(7 downto 2) => lcd_red,
+      red_o(1 downto 0) => dummy_red,
+      green_o(7 downto 2) => lcd_green,
+      green_o(1 downto 0) => dummy_green,
+      blue_o(7 downto 2) => lcd_blue,      
+      blue_o(1 downto 0) => dummy_blue,
+--      hsync => hsync,
+--      vsync => vsync,
+
+      -- And the variations on those signals for the LCD display
+      lcd_hsync => lcd_hsync,
+      lcd_vsync => lcd_vsync,
+      lcd_display_enable => lcd_display_enable
+--      lcd_inletterbox => lcd_in_letterbox,
+--      inframe => vga_in_frame,
+--      lcd_inframe => lcd_in_frame
+
+      );
+
+  
   lcd_dclk <= clock30 when pal50_select='1' else cpuclock;
   
   process (clock240,pal50_select,clock30,clock30in)
