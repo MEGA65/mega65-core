a3ce86960efffdd2fdb3b7ca5bc9ac1eb882b641
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-22 17:45:13 +0200

    begin implementing simple test target for M65R2 PCB debugging

---
+++ src/vhdl/testr2.vhdl
@@ -0,0 +1,345 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:30:37 12/10/2013 
+-- Design Name: 
+-- Module Name:    container - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use Std.TextIO.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity container is
+  Port ( CLK_IN : STD_LOGIC;         
+         btnCpuReset : in  STD_LOGIC;
+--         irq : in  STD_LOGIC;
+--         nmi : in  STD_LOGIC;
+         
+         ----------------------------------------------------------------------
+         -- CIA1 ports for keyboard/joystick 
+         ----------------------------------------------------------------------
+         -- XXX Replace with new 8-pin keyboard interface
+--         restore_key : in std_logic;
+--         column : inout  std_logic_vector(8 downto 0) := (others => 'Z');
+--         row : in  std_logic_vector(8 downto 0);
+--         keyleft : inout std_logic := 'Z';
+--         keyup : inout std_logic := 'Z';
+         
+         fa_left : in std_logic;
+         fa_right : in std_logic;
+         fa_up : in std_logic;
+         fa_down : in std_logic;
+         fa_fire : in std_logic;
+         fb_left : in std_logic;
+         fb_right : in std_logic;
+         fb_up : in std_logic;
+         fb_down : in std_logic;
+         fb_fire : in std_logic;
+
+         ----------------------------------------------------------------------
+         -- Expansion/cartridge port
+         ----------------------------------------------------------------------
+         cart_ctrl_dir : out std_logic;
+         cart_haddr_dir : out std_logic;
+         cart_laddr_dir : out std_logic;
+         cart_data_en : out std_logic;
+         cart_addr_en : out std_logic;
+         cart_data_dir : out std_logic;
+         cart_phi2 : out std_logic;
+         cart_dotclock : out std_logic;
+         cart_reset : out std_logic;
+
+         cart_nmi : in std_logic;
+         cart_irq : in std_logic;
+         cart_dma : in std_logic;
+
+         cart_exrom : inout std_logic := 'Z';
+         cart_ba : inout std_logic := 'Z';
+         cart_rw : inout std_logic := 'Z';
+         cart_roml : inout std_logic := 'Z';
+         cart_romh : inout std_logic := 'Z';
+         cart_io1 : inout std_logic := 'Z';
+         cart_game : inout std_logic := 'Z';
+         cart_io2 : inout std_logic := 'Z';
+
+         cart_d : inout unsigned(7 downto 0) := (others => 'Z');
+         cart_a : inout unsigned(15 downto 0) := (others => 'Z');
+         
+         ----------------------------------------------------------------------
+         -- CBM floppy serial port
+         ----------------------------------------------------------------------
+         iec_clk_en : out std_logic;
+         iec_data_en : out std_logic;
+         iec_data_o : out std_logic;
+         iec_reset : out std_logic;
+         iec_clk_o : out std_logic;
+         iec_data_i : in std_logic;
+         iec_clk_i : in std_logic;
+         iec_atn : out std_logic;
+         
+         ----------------------------------------------------------------------
+         -- VGA output
+         ----------------------------------------------------------------------
+         vdac_clk : out std_logic;
+         vdac_sync_n : out std_logic; -- tie low
+         vdac_blank_n : out std_logic; -- tie high
+         vsync : out  STD_LOGIC;
+         hsync : out  STD_LOGIC;
+         vgared : out  UNSIGNED (7 downto 0);
+         vgagreen : out  UNSIGNED (7 downto 0);
+         vgablue : out  UNSIGNED (7 downto 0);
+
+         hdmi_vsync : out  STD_LOGIC;
+         hdmi_hsync : out  STD_LOGIC;
+         hdmired : out  UNSIGNED (7 downto 0);
+         hdmigreen : out  UNSIGNED (7 downto 0);
+         hdmiblue : out  UNSIGNED (7 downto 0);
+         hdmi_spdif : in std_logic;
+         hdmi_spdif_out : out std_logic;
+         hdmi_scl : inout std_logic;
+         hdmi_sda : inout std_logic;
+         hdmi_de : out std_logic; -- high when valid pixels being output
+         -- (i.e., when hsync, vsync both low?)
+         
+         
+         ---------------------------------------------------------------------------
+         -- IO lines to the ethernet controller
+         ---------------------------------------------------------------------------
+         eth_mdio : inout std_logic;
+         eth_mdc : out std_logic;
+         eth_reset : out std_logic;
+         eth_rxd : in unsigned(1 downto 0);
+         eth_txd : out unsigned(1 downto 0);
+         eth_rxer : in std_logic;
+         eth_txen : out std_logic;
+         eth_rxdv : in std_logic;
+--         eth_interrupt : in std_logic;
+         eth_clock : out std_logic;
+         
+         -------------------------------------------------------------------------
+         -- Lines for the SDcard interface itself
+         -------------------------------------------------------------------------
+         sdReset : out std_logic := '0';  -- must be 0 to power SD controller (cs_bo)
+         sdClock : out std_logic;       -- (sclk_o)
+         sdMOSI : out std_logic;      
+         sdMISO : in  std_logic;
+
+         sd2MOSI : out std_logic;
+         sd2MISO : in std_logic;
+
+         -- Left and right audio
+         pwm_l : out std_logic;
+         pwm_r : out std_logic;
+         
+         ----------------------------------------------------------------------
+         -- Floppy drive interface
+         ----------------------------------------------------------------------
+         f_density : out std_logic := '1';
+         f_motor : out std_logic := '1';
+         f_select : out std_logic := '1';
+         f_stepdir : out std_logic := '1';
+         f_step : out std_logic := '1';
+         f_wdata : out std_logic := '1';
+         f_wgate : out std_logic := '1';
+         f_side1 : out std_logic := '1';
+         f_index : in std_logic;
+         f_track0 : in std_logic;
+         f_writeprotect : in std_logic;
+         f_rdata : in std_logic;
+         f_diskchanged : in std_logic;
+
+         led : out std_logic;
+         
+         ----------------------------------------------------------------------
+         -- Debug interfaces on Nexys4 board
+         ----------------------------------------------------------------------
+         UART_TXD : out std_logic;
+         RsRx : in std_logic
+         
+         );
+end container;
+
+architecture Behavioral of container is
+  
+  signal irq : std_logic := '1';
+  signal nmi : std_logic := '1';
+  signal reset_out : std_logic := '1';
+  signal cpu_game : std_logic := '1';
+  signal cpu_exrom : std_logic := '1';
+
+  signal pixelclock : std_logic;
+  signal ethclock : std_logic;
+  signal cpuclock : std_logic;
+  signal clock40 : std_logic;
+  signal clock120 : std_logic;
+  signal clock100 : std_logic := '0';
+  signal clock200 : std_logic;
+  signal clock240 : std_logic;
+
+  -- XXX Actually connect to new keyboard
+  signal restore_key : std_logic := '1';
+  signal column : std_logic_vector(8 downto 0) := (others => '1');
+  signal row : std_logic_vector(8 downto 0) := (others => '1');
+  signal keyleft : std_logic := '1';
+  signal keyup : std_logic := '1';
+  
+  
+  signal segled_counter : unsigned(31 downto 0) := (others => '0');
+
+  signal slow_access_request_toggle : std_logic;
+  signal slow_access_ready_toggle : std_logic;
+  signal slow_access_write : std_logic;
+  signal slow_access_address : unsigned(27 downto 0);
+  signal slow_access_wdata : unsigned(7 downto 0);
+  signal slow_access_rdata : unsigned(7 downto 0);
+  signal sector_buffer_mapped : std_logic;  
+
+  signal pmoda_dummy :  std_logic_vector(7 downto 0) := (others => '1');
+
+  signal v_hsync : std_logic;
+  signal v_vsync : std_logic;
+  signal v_red : unsigned(7 downto 0);
+  signal v_green : unsigned(7 downto 0);
+  signal v_blue : unsigned(7 downto 0);
+  signal v_de : std_logic;
+  
+  -- XXX We should read the real temperature and feed this to the DDR controller
+  -- so that it can update timing whenever the temperature changes too much.
+  signal fpga_temperature : std_logic_vector(11 downto 0) := (others => '0');
+
+  -- XXX Connect to real QSPI flash interface at some point
+  signal QspiDB : std_logic_vector(3 downto 0) := (others => '0');
+  signal QspiCSn : std_logic := '1';
+
+  signal fa_left_drive : std_logic;
+  signal fa_right_drive : std_logic;
+  signal fa_up_drive : std_logic;
+  signal fa_down_drive : std_logic;
+  signal fa_fire_drive : std_logic;
+  
+  signal fb_left_drive : std_logic;
+  signal fb_right_drive : std_logic;
+  signal fb_up_drive : std_logic;
+  signal fb_down_drive : std_logic;
+  signal fb_fire_drive : std_logic;
+
+  signal fa_potx : std_logic;
+  signal fa_poty : std_logic;
+  signal fb_potx : std_logic;
+  signal fb_poty : std_logic;
+  signal pot_drain : std_logic;
+
+  signal pot_via_iec : std_logic;
+  
+  signal iec_clk_en_drive : std_logic;
+  signal iec_data_en_drive : std_logic;
+  signal iec_data_o_drive : std_logic;
+  signal iec_reset_drive : std_logic;
+  signal iec_clk_o_drive : std_logic;
+  signal iec_data_i_drive : std_logic;
+  signal iec_clk_i_drive : std_logic;
+  signal iec_atn_drive : std_logic;
+
+  signal pwm_l_drive : std_logic;
+  signal pwm_r_drive : std_logic;
+
+  signal flopled_drive : std_logic;
+  signal flopmotor_drive : std_logic;
+
+  signal joy3 : std_logic_vector(4 downto 0);
+  signal joy4 : std_logic_vector(4 downto 0);
+
+  signal cart_access_count : unsigned(7 downto 0);
+  
+begin
+
+  dotclock1: entity work.dotclock100
+    port map ( clk_in1 => CLK_IN,
+               clock80 => pixelclock, -- 80MHz
+               clock40 => cpuclock, -- 40MHz
+               clock50 => ethclock,
+               clock100 => clock100,
+               clock120 => clock120,
+               clock240 => clock240
+               );
+
+  process (pixelclock) is
+  begin
+    vdac_sync_n <= '0';  -- no sync on green
+    vdac_blank_n <= '1'; -- was: not (v_hsync or v_vsync); 
+
+    -- VGA output at full pixel clock
+    vdac_clk <= pixelclock;
+
+    -- Ethernet clock at 50MHz
+    eth_clock <= ethclock;
+
+    -- Drive most ports, to relax timing
+    if rising_edge(cpuclock) then
+
+      -- Connect UART RX and TX
+      uart_txd <= rsrx;
+      
+      fa_left_drive <= fa_left;
+      fa_right_drive <= fa_right;
+      fa_up_drive <= fa_up;
+      fa_down_drive <= fa_down;
+      fa_fire_drive <= fa_fire;  
+      fb_left_drive <= fb_left;
+      fb_right_drive <= fb_right;
+      fb_up_drive <= fb_up;
+      fb_down_drive <= fb_down;
+      fb_fire_drive <= fb_fire;  
+
+    end if;
+
+    if rising_edge(pixelclock) then
+      hsync <= v_hsync;
+      vsync <= v_vsync;
+      vgared <= v_red;
+      vgagreen <= v_green;
+      vgablue <= v_blue;
+    end if;
+    
+    if rising_edge(pixelclock) then
+
+      hdmi_hsync <= v_hsync;
+      hdmi_vsync <= v_vsync;
+      hdmired <= v_red;
+      hdmigreen <= v_green;
+      hdmiblue <= v_blue;
+      -- pixels valid only when neither sync signal is asserted
+      hdmi_de <= not (v_hsync or v_vsync);
+      -- no hdmi audio yet
+      hdmi_spdif_out <= 'Z';
+      -- HDMI control interface
+      -- XXX We need to send some commands via I2C to configure the HDMI
+      -- interface, which we don't yet do, so HDMI output will not yet work.
+--      hdmi_scl <= hdmi_scl;
+--      hdmi_sda <= hdmi_sda;
+    end if;
+  end process;    
+  
+end Behavioral;
