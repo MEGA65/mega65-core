cb73989fb7d323528defb5eb8aced4c266f03ddd
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-25 19:56:22 +0200

    tweak M65R2 PCB test program

--- vivado/testr2_gen.tcl
+++ vivado/testr2_gen.tcl
@@ -179,6 +179,7 @@ set files [list \
  "[file normalize "$origin_dir/src/vhdl/uart_charrom.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/ddrwrapper.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/testr2.vhdl"]"\
+ "[file normalize "$origin_dir/src/vhdl/pin_id.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/keyboard_complex.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/virtual_to_matrix.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/keyboard_to_matrix.vhdl"]"\
