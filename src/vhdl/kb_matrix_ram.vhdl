791e4610c62875a0d2557c37b24561a41e402e80
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-27 22:43:09 +0200

    add debug info

--- src/vhdl/kb_matrix_ram.vhdl
+++ src/vhdl/kb_matrix_ram.vhdl
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
+use work.debugtools.all;
 
 --
 entity kb_matrix_ram is
@@ -27,13 +28,15 @@ begin
       for i in 0 to 7 loop
         if wea(i) = '1' then
           ram(addressa)(i) := dia(i);
+          report "Writing bit " & integer'image(i) & " of byte " & integer'image(addressa) & " with " & std_logic'image(dia(i));
         end if;
       end loop;
     end if;
   END PROCESS;
 PROCESS(addressb)
 BEGIN
-      dob <= ram(addressb);
+  dob <= ram(addressb);
+  report "Reading byte " & integer'image(addressb) & " with value $" & to_hstring(ram(addressb));
 END PROCESS;
 
 end Behavioral;
