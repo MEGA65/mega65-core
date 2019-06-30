c99ce4dc965e1189095e3880daa0f12b7d44f451
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-30 22:35:21 +0200

    fix missing right edge pixel when sprites are not horizontally expanded

--- src/vhdl/sprite.vhdl
+++ src/vhdl/sprite.vhdl
@@ -407,10 +407,10 @@ begin  -- behavioural
             
             ((sprite_stretch_x='0') and
             (
-              ((x_offset = (23-1)) and (sprite_extended_width_enable='0'))
-              or ((x_offset = (63-1)) and (sprite_extended_width_enable='1'))
+              ((x_offset = (23)) and (sprite_extended_width_enable='0'))
+              or ((x_offset = (63)) and (sprite_extended_width_enable='1'))
               -- 16 colour sprites are always 16 pixels wide
-              or ((x_offset = (15-1)) and (sprite_sixteen_colour_mode='1'))))
+              or ((x_offset = (15)) and (sprite_sixteen_colour_mode='1'))))
             )
             
             and (sprite_horizontal_tile_enable='0')
