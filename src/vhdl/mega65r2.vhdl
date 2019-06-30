f7c31ab6bef765c82f310aa7b7127395944794e3
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-30 23:20:25 +0200

    try to find cause of hr_clk_p pin not assignable error

--- src/vhdl/mega65r2.vhdl
+++ src/vhdl/mega65r2.vhdl
@@ -94,7 +94,7 @@ entity container is
          hr_d : inout unsigned(7 downto 0);
          hr_rwds : inout std_logic;
          hr_reset : out std_logic;
-         hr_clk_p : out std_logic;
+--         hr_clk_p : out std_logic;
          hr_cs0 : out std_logic;
          
          ----------------------------------------------------------------------
@@ -195,6 +195,8 @@ entity container is
 end container;
 
 architecture Behavioral of container is
+
+  signal hr_clk_p : std_logic;
   
   signal irq : std_logic := '1';
   signal nmi : std_logic := '1';
