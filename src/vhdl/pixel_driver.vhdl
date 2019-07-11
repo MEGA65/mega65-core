241afa4040f9347ec6e25aa3bfa3b92865ccf266
Paul Gardner-Stephen <paul@servalproject.org>
2019-07-11 19:43:33 +0930

    further tweaking video mode parameters.

--- src/vhdl/pixel_driver.vhdl
+++ src/vhdl/pixel_driver.vhdl
@@ -207,10 +207,10 @@ begin
                   frame_height => 624,        -- 312 lines x 2 fields
                   pipeline_delay => 128,
                   display_height => 600,
-                  vsync_start => 624-36-5,
-                  vsync_end => 624-36,
-                  hsync_start => (968-64-46-1)*4,
-                  hsync_end => (968-64-1)*4
+                  vsync_start => 624-64-5,
+                  vsync_end => 624-64,
+                  hsync_start => (968-48-46-1)*4,
+                  hsync_end => (968-48-1)*4
                   )                  
     port map ( clock120 => clock120,
                clock240 => clock240,
@@ -245,10 +245,10 @@ begin
                   frame_height => 526,       -- NTSC frame is 263 lines x 2 frames
                   display_height => 526-4,
                   pipeline_delay => 96,
-                  vsync_start => 526-4,
-                  vsync_end => 526,
-                  hsync_start => 860*3,
-                  hsync_end => 20*3
+                  vsync_start => 526-10-4,
+                  vsync_end => 526-10,
+                  hsync_start => 850*3,
+                  hsync_end => 10*3
                   )                  
     port map ( clock120 => clock120,
                clock240 => clock240,
