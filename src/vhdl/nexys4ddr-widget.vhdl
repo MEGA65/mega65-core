a918cd251df2db15bb8fd0f08488b05f92ac97d9
Paul Gardner-Stephen <paul@servalproject.org>
2019-08-02 05:18:07 +0930

    add missing enum definition for more targets.

--- src/vhdl/nexys4ddr-widget.vhdl
+++ src/vhdl/nexys4ddr-widget.vhdl
@@ -21,7 +21,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 use Std.TextIO.all;
-
+use work.cputypes.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
