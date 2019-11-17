b87b0b0554941e502794e0599cd2da61c985ede0
Paul Gardner-Stephen <paul@servalproject.org>
2019-11-17 14:20:06 +1030

    add missing I2C registers for SPDIF audio setup.

--- zed_hdmi_720p/i2c_sender.vhd
+++ zed_hdmi_720p/i2c_sender.vhd
@@ -70,6 +70,10 @@ architecture Behavioral of i2c_sender is
             -- Audio setup
             ---------------
             x"0A10",  -- SPDIF audio format
+            x"0B80",  -- SPDIF audio TX enable, extract MCLK from SPDIF audio
+                      -- stream, i.e no separate MCLK
+            x"0C00",  -- Use sampling rate encoded in the SPDIF stream instead
+                      -- of specifying the rate.
             
             -- Set HDMI device name
             x"1F80",x"4478", -- Allow setting HDMI packet memory
