a7a36a86771e4d86b0e022ec36a82a1b1e441d2e
Paul Gardner-Stephen <paul@servalproject.org>
2019-11-19 04:17:38 +1030

    add missing use statements. #138

--- spdif_out/spdf_out.vhd
+++ spdif_out/spdf_out.vhd
@@ -9,6 +9,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 entity spdf_out is
     Port ( clk : in  STD_LOGIC;
--- src/vhdl/hdmi_spdif.vhdl
+++ src/vhdl/hdmi_spdif.vhdl
@@ -9,6 +9,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 entity hdmi_spdif is
   Port ( clk : in  STD_LOGIC;
