0ff9ccc43368e4c1898036c0dc65a4c0d624e29f
Paul Gardner-Stephen <paul@servalproject.org>
2019-11-19 23:20:22 +1030

    Reduce HDMI audio volume 16x. #138

--- src/vhdl/hdmi_spdif.vhdl
+++ src/vhdl/hdmi_spdif.vhdl
@@ -50,10 +50,12 @@ begin
   process (clk) is
   begin
     if rising_edge(clk) then
+      -- If we put the sample it the top 20 bits, then it is REALLY
+      -- loud and distorts.  So put it 4 bits down.
       if sample_channel = '0' then
-        sample_32(31 downto 12) <= left_in;
+        sample_32(27 downto 8) <= left_in;
       else
-        sample_32(31 downto 12) <= right_in;
+        sample_32(27 downto 8) <= right_in;
       end if;
     end if;
   end process;
