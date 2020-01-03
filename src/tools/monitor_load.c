99543847bcc82c8c5c853e019176842636cca25a
Paul Gardner-Stephen <paul@servalproject.org>
2020-01-03 11:01:55 +1030

    begin implementing PETSCII funny chars. #154

--- src/tools/monitor_load.c
+++ src/tools/monitor_load.c
@@ -44,7 +44,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sys/time.h>
 #include <errno.h>
 #include <getopt.h>
-
+#include <inttypes.h>
 #ifdef APPLE
 static const int B1000000 = 1000000;
 static const int B1500000 = 1500000;
@@ -465,6 +465,79 @@ int virtual_f011_read(int device,int track,int sector,int side)
   return 0;
 }
 
+typedef struct {
+	char mask;    /* char data will be bitwise AND with this */
+	char lead;    /* start bytes of current char in utf-8 encoded character */
+	uint32_t beg; /* beginning of codepoint range */
+	uint32_t end; /* end of codepoint range */
+	int bits_stored; /* the number of bits from the codepoint that fits in char */
+}utf_t;
+ 
+utf_t * utf[] = {
+	/*             mask        lead        beg      end       bits */
+	[0] = &(utf_t){0b00111111, 0b10000000, 0,       0,        6    },
+	[1] = &(utf_t){0b01111111, 0b00000000, 0000,    0177,     7    },
+	[2] = &(utf_t){0b00011111, 0b11000000, 0200,    03777,    5    },
+	[3] = &(utf_t){0b00001111, 0b11100000, 04000,   0177777,  4    },
+	[4] = &(utf_t){0b00000111, 0b11110000, 0200000, 04177777, 3    },
+	      &(utf_t){0},
+};
+
+// UTF-8 from https://rosettacode.org/wiki/UTF-8_encode_and_decode#C
+
+/* All lengths are in bytes */
+int codepoint_len(const uint32_t cp); /* len of associated utf-8 char */
+int utf8_len(const char ch);          /* len of utf-8 encoded char */
+ 
+char *to_utf8(const uint32_t cp);
+uint32_t to_cp(const char chr[4]);
+ 
+int codepoint_len(const uint32_t cp)
+{
+	int len = 0;
+	for(utf_t **u = utf; *u; ++u) {
+		if((cp >= (*u)->beg) && (cp <= (*u)->end)) {
+			break;
+		}
+		++len;
+	}
+	if(len > 4) /* Out of bounds */
+		exit(1);
+ 
+	return len;
+}
+ 
+int utf8_len(const char ch)
+{
+	int len = 0;
+	for(utf_t **u = utf; *u; ++u) {
+		if((ch & ~(*u)->mask) == (*u)->lead) {
+			break;
+		}
+		++len;
+	}
+	if(len > 4) { /* Malformed leading byte */
+		exit(1);
+	}
+	return len;
+}
+ 
+char *to_utf8(const uint32_t cp)
+{
+	static char ret[5];
+	const int bytes = codepoint_len(cp);
+ 
+	int shift = utf[0]->bits_stored * (bytes - 1);
+	ret[0] = (cp >> shift & utf[bytes]->mask) | utf[bytes]->lead;
+	shift -= utf[0]->bits_stored;
+	for(int i = 1; i < bytes; ++i) {
+		ret[i] = (cp >> shift & utf[0]->mask) | utf[0]->lead;
+		shift -= utf[0]->bits_stored;
+	}
+	ret[bytes] = '\0';
+	return ret;
+}
+
 int print_screencode(unsigned char c)
 {
   int rev=0;
@@ -476,7 +549,7 @@ int print_screencode(unsigned char c)
   if (c>='0'&&c<='9') printf("%c",c);
   else if (c>=0x00&&c<=0x1f) printf("%c",c+0x40);
   else if (c>=0x20&&c<=0x3f) printf("%c",c);
-  
+  else if (c==0x69) printf("%s",to_utf8(0x25e4));
   else printf("?");
 
   if (rev) {
