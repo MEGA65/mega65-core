6a3dc7ec8d3e2053d653b3a7c411b9b4a4d8cbd5
Paul Gardner-Stephen <paul@servalproject.org>
2020-02-06 11:33:50 +1030

    pull in file chooser code from freeze menu and begin to integrate. #156

--- src/utilities/megaflash.c
+++ src/utilities/megaflash.c
@@ -6,6 +6,8 @@
 #include "dirent.h"
 #include "fileio.h"
 
+char *select_bitstream_file(void);
+
 unsigned char joy_x=100;
 unsigned char joy_y=100;
 
@@ -99,7 +101,7 @@ void reconfig_fpga(unsigned long addr)
 
 void reflash_slot(unsigned char slot)
 {
-  printf("%c",0x93);
+  unsigned char *file=select_bitstream_file();
 }
 
 int bash_bits=0xFF;
@@ -498,8 +500,6 @@ unsigned char slot_empty_check(unsigned short mb_num)
 
 void main(void)
 {
-  unsigned char d;
-  struct m65_dirent *de=NULL;
   unsigned char valid;
   
   mega65_io_enable();
@@ -692,18 +692,6 @@ void main(void)
   program_page(4*1048576L);  
 #endif
 
-#if 0
-  d=opendir();
-  while ((de=readdir(d))!=NULL) {
-    if (strlen(de->d_name)>4) {
-      if (!strcmp(&de->d_name[strlen(de->d_name)-4],".d81")) {
-	printf("file '%s'\n",de->d_name);
-      }
-    }
-  }
-  closedir(d);
-#endif
-  
 }
 
 unsigned char progress_chars[4]={32,101,97,231};
@@ -728,4 +716,223 @@ void progress_bar(unsigned char onesixtieths)
   POKE(0x0400+(5*40)+(onesixtieths/4),progress_chars[x & 3]);
   POKE(0x0400+(6*40)+(onesixtieths/4),progress_chars[x & 3]);
   return;
+}
+
+
+/*
+  Bitstream file chooser. Adapted from Freeze Menu disk
+  image chooser.
+
+  Disk chooser for freeze menu.
+
+  It is displayed over the top of the normal freeze menu,
+  and so we use that screen mode.
+
+  We get our list of disknames and put them at $40000.
+  As we only care about their names, and file names are
+  limited to 64 characters, we can fit ~1000.
+  In fact, we can only safely mount images with names <32
+  characters.
+
+  We return the disk image name or a NULL pointer if the
+  selection has failed and $FFFF if the user cancels selection
+  of a disk.
+*/
+
+
+short file_count=0;
+short selection_number=0;
+short display_offset=0;
+
+char *reading_disk_list_message="SCANNING DIRECTORY ...";
+
+char *diskchooser_instructions=
+  "  SELECT FLASH FILE, THEN PRESS RETURN  "
+  "       OR PRESS RUN/STOP TO ABORT       ";
+
+unsigned char normal_row[40]={
+  0,1,0,1,0,1,0,1,
+  0,1,0,1,0,1,0,1,
+  0,1,0,1,0,1,0,1,
+  0,1,0,1,0,1,0,1,
+  0,1,0,1,0,1,0,1
+};
+
+unsigned char highlight_row[40]={
+  0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,
+  0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,0,0x21,
+  0,0x21,0,0x21,0,0x21,0,0x21
+};
+
+unsigned char dir_line_colour[40]={
+  0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,
+  0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,0,0xe,
+  0,0xe,0,0xe,0,0xe,0,0xe
+};
+
+char disk_name_return[32];
+
+unsigned char joy_to_key_disk[32]={
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x0d, // With fire pressed
+  0,0,0,0,0,0,0,0x9d,0,0,0,0x1d,0,0x11,0x91,0     // without fire
+};
+
+#define SCREEN_ADDRESS 0x0400
+#define COLOUR_RAM_ADDRESS 0x1f800
+
+void draw_file_list(void)
+{
+  unsigned addr=SCREEN_ADDRESS;
+  unsigned char i,x;
+  unsigned char name[64];
+  // First, clear the screen
+  POKE(SCREEN_ADDRESS+0,' ');
+  POKE(SCREEN_ADDRESS+1,0);
+  POKE(SCREEN_ADDRESS+2,' ');
+  POKE(SCREEN_ADDRESS+3,0);
+  lcopy(SCREEN_ADDRESS,SCREEN_ADDRESS+4,40*2*23-4);
+  lpoke(COLOUR_RAM_ADDRESS+0,0);
+  lpoke(COLOUR_RAM_ADDRESS+1,1);
+  lpoke(COLOUR_RAM_ADDRESS+2,0);
+  lpoke(COLOUR_RAM_ADDRESS+3,1);
+  lcopy(COLOUR_RAM_ADDRESS,COLOUR_RAM_ADDRESS+4,40*2*23-4);
+
+  // Draw instructions
+  for(i=0;i<80;i++) {
+    if (diskchooser_instructions[i]>='A'&&diskchooser_instructions[i]<='Z') 
+      POKE(SCREEN_ADDRESS+23*80+(i<<1)+0,diskchooser_instructions[i]&0x1f);
+    else
+      POKE(SCREEN_ADDRESS+23*80+(i<<1)+0,diskchooser_instructions[i]);
+    POKE(SCREEN_ADDRESS+23*80+(i<<1)+1,0);
+  }
+  lcopy((long)highlight_row,COLOUR_RAM_ADDRESS+(23*80)+0,40);
+  lcopy((long)highlight_row,COLOUR_RAM_ADDRESS+(23*80)+40,40);
+  lcopy((long)highlight_row,COLOUR_RAM_ADDRESS+(24*80)+0,40);
+  lcopy((long)highlight_row,COLOUR_RAM_ADDRESS+(24*80)+40,40);
+
+  
+  for(i=0;i<23;i++) {
+    if ((display_offset+i)<file_count) {
+      // Real line
+      lcopy(0x40000U+((display_offset+i)<<6),(unsigned char *)name,64);
+
+      for(x=0;x<20;x++) {
+	if ((name[x]>='A'&&name[x]<='Z') ||(name[x]>='a'&&name[x]<='z'))
+	  POKE(addr+(x<<1),name[x]&0x1f);
+	else
+	  POKE(addr+(x<<1),name[x]);
+      }
+    } else {
+      // Blank dummy entry
+      for(x=0;x<40;x++) POKE(addr+(x<<1),' ');
+    }
+    if ((display_offset+i)==selection_number) {
+      // Highlight the row
+      lcopy((long)highlight_row,COLOUR_RAM_ADDRESS+(i*80),40);
+    } else {
+      // Normal row
+      lcopy((long)normal_row,COLOUR_RAM_ADDRESS+(i*80),40);
+    }
+    addr+=(40*2);  
+  }
+
+  
+}
+
+char *select_bitstream_file(void)
+{
+  unsigned char x,dir;
+  struct m65_dirent *dirent;
+  int idle_time=0;
+  
+  file_count=0;
+  selection_number=0;
+  display_offset=0;
+  
+  // First, clear the screen
+  POKE(SCREEN_ADDRESS+0,' ');
+  POKE(SCREEN_ADDRESS+1,' ');
+  lcopy(SCREEN_ADDRESS,SCREEN_ADDRESS+2,40*25-2);
+
+  for(x=0;reading_disk_list_message[x];x++)
+    POKE(SCREEN_ADDRESS+12*40+(9)+(x*1),reading_disk_list_message[x]&0x3f);
+
+  dir=opendir();
+  dirent=readdir(dir);
+  while(dirent&&((unsigned short)dirent!=0xffffU)) {
+    x=strlen(dirent->d_name)-4;
+    if (x>=0) {
+      if ((!strncmp(&dirent->d_name[x],".D81",4))||(!strncmp(&dirent->d_name[x],".d81",4))) {
+	// File is a disk image
+	lfill(0x40000L+(file_count*64),' ',64);
+	lcopy((long)&dirent->d_name[0],0x40000L+(file_count*64),x+4);
+	file_count++;
+      }
+    }
+    
+    dirent=readdir(dir);
+  }
+
+  closedir(dir);
+
+  // If we didn't find any disk images, then just return
+  if (!file_count) return NULL;
+
+  // Okay, we have some disk images, now get the user to pick one!
+  draw_file_list();
+  while(1) {
+    x=PEEK(0xD610U);
+
+    if(!x) {
+      // We use a simple lookup table to do this
+      x=joy_to_key_disk[PEEK(0xDC00)&PEEK(0xDC01)&0x1f];
+      // Then wait for joystick to release
+      while((PEEK(0xDC00)&PEEK(0xDC01)&0x1f)!=0x1f) continue;
+    }
+    
+    if (!x) {
+      idle_time++;
+
+      usleep(10000);
+      continue;
+    } else idle_time=0;
+
+    // Clear read key
+    POKE(0xD610U,0);
+
+    switch(x) {
+    case 0x03:             // RUN-STOP = make no change
+      return NULL;
+    case 0x0d:             // Return = select this disk.
+      // Copy name out
+      lcopy(0x40000L+(selection_number*64),disk_name_return,32);
+      // Then null terminate it
+      for(x=31;x;x--)
+	if (disk_name_return[x]==' ') { disk_name_return[x]=0; } else { break; }
+      
+      return disk_name_return;
+      break;
+    case 0x11: case 0x9d:  // Cursor down or left
+      POKE(0xD020U,6);
+      selection_number++;
+      if (selection_number>=file_count) selection_number=0;
+      break;
+    case 0x91: case 0x1d:  // Cursor up or right
+      POKE(0xD020U,6);
+      selection_number--;
+      if (selection_number<0) selection_number=file_count-1;
+      break;
+    }
+
+    // Adjust display position
+    if (selection_number<display_offset) display_offset=selection_number;
+    if (selection_number>(display_offset+23)) display_offset=selection_number-22;
+    if (display_offset>(file_count-22)) display_offset=file_count-22;
+    if (display_offset<0) display_offset=0;
+
+    draw_file_list();
+    
+  }
+  
+  return NULL;
 }
