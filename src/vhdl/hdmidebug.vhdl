8c50de0dc09171cdeeda87000d130586ea4f7c99
Paul Gardner-Stephen <paul@servalproject.org>
2020-02-24 08:51:04 +1030

    more work on debugging hyperram

--- src/vhdl/hdmidebug.vhdl
+++ src/vhdl/hdmidebug.vhdl
@@ -45,9 +45,9 @@ entity container is
          ----------------------------------------------------------------------
          hr_d : inout unsigned(7 downto 0);
          hr_rwds : inout std_logic;
-         hr_reset : out std_logic;
+         hr_reset : inout std_logic;
          hr_clk_p : out std_logic;
-         hr_cs0 : out std_logic
+         hr_cs0 : inout std_logic
          
          );
 end container;
@@ -67,6 +67,14 @@ architecture Behavioral of container is
 
   signal counter : unsigned(31 downto 0) := to_unsigned(0,32);
 
+  signal expansionram_address : unsigned(26 downto 0) := to_unsigned(0,27);
+  signal expansionram_wdata : unsigned(7 downto 0) := to_unsigned(42,8);
+  signal expansionram_rdata : unsigned(7 downto 0);
+  signal expansionram_read : std_logic := '1';
+  signal expansionram_write : std_logic := '0';
+  signal expansionram_data_ready_strobe : std_logic;
+  signal expansionram_busy : std_logic;
+  
 begin
 
   dotclock1: entity work.dotclock100
@@ -79,27 +87,24 @@ begin
                clock27 => clock27
                );
 
---   hyperram0: entity work.hyperram
---     port map (
---       cpuclock => cpuclock,
---       clock240 => cpuclock,
---       -- reset => reset_out,
---       address => expansionram_address,
---       wdata => expansionram_wdata,
---       read_request => expansionram_read,
---       write_request => expansionram_write,
---       rdata => expansionram_rdata,
---       data_ready_strobe => expansionram_data_ready_strobe,
---       busy => expansionram_busy,
--- --      latency_1x => to_unsigned(4,8),
--- --      latency_2x => to_unsigned(8,8),
+   hyperram0: entity work.hyperram
+     port map (
+       cpuclock => cpuclock,
+       clock240 => cpuclock,
+       address => expansionram_address,
+       wdata => expansionram_wdata,
+       read_request => expansionram_read,
+       write_request => expansionram_write,
+       rdata => expansionram_rdata,
+       data_ready_strobe => expansionram_data_ready_strobe,
+       busy => expansionram_busy,
       
---       hr_d => hr_d,
---       hr_rwds => hr_rwds,
---       hr_reset => hr_reset,
---       hr_clk_p => hr_clk_p,
---       hr_cs0 => hr_cs0
---       );
+       hr_d => hr_d,
+       hr_rwds => hr_rwds,
+       hr_reset => hr_reset,
+       hr_clk_p => hr_clk_p,
+       hr_cs0 => hr_cs0
+       );
   
   PROCESS (PIXELCLOCK) IS
   BEGIN
@@ -108,12 +113,13 @@ begin
       counter <= counter + 1; 
 
       -- Try waggling Hyperram pins
-      hr_d <= counter(7 downto 0);
-      hr_cs0 <= counter(24);
-      hr_reset <= counter(25);
-      hr_rwds <= counter(26);
-      hr_clk_p <= counter(23);
-      led <= counter(23);
+--      hr_d <= counter(7 downto 0);
+--      hr_cs0 <= counter(24);
+--      hr_reset <= counter(25);
+--      hr_rwds <= counter(26);
+--      hr_clk_p <= counter(23);
+--      led <= counter(23);
+      led <= hr_cs0;
       
     end if;
     
