754d6f6dc0ee306d34705658e0b2180bcb2468ed
Paul Gardner-Stephen <paul@servalproject.org>
2020-02-26 05:11:36 +1030

    try to debug hyperram plumbing issue. #165

--- src/vhdl/hyperram.vhdl
+++ src/vhdl/hyperram.vhdl
@@ -153,7 +153,8 @@ begin
         ram_address <= address;
         ram_reading <= '1';
 
-        if address(23 downto 4) = x"FFFFF" then
+        -- XXX always read from dummy debug bitbash registers
+--        if address(23 downto 4) = x"FFFFF" then
           case address(3 downto 0) is
             when x"0" =>
               rdata <= (others => debug_mode);
@@ -172,7 +173,7 @@ begin
               rdata <= x"48";
           end case;
           data_ready_strobe <= '1';
-        end if;        
+--        end if;        
       elsif write_request='1' and busy_internal='0' then
         report "Making write request";
         -- Begin write request
