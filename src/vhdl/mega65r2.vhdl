db8fd6d1c0b802fed02929e9ef54f6363a3782db
Paul Gardner-Stephen <paul@servalproject.org>
2020-02-27 22:20:53 +1030

    remove hr_clk_n stray. #165

--- src/vhdl/mega65r2.vhdl
+++ src/vhdl/mega65r2.vhdl
@@ -95,7 +95,7 @@ entity container is
          hr_rwds : inout std_logic;
          hr_reset : out std_logic;
          hr_clk_p : out std_logic;
-         hr_clk_n : out std_logic;
+--         hr_clk_n : out std_logic;
          hr_cs0 : out std_logic;
          
          ----------------------------------------------------------------------
@@ -463,7 +463,7 @@ begin
       hr_rwds => hr_rwds,
       hr_reset => hr_reset,
       hr_clk_p => hr_clk_p,
-      hr_clk_n => hr_clk_n,
+--      hr_clk_n => hr_clk_n,
       hr_cs0 => hr_cs0
       );
 
