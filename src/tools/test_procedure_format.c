99eedc10c2a2eefe2b09573490730b450f0dbeef
Paul Gardner-Stephen <paul@servalproject.org>
2020-03-02 09:45:33 +1030

    extract problems / issues from issue files. #175

--- src/tools/test_procedure_format.c
+++ src/tools/test_procedure_format.c
@@ -5,6 +5,44 @@
 
 int max_issue=0;
 
+int parse_string(char *in,char *out)
+{
+  int outlen=0;
+  for(int i=0;in[i];i++) {
+    if (in[i]=='\\') {
+      i++;
+      switch(in[i]) {
+      case 'r':	out[outlen++]='\r'; break;
+      case 'n':	out[outlen++]='\r'; break;
+      case 't':	out[outlen++]='\t'; break;
+      case '\"':	out[outlen++]='\"'; break;
+      case '\'':	out[outlen++]='\''; break;
+      default:
+	fprintf(stderr,"WARNING: Unknown \\ escape \\%c\n",in[i]);
+	i--;
+	out[outlen++]='\\';
+      }
+    } else {
+	out[outlen++]=in[i];
+    }
+  }
+  out[outlen]=0;
+}
+
+int register_breaks(int issue,char *ttile,char *problem)
+{
+  int i;
+  char problem_msg[8192];
+  for(i=0;problem[i]&&problem[i]!='\r';i++) {
+    problem_msg[i]=problem[i];
+    problem_msg[i+1]=0;
+  }
+  
+  fprintf(stderr,"Registering problem: #%d : '%s'\n",
+	  issue,problem_msg);
+  return 0;
+}
+
 int main(int argc,char **argv)
 {
 
@@ -59,7 +97,7 @@ int main(int argc,char **argv)
       char cmd[1024];
       snprintf(cmd,1024,"curl -i https://api.github.com/repos/mega65/mega65-core/issues/%d > %s\n",
 	      issue,issue_file);
-      //system(cmd);
+      system(cmd);
       isf=fopen(issue_file,"r");
       if (!isf) {
 	fprintf(stderr,"WARNING: Could not fetch issue #%d\n",issue);
@@ -69,8 +107,44 @@ int main(int argc,char **argv)
     // Ok, have file, parse it.
     if (isf) {
 
+      int issue_num;
+      char title[8192];
+      char body[8192];
+      
+      char line[8192];
+      line[0]=0; fgets(line,8192,isf);
+      while(line[0]) {
+	while(line[0]&&(line[0]<=' '))
+	  bcopy(&line[1],&line[0],strlen(line));
+	while(line[0]&&(line[strlen(line)-1]<' '))
+	  line[strlen(line)-1]=0;
+	//	    printf("> '%s'\n",line);
+	sscanf(line,"\"number\": %d",&issue_num);
+	if (!strncmp("\"title\": ",line,9)) {
+	  parse_string(&line[10],title);
+	}
+	if (!strncmp("\"body\": ",line,8)) {
+	  parse_string(&line[9],body);
+	}
+
+	line[0]=0; fgets(line,8192,isf);       
+      }
       
       fclose(isf);
+
+      if (0) printf("Issue #%d:\ntitle = %s\nbody = %s\n",
+		    issue_num,title,body);
+      int problem_count=0;
+      for (int i=0;body[i];i++) {
+	if (!strncmp("\r##BREAKS ",&body[i],10)) {
+	  register_breaks(issue_num,title,&body[i+10]);
+	  problem_count++;
+	}
+      }
+      // If no specific problems were registered, then we just need to log the whole
+      // issue
+      if (!problem_count)
+        register_breaks(issue_num,title,"Unspecified problem. Please add ##BREAKS tags via github issue");
     }
   }
 
