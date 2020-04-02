e8af5ef1c17b8838b12f38a93a22e65aa99b3af9
Paul Gardner-Stephen <paul@servalproject.org>
2020-04-03 09:49:59 +1030

    restore hypervisor memory protection, now that serial monitor bypasses it anyway. #192

--- src/vhdl/iomapper.vhdl
+++ src/vhdl/iomapper.vhdl
@@ -1605,9 +1605,7 @@ begin
       -- @IO:GS $FFF8114 Hypervisor entry point on FDC write (when virtualised) (trap $45)
       
       if address(19 downto 14)&"00" = x"F8" then
-        -- XXX SECURITY DEBUG TEMPORARY Leave HYPPO mappable all the time for debug
-        -- hyppocs <= cpu_hypervisor_mode;
-        hyppocs <= '1';
+        hyppocs <= cpu_hypervisor_mode;
       else
         hyppocs <='0';
       end if;
