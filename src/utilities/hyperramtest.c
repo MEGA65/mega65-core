acda983bbad2b1c7b7b364a5c64ebf93c9b7be3a
Paul Gardner-Stephen <paul@servalproject.org>
2020-04-11 15:11:29 +0930

    revert hyperram to get rid of broken timing optimisation stuff. #165

--- src/utilities/hyperramtest.c
+++ src/utilities/hyperramtest.c
@@ -6,13 +6,14 @@
 #include <dirent.h>
 #include <fileio.h>
 
-unsigned long addr,time,speed;
+unsigned long addr,upper_addr,time,speed;
 unsigned char r2;
 unsigned char cr0hi;
 unsigned char cr0lo;
 unsigned char id0hi;
 unsigned char id0lo;
 unsigned int mbs;
+unsigned int temp_addr;
 
 void bust_cache(void) {
   lpeek(0x8000100);
@@ -39,14 +40,6 @@ void bust_cache(void) {
 
 void setup_hyperram(void)
 {
-  // Pre-fill all hyperram for convenience
-  printf("Erasing hyperram");
-  lpoke(0xbfffff2,0x82);  // cache on for much faster linear writes
-  for(addr=0x8000000;(addr<0x9000000);addr+=0x8000)
-    { lfill(addr,0x00,0x8000); printf("."); }
-  printf("\n");
-
-  
   /*
     Test complete HyperRAM, including working out the size.
   */
@@ -59,7 +52,7 @@ void setup_hyperram(void)
   }
   for(addr=0x8001000;(lpeek(0x8000000)==0xbd)&&(addr!=0x9000000);addr+=0x1000)
     {
-      printf(".");
+      if (!(addr&0xfffff)) printf(".");
       lpoke(addr,0x55);
 
       bust_cache();
@@ -85,12 +78,149 @@ void setup_hyperram(void)
       }
     }
 
-  lpoke(0xbfffff2,0x82);
+
+  upper_addr=addr;
+
+  // Pre-fill all hyperram for convenience
+  printf("Erasing hyperram");
+  lpoke(0xbfffff2,0x82);  // cache on for much faster linear writes
+
+  // Allow for upto 16MB of HyperRAM
+  for(addr=0x8000000;(addr<upper_addr);addr+=0x8000)
+    { lfill(addr,0x00,0x8000);
+      if (!(addr&0xfffff)) printf(".");
+    }
+  printf("\n");
+
   
-  printf("\nPress any key to continue...\n");
-  while(PEEK(0xD610)) POKE(0xD610,0);
-  while(!PEEK(0xD610)) continue;
-  while(PEEK(0xD610)) POKE(0xD610,0);
+
+ 
+  /*
+    printf("\nPress any key to continue...\n");
+    while(PEEK(0xD610)) POKE(0xD610,0);
+    while(!PEEK(0xD610)) continue;
+    while(PEEK(0xD610)) POKE(0xD610,0);
+  */
+  
+}
+
+void test_checkerboard(void)
+{
+  printf("Performing checkerboard test.\n");
+
+  // Make 2KB checkerboard block, and then copy it into all RAM
+  // (last 16 bytes of $CXXX is used by libc for dma lists, so can't be used).
+  for(addr=0xc000;addr<0xc800;addr++)
+    if (addr&1) POKE(addr,0x55); else POKE(addr,0xaa);
+
+  printf("Filling HyperRAM with checkerboard pattern");
+  for(addr=0x8000000;addr<upper_addr;addr+=0x800)
+    {
+      if (!(addr&0xfffff)) printf(".");
+      lcopy(0xc000,addr,0x800);
+    }
+  printf("\n");
+
+  // Now copy out blocks of HypeRAM and verify that the checkerboard pattern is still there.
+  printf("Verifying");
+  for(addr=0x8000000;addr<upper_addr;addr+=0x800)
+    {
+      if (!(addr&0xffff)) printf(".");
+      lcopy(addr,0xc000,0x800);
+      for(temp_addr=0xc000;temp_addr<0xc800;temp_addr++)
+	if (
+	    ((temp_addr&1)&(PEEK(temp_addr)!=0x55))
+	    ||
+	    ((!(temp_addr&1))&(PEEK(temp_addr)!=0xAA))
+	    )
+	  {
+	    printf("\nVerify error: $%08lx contained $%02x instead of $%02x\n",
+		   addr+temp_addr-0xc000,PEEK(temp_addr),
+		   (addr&1)?0x55:0xaa);
+	    while(PEEK(0xD610)) POKE(0xD610,0);
+	    while(!PEEK(0xD610)) continue;
+	    while(PEEK(0xD610)) POKE(0xD610,0);
+	  }      		 
+    }
+  printf("\n");
+  
+
+  // Make 2KB inverse checkerboard block, and then copy it into all RAM
+  // (last 16 bytes of $CXXX is used by libc for dma lists, so can't be used).
+  for(addr=0xc000;addr<0xc800;addr++)
+    if (addr&1) POKE(addr,0xaa); else POKE(addr,0x55);
+
+  printf("Filling HyperRAM with inverse checkerboard pattern");
+  for(addr=0x8000000;addr<upper_addr;addr+=0x800)
+    {
+      if (!(addr&0xfffff)) printf(".");
+      lcopy(0xc000,addr,0x800);
+    }
+  printf("\n");
+
+  // Now copy out blocks of HypeRAM and verify that the checkerboard pattern is still there.
+  printf("Verifying");
+  for(addr=0x8000000;addr<upper_addr;addr+=0x800)
+    {
+      if (!(addr&0xffff)) printf(".");
+      lcopy(addr,0xc000,0x800);
+      for(temp_addr=0xc000;temp_addr<0xc800;temp_addr++)
+	if (
+	    ((temp_addr&1)&(PEEK(temp_addr)!=0xAA))
+	    ||
+	    ((!(temp_addr&1))&(PEEK(temp_addr)!=0x55))
+	    )
+	  {
+	    printf("\nVerify error: $%08lx contained $%02x instead of $%02x\n",
+		   addr+temp_addr-0xc000,PEEK(temp_addr),
+		   (addr&1)?0x55:0xaa);
+	    while(PEEK(0xD610)) POKE(0xD610,0);
+	    while(!PEEK(0xD610)) continue;
+	    while(PEEK(0xD610)) POKE(0xD610,0);
+	  }      		 
+    }
+  printf("\n");
+  
+
+  
+}
+
+void show_info(void)
+{
+  printf("%cUpper limit of Extra RAM is $%08lx\n",0x13,upper_addr);
+  mbs=(unsigned int)((addr-0x8000000L)>>20L);
+  printf("Extra RAM is %d MB\n",mbs);
+  printf("Chip ID: %d rows, %d columns\n",
+	 (id0hi&0x1f)+1,(id0lo>>4)+1);
+  printf("Expected capacity: %d MB\n",
+	 1<<((id0hi&0x1f)+1+(id0lo>>4)+1+1-20));
+  printf("Vendor: ");
+  switch(id0lo&0xf) {
+  case 1: printf("Cypress"); break;
+  case 3: printf("ISSI"); break;
+  default: printf("<unknown>");
+  }
+  printf("\n");
+  
+  printf("Config: Powered up=%c,\n drive strength=$%x,\n",(cr0hi&0x80)?'Y':'N',(cr0hi>>4)&7);
+  switch((cr0lo&0xf0)>>4) {
+  case 0: printf(" 5 clock latency,\n"); break;
+  case 1: printf(" 6 clock latency,\n"); break;
+  case 14: printf(" 3 clock latency,\n"); break;
+  case 15: printf(" 4 clock latency,\n"); break;
+  default:
+    printf("unknown latency clocks ($%x),\n",(cr0lo&0xf0)>>4);      
+  }
+  if (cr0lo&8) printf(" fixed latency,"); else printf(" variable latency,");
+  printf("\n");
+  if (cr0lo&28) printf(" legacy burst,"); else printf(" hybrid burst,");
+  printf("\n");
+  switch(cr0lo&3) {
+  case 0: printf(" 128 byte burst length.\n"); break;
+  case 1: printf(" 64 byte burst length.\n"); break;
+  case 2: printf(" 16 byte burst length.\n"); break;
+  case 3: printf(" 32 byte burst length.\n"); break;
+  }
   
 }
 
@@ -117,6 +247,8 @@ void main(void)
   bust_cache();
   id0lo=lpeek(0x9000001);
 
+  while (PEEK(0xd610)) POKE(0xd610,0);
+  
   while(1) {
 
     while (PEEK(0xD610)) {
@@ -124,42 +256,16 @@ void main(void)
       POKE(0xD610,0);
       printf("%c",0x93);
     }
+
+    printf("%c",0x93);
+    show_info();
+
+    // Perform various tests before showing live speed info
+    if (!PEEK(0xD610)) test_checkerboard();
+
+    printf("%c",0x93);
+    show_info();
     
-    printf("%cUpper limit of Extra RAM is $%08lx\n",0x13,addr);
-    mbs=(unsigned int)((addr-0x8000000L)>>20L);
-    printf("Extra RAM is %d MB\n",mbs);
-    printf("Chip ID: %d rows, %d columns\n",
-	   (id0hi&0x1f)+1,(id0lo>>4)+1);
-    printf("Expected capacity: %d MB\n",
-	   1<<((id0hi&0x1f)+1+(id0lo>>4)+1+1-20));
-    printf("Vendor: ");
-    switch(id0lo&0xf) {
-    case 1: printf("Cypress"); break;
-    case 3: printf("ISSI"); break;
-    default: printf("<unknown>");
-    }
-    printf("\n");
-
-    printf("Config: Powered up=%c,\n drive strength=$%x,\n",(cr0hi&0x80)?'Y':'N',(cr0hi>>4)&7);
-    switch((cr0lo&0xf0)>>4) {
-    case 0: printf(" 5 clock latency,\n"); break;
-    case 1: printf(" 6 clock latency,\n"); break;
-    case 14: printf(" 3 clock latency,\n"); break;
-    case 15: printf(" 4 clock latency,\n"); break;
-    default:
-      printf("unknown latency clocks ($%x),\n",(cr0lo&0xf0)>>4);      
-    }
-    if (cr0lo&8) printf(" fixed latency,"); else printf(" variable latency,");
-    printf("\n");
-    if (cr0lo&28) printf(" legacy burst,"); else printf(" hybrid burst,");
-    printf("\n");
-    switch(cr0lo&3) {
-    case 0: printf(" 128 byte burst length.\n"); break;
-    case 1: printf(" 64 byte burst length.\n"); break;
-    case 2: printf(" 16 byte burst length.\n"); break;
-    case 3: printf(" 32 byte burst length.\n"); break;
-    }
-
     // Test read speed of normal and extra ram  
     while(PEEK(0xD012)!=0x10)
       while(PEEK(0xD011)&0x80) continue;
@@ -250,6 +356,10 @@ void main(void)
     time=(r2-0x10)*63;
     speed=4096000L/time;
     printf("%ld KB/sec\n",speed);
+
+    // cache back on after no-cache test
+    lpoke(0xbfffff2,0x82);
+    
   }
   
   
