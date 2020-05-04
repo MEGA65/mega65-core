1bc5bf7bf7a6c1124ef270f8e2202140f15ef963
Paul Gardner-Stephen <paul@servalproject.org>
2020-05-04 09:55:07 +0930

    Pull in @Deft's windows fixes for bit2core #187

--- src/tools/bit2core.c
+++ src/tools/bit2core.c
@@ -2,30 +2,31 @@
 #include <stdlib.h>
 #include <strings.h>
 
+unsigned char bitstream[4*1048576];
+
 int main(int argc,char **argv)
 {
   if (argc!=5) {
-    fprintf(stderr,"MEGA65 bitstream to core file converter.\n");
+    fprintf(stderr,"MEGA65 bitstream to core file converter v0.0.1.\n");
     fprintf(stderr,"usage: <foo.bit> <core name> <core version> <out.cor>\n");
     exit(-1);
   }
 
-  unsigned char bitstream[4*1048576];
-  FILE *bf=fopen(argv[1],"r");
+  FILE *bf=fopen(argv[1],"rb");
   if (!bf) {
     fprintf(stderr,"ERROR: Could not read bitstream file '%s'\n",argv[1]);
     exit(-3);
   }
   int bit_size=fread(bitstream,1,4*1048576,bf);
   fclose(bf);
-  
+
   printf("Bitstream file is %d bytes long.\n",bit_size);
   if (bit_size<1024||bit_size>(4*1048576-4096)) {
     fprintf(stderr,"ERROR: Bitstream file must be >1K and no bigger than (4MB - 4K)\n");
     exit(-2);
   }
 
-  FILE *of=fopen(argv[4],"w");
+  FILE *of=fopen(argv[4],"wb");
   if (!of) {
     fprintf(stderr,"ERROR: Could not create core file '%s'\n",argv[4]);
     exit(-3);
@@ -43,4 +44,4 @@ int main(int argc,char **argv)
 
   printf("Core file written.\n");
   return 0;
-}
+} 
