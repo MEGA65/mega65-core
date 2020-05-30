e0f52d037051cdbf52242344ddb9b7f12515c9dc
Paul Gardner-Stephen <paul@servalproject.org>
2020-05-30 12:45:58 +0930

    make it possible to read autotune_enable signal #129
    # Conflicts:
    #	src/vhdl/sdcardio.vhdl

--- src/vhdl/sdcardio.vhdl
+++ src/vhdl/sdcardio.vhdl
@@ -908,16 +908,18 @@ begin  -- behavioural
           -- @IO:GS $D689.2 - (read only, debug) sd_handshake signal.
           -- @IO:GS $D689.3 - (read only, debug) sd_data_ready signal.
           -- @IO:GS $D689.4 - F011 swap drive 0 / 1 
+          -- @IO:GS $D689.4 - (read only) FDC autotune_enable signal
           -- @IO:GS $D689.7 - Memory mapped sector buffer select: 1=SD-Card, 0=F011/FDC
           when x"89" =>
             fastio_rdata(0) <= f011_buffer_disk_address(8);
             fastio_rdata(1) <= f011_flag_eq and f011_drq;
             fastio_rdata(2) <= sd_handshake;
             fastio_rdata(3) <= sd_data_ready;
-            fastio_rdata(4) <= f011_swap_drives;
+            fastio_rdata(4) <= autotune_enable;
             fastio_rdata(5) <= virtualise_f011_drive0;
             fastio_rdata(6) <= virtualise_f011_drive1;
             fastio_rdata(7) <= f011sd_buffer_select;
+            fastio_rdata(6 downto 5) <= (others => '0');
           when x"8a" =>
             -- @IO:GS $D68A - DEBUG check signals that can inhibit sector buffer mapping
             fastio_rdata(0) <= colourram_at_dc00;
