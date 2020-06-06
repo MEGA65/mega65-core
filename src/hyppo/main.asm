d118baa312403a8106f5e1d0a1cb1de1c6d7add7
Paul Gardner-Stephen <paul@servalproject.org>
2020-06-06 22:49:17 +0930

    remove audio dma test #228

--- src/hyppo/main.asm
+++ src/hyppo/main.asm
@@ -618,15 +618,6 @@ reset_machine_state:
 reset_entry:
         sei
 
-	// Test audio dma
-	lda #$ff
-	sta $d726
-	sta $d727
-	sta $d728
-	sta $d729
-	lda #$83
-	sta $d720
-	
 	// Put ZP and stack back where they belong
 	lda #$bf
 	.byte $5B // tab
