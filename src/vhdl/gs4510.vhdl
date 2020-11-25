d6aff18714bbd9c00e4764e7319863145426686d
Paul Gardner-Stephen <paul@servalproject.org>
2020-11-26 07:26:02 +1030

    set C flag based on bit 15 of ASW operand #306

--- src/vhdl/gs4510.vhdl
+++ src/vhdl/gs4510.vhdl
@@ -7460,6 +7460,7 @@ begin
                   end if;
                   reg_t_high <= temp_addr(15 downto 8);
                   reg_t <= temp_addr(7 downto 0);
+                  flag_c <= temp_addr(15);
                 when I_DEW =>
                   temp_addr := (memory_read_value&reg_t) - 1;
                   if temp_addr = x"0000" then
