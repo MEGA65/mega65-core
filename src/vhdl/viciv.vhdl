1d97a82df505d984f033b782257a4b81ee08f0fe
Paul Gardner-Stephen <paul@servalproject.org>
2021-01-19 21:57:25 +1030

    try to fix junk line at bottom #345

--- src/vhdl/viciv.vhdl
+++ src/vhdl/viciv.vhdl
@@ -294,7 +294,6 @@ architecture Behavioral of viciv is
   -- (Sprite fetching should happen as soon as the border begins, so that we have
   -- maximum time to do the fetch.)
   constant display_fetch_start : unsigned(11 downto 0) := to_unsigned(719,12);
-  constant display_fetch_start_double_buffered : unsigned(11 downto 0) := to_unsigned(0,12);
   constant display_height_pal : unsigned(11 downto 0) := to_unsigned(623-20,12);
   constant display_height_ntsc : unsigned(11 downto 0) := to_unsigned(526-20,12);
   signal display_height : unsigned(11 downto 0);
@@ -3303,7 +3302,7 @@ begin
             is_fetch_start <= '1';
           end if;
         else
-          if xcounter=(safe_to_integer(frame_h_front)+display_fetch_start_double_buffered) then
+          if xcounter=(safe_to_integer(frame_h_front)+display_fetch_start) then
             is_fetch_start <= '1';
           end if;
         end if;
