c34a293b375a8d77fa6a5701a0c8030b91535f1b
Paul Gardner-Stephen <paul@servalproject.org>
2021-03-21 13:26:15 +1030

    add more test constants from C64 ROM etc

--- src/tools/c64float.c
+++ src/tools/c64float.c
@@ -33,14 +33,15 @@ double c64float_to_double(c64float *f)
   if (!f->f.exp) return 0;
   
   unsigned int man=(f->f.man0<<24)+(f->f.man1<<16)+(f->f.man2<<8)+(f->f.man3<<0);
-  d=man|0x80000000;
+  man|=0x80000000;
+  d=man;
   int exp=f->f.exp;
   while(exp>0x81) { d=d*2.0; exp--; }
   while(exp<0x81) { d=d/2.0; exp++; }
 
-  d=d/0x80000000;
+  d=d/(1.0*0x80000000);
 
-  if (man&0x80000000) d=-d;
+  if (f->f.man0&0x80) d=-d;
   
   return d;
 }
@@ -65,6 +66,19 @@ test_case test_cases[]={
 			{ .f={{{0x80,0x00,0x00,0x00,0x00}}},0.5},
 			{ .f={{{0xFF,0x7F,0xFF,0xFF,0xFF}}},1.70141183e+38},
 			{ .f={{{0xFF,0xFF,0xFF,0xFF,0xFF}}},-1.70141183e+38},
+			{ .f={{{0x98,0x35,0x44,0x7A,0x00}}},11879546.0},
+			{ .f={{{0x81,0x40,0x0,0x0,0x0}}},1.5},
+			{ .f={{{0x82,0x40,0x0,0x0,0x0}}},3},
+			{ .f={{{0x82,0x49,0x0F,0xDA,0xA1}}},3.14159265}, // PI
+			{ .f={{{0x90,0x80,0x0,0x0,0x0}}},-32768},
+			{ .f={{{0x7F,0x5E,0x56,0xCB,0x79}}},.434255942},  // 7F 5E 56 CB 79 = .434255942
+			{ .f={{{0x82,0x38,0xAA,0x3B,0x20}}},2.88539007}, // 82 38 AA 3B 20 = 2.88539007
+			{ .f={{{0x80,0x35,0x04,0xF3,0x34}}},.707106781}, // 80 35 04 F3 34 = .707106781 = 1/SQR(2)
+			{ .f={{{0x80,0x31,0x72,0x17,0xf8}}},.693147181}, // 80 31 72 17 F8 = ln(2)
+			{ .f={{{0x9B,0x3E,0xBC,0x1F,0xFD}}},99999999.9}, // 9B 3E BC 1F FD = 99999999.9
+			{ .f={{{0x9e,0x6e,0x6b,0x27,0xfd}}},999999999}, // 9E 6E 6B 27 FD = 999999999
+			{ .f={{{0x9e,0x6e,0x6b,0x28,0x0}}},1e9}, // 9E 6E 6B 28 00 = 1E9
+			//			{ .f={{{0x,0x,0x,0x,0x}}},},
 			{ .f={{{0x00,0x00,0x00,0x00,0x00}}},0.0}
   };
 
@@ -77,13 +91,14 @@ int main(int argc,char **argv)
   for(int i=0;test_cases[i].f.f.exp||test_cases[i].f.f.man0||test_cases[i].d;i++) {
     f.f=test_cases[i].f.f;
     d=c64float_to_double(&f);
-    printf("C64 float %s = %g\n",c64float_to_string(&f),d);
+    printf("C64 float %s = %.9g\n",c64float_to_string(&f),d);
 
     double err= ((test_cases[i].d-d)/test_cases[i].d);
     if (err<0) err=-err;
-    
+
+    // Allow 3 parts per billion error
     if (err>=0.000000003) {
-      printf("ERROR: Result was %g, but should have been %g (difference = %g, error fraction=%g)\n",
+      printf("ERROR: Result was %.9g, but should have been %.9g (difference = %.9g, error fraction=%.9g)\n",
 	     d,test_cases[i].d,test_cases[i].d-d,err);
       errors++;
     }
