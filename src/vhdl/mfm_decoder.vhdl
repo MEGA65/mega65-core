1be778fc9e3e43f30d3ac00b7e7f4973a0bd68ff
Paul Gardner-Stephen <paul@servalproject.org>
2021-06-27 21:18:13 +0930

    fix typo in mfm_decode #230

--- src/vhdl/mfm_decoder.vhdl
+++ src/vhdl/mfm_decoder.vhdl
@@ -258,7 +258,7 @@ begin
             if (target_any='1')
               or (
                 (to_integer(target_track) = to_integer(seen_track))
-                and (to_integer(target_sector) = to_integer(seen_sector))) then
+                and (to_integer(target_sector) = to_integer(seen_sector))
                 and (to_integer(target_side) = to_integer(seen_side))) then
               if (last_crc = x"0000") then
 --                report "Seen sector matches target";
