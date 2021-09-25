7495854150de27c6f4421321e0e7372cdf5c7163
Paul Gardner-Stephen <paul@servalproject.org>
2021-09-26 05:33:57 +0930

    begin calculating accumulated positions #422

--- src/vhdl/floppy_read_compensator.vhdl
+++ src/vhdl/floppy_read_compensator.vhdl
@@ -34,6 +34,7 @@ architecture behavioural of floppy_read_compensate is
   signal gap_time_5 : unsigned(15 downto 0) := to_unsigned(0,16);
   signal gap_time_6 : unsigned(15 downto 0) := to_unsigned(0,16);
   signal gap_time_7 : unsigned(15 downto 0) := to_unsigned(0,16);
+
   signal gap_quanta_0 : unsigned(15 downto 0) := to_unsigned(0,16);
   signal gap_quanta_1 : unsigned(15 downto 0) := to_unsigned(0,16);
   signal gap_quanta_2 : unsigned(15 downto 0) := to_unsigned(0,16);
@@ -43,13 +44,53 @@ architecture behavioural of floppy_read_compensate is
   signal gap_quanta_6 : unsigned(15 downto 0) := to_unsigned(0,16);
   signal gap_quanta_7 : unsigned(15 downto 0) := to_unsigned(0,16);  
 
+  signal gap_time_ticks0 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_time_ticks1 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_time_ticks2 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_time_ticks3 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_time_ticks4 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_time_ticks5 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_time_ticks6 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_time_ticks7 : unsigned(15 downto 0) := to_unsigned(0,16);
+
+  signal gap_time_remainders0 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_time_remainders1 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_time_remainders2 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_time_remainders3 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_time_remainders4 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_time_remainders5 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_time_remainders6 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_time_remainders7 : signed(7 downto 0) := to_signed(0,8);
+
+  signal gap_accum_ticks0 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_accum_ticks1 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_accum_ticks2 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_accum_ticks3 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_accum_ticks4 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_accum_ticks5 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_accum_ticks6 : unsigned(15 downto 0) := to_unsigned(0,16);
+  signal gap_accum_ticks7 : unsigned(15 downto 0) := to_unsigned(0,16);
+
+  signal gap_accum_remainders0 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_accum_remainders1 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_accum_remainders2 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_accum_remainders3 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_accum_remainders4 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_accum_remainders5 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_accum_remainders6 : signed(7 downto 0) := to_signed(0,8);
+  signal gap_accum_remainders7 : signed(7 downto 0) := to_signed(0,8);
+  
+  
   type state_t is (idle,
-                   dividing_bucket_into_gap_quant0
+                   dividing_bucket_into_gap_quant0,
+                   WaitForSums
                    );
   signal state : state_t := idle;
 
   signal bucket : unsigned(15 downto 0) := to_unsigned(0,16);
-
+  signal report_recent : std_logic := '0';
+  signal wait_counter : integer range 0 to 15 := 0;
+  
 begin
 
   process (clock40mhz) is
@@ -77,7 +118,7 @@ begin
         gap_time_6 <= gap_time_5 + gap_length_in;
         gap_time_7 <= gap_time_6 + gap_length_in;
         
-        -- What did we wantise each of the last 8 gaps to be?
+        -- What did we quantise each of the last 8 gaps to be?
         gap_quanta_7 <= gap_quanta_6;
         gap_quanta_6 <= gap_quanta_5;
         gap_quanta_5 <= gap_quanta_4;
@@ -86,6 +127,23 @@ begin
         gap_quanta_2 <= gap_quanta_1;
         gap_quanta_1 <= gap_quanta_0;
 
+        gap_time_ticks0 <= x"0000";
+        gap_time_ticks1 <= gap_time_ticks0;
+        gap_time_ticks2 <= gap_time_ticks1;
+        gap_time_ticks3 <= gap_time_ticks2;
+        gap_time_ticks4 <= gap_time_ticks3;
+        gap_time_ticks5 <= gap_time_ticks4;
+        gap_time_ticks6 <= gap_time_ticks5;
+        gap_time_ticks7 <= gap_time_ticks6;
+
+        gap_time_remainders0 <= x"00";
+        gap_time_remainders1 <= gap_time_remainders0;
+        gap_time_remainders2 <= gap_time_remainders1;
+        gap_time_remainders3 <= gap_time_remainders2;
+        gap_time_remainders4 <= gap_time_remainders3;
+        gap_time_remainders5 <= gap_time_remainders4;
+        gap_time_remainders6 <= gap_time_remainders5;
+        gap_time_remainders7 <= gap_time_remainders6;
         -- Now we need to work out what the quantisation for the current gap is.
         -- This would be the easy way, if we had hardware immediate division
         -- and floating point:
@@ -98,6 +156,88 @@ begin
 
       end if;
       
+      -- Work out the summed whole and remainder parts
+      -- gap_time_ticks0 and gap_time_remainders0 has been updated when
+      -- this is required.
+      -- First accumulated is just the actual value:
+      gap_accum_ticks0 <= gap_time_ticks0;
+      gap_accum_remainders0 <= gap_time_remainders0;
+--      report "gap_time_remainders0 = " & integer'image(to_integeR(gap_time_remainders0));
+      -- Then we need to add the time of the relevant gap to the accumulated
+      -- from the previous
+      gap_accum_ticks1 <= gap_accum_ticks0 + gap_time_ticks1;
+      gap_accum_ticks2 <= gap_accum_ticks1 + gap_time_ticks2;
+      gap_accum_ticks3 <= gap_accum_ticks2 + gap_time_ticks3;
+      gap_accum_ticks4 <= gap_accum_ticks3 + gap_time_ticks4;
+      gap_accum_ticks5 <= gap_accum_ticks4 + gap_time_ticks5;
+      gap_accum_ticks6 <= gap_accum_ticks5 + gap_time_ticks6;
+      gap_accum_ticks7 <= gap_accum_ticks6 + gap_time_ticks7;
+      gap_accum_remainders1 <= gap_accum_remainders0 + gap_time_remainders1;
+      gap_accum_remainders2 <= gap_accum_remainders1 + gap_time_remainders2;
+      gap_accum_remainders3 <= gap_accum_remainders2 + gap_time_remainders3;
+      gap_accum_remainders4 <= gap_accum_remainders3 + gap_time_remainders4;
+      gap_accum_remainders5 <= gap_accum_remainders4 + gap_time_remainders5;
+      gap_accum_remainders6 <= gap_accum_remainders5 + gap_time_remainders6;
+      gap_accum_remainders7 <= gap_accum_remainders6 + gap_time_remainders7;
+        
+      if report_recent='1' then
+        report_recent <= '0';
+        report "     Recent gaps: "
+          & integer'image(to_integer(gap_quanta_7)) & ", "
+          & integer'image(to_integer(gap_quanta_6)) & ", "
+          & integer'image(to_integer(gap_quanta_5)) & ", "
+          & integer'image(to_integer(gap_quanta_4)) & ", "
+          & integer'image(to_integer(gap_quanta_3)) & ", "
+          & integer'image(to_integer(gap_quanta_2)) & ", "
+          & integer'image(to_integer(gap_quanta_1)) & ", "
+          & integer'image(to_integer(gap_quanta_0));
+        report "Recent Gap Times: "
+          & integer'image(to_integer(gap_time_7)) & ", "
+          & integer'image(to_integer(gap_time_6)) & ", "
+          & integer'image(to_integer(gap_time_5)) & ", "
+          & integer'image(to_integer(gap_time_4)) & ", "
+          & integer'image(to_integer(gap_time_3)) & ", "
+          & integer'image(to_integer(gap_time_2)) & ", "
+          & integer'image(to_integer(gap_time_1)) & ", "
+          & integer'image(to_integer(gap_time_0));
+        report "         Divided: "
+          & integer'image(to_integer(gap_time_ticks7)) & ", "
+          & integer'image(to_integer(gap_time_ticks6)) & ", "
+          & integer'image(to_integer(gap_time_ticks5)) & ", "
+          & integer'image(to_integer(gap_time_ticks4)) & ", "
+          & integer'image(to_integer(gap_time_ticks3)) & ", "
+          & integer'image(to_integer(gap_time_ticks2)) & ", "
+          & integer'image(to_integer(gap_time_ticks1)) & ", "
+          & integer'image(to_integer(gap_time_ticks0));
+        report "      Remainders: "
+          & integer'image(to_integer(gap_time_remainders7)) & ", "
+          & integer'image(to_integer(gap_time_remainders6)) & ", "
+          & integer'image(to_integer(gap_time_remainders5)) & ", "
+          & integer'image(to_integer(gap_time_remainders4)) & ", "
+          & integer'image(to_integer(gap_time_remainders3)) & ", "
+          & integer'image(to_integer(gap_time_remainders2)) & ", "
+          & integer'image(to_integer(gap_time_remainders1)) & ", "
+          & integer'image(to_integer(gap_time_remainders0));
+        report "   Accum Divided: "
+          & integer'image(to_integer(gap_accum_ticks7)) & ", "
+          & integer'image(to_integer(gap_accum_ticks6)) & ", "
+          & integer'image(to_integer(gap_accum_ticks5)) & ", "
+          & integer'image(to_integer(gap_accum_ticks4)) & ", "
+          & integer'image(to_integer(gap_accum_ticks3)) & ", "
+          & integer'image(to_integer(gap_accum_ticks2)) & ", "
+          & integer'image(to_integer(gap_accum_ticks1)) & ", "
+          & integer'image(to_integer(gap_accum_ticks0));
+        report "Accum Remainders: "
+          & integer'image(to_integer(gap_accum_remainders7)) & ", "
+          & integer'image(to_integer(gap_accum_remainders6)) & ", "
+          & integer'image(to_integer(gap_accum_remainders5)) & ", "
+          & integer'image(to_integer(gap_accum_remainders4)) & ", "
+          & integer'image(to_integer(gap_accum_remainders3)) & ", "
+          & integer'image(to_integer(gap_accum_remainders2)) & ", "
+          & integer'image(to_integer(gap_accum_remainders1)) & ", "
+          & integer'image(to_integer(gap_accum_remainders0));
+      end if;
+      
       case state is
         when dividing_bucket_into_gap_quant0 =>
           if bucket >= cycles_per_interval then
@@ -108,11 +248,24 @@ begin
             report "Gap is " & integer'image(to_integer(gap_quanta_0)) & ", remainder " & integer'image(to_integer(bucket));
             if bucket > cycles_per_interval(7 downto 1) then
               report "Round gap up to next";
-              gap_quanta_0 <= gap_quanta_0 + 1;              
+              gap_quanta_0 <= gap_quanta_0 + 1;
+              gap_time_ticks0 <= gap_quanta_0 + 1;
+              gap_time_remainders0 <= signed(bucket(7 downto 0)) - signed(cycles_per_interval);
+            else
+              gap_time_ticks0 <= gap_quanta_0;
+              gap_time_remainders0 <= signed(bucket(7 downto 0));
             end if;
-            state <= Idle;
+            -- Now wait for the sums of recent gaps to propagate through
+            state <= WaitForSums;
+            wait_counter <= 8;
           end if;
-          null;
+        when WaitForSums =>
+          if wait_counter=0 then
+            report_recent <= '1';
+            state <= Idle;
+          else
+            wait_counter <= wait_counter - 1;
+          end if;
         when others =>
           null;
       end case;
