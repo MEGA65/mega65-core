9f64d81ff91c4976568abed21169ad4176ebf3dc
Falk Rehwagen <frehwagen@bluewaysw.de>
2021-12-22 19:24:50 +0100

    Cleanup solution, seems to work as needed, still pal simulation mode and regular mode are one physical pixel off. #415

--- src/vhdl/bitplanes.vhdl
+++ src/vhdl/bitplanes.vhdl
@@ -257,15 +257,15 @@ begin  -- behavioural
       if bitplane_h640='1' then
         v_x_in := x640_in;
         v_bitplane_y_start := bitplane_y_start_h640;
-        v_bitplane_x_start := bitplane_x_start_h640 + 10;
+        v_bitplane_x_start := bitplane_x_start_h640;
       elsif bitplane_h1280='1' then
         v_x_in := x1280_in;
         v_bitplane_y_start := bitplane_y_start_h1820;
-        v_bitplane_x_start := bitplane_x_start_h1280 + 10;
+        v_bitplane_x_start := bitplane_x_start_h1280;
       else
         v_x_in := x_in;
         v_bitplane_y_start := bitplane_y_start;
-        v_bitplane_x_start := bitplane_x_start + 10;
+        v_bitplane_x_start := bitplane_x_start;
       end if;
 
       -- Pre-calculate some things to improve timing
