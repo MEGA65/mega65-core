c503211a9b8de47b2e106a7c9b551f72ff8e9437
Paul Gardner-Stephen <paul@servalproject.org>
2022-05-23 06:14:43 +0930

    strip old stuff out of test harness for grove i2c

--- src/vhdl/test_grove.vhdl
+++ src/vhdl/test_grove.vhdl
@@ -16,40 +16,6 @@ architecture foo of test_grove is
   signal sda_last : std_logic := '1';
   signal scl_last : std_logic := '1';
 
-  type dummy_data_t is array(0 to 15) of unsigned(7 downto 0);
-  
-  signal dummy_touch_event : dummy_data_t
-    := (
-      0 => x"00", -- Not factory test mode
-      1 => x"00", -- recognised gesture (always 0 it seems)
-      2 => x"02", -- number of touch events
-
-      -- Touch event 1:
-      3 => "00" & "00" & to_unsigned(dummy_y1/256,4), -- touch is being held,
-                                                      -- 2 unused bits,
-                                                      -- MSB of Y position
-      4 => to_unsigned(dummy_y1 mod 256,8),           -- LSB of Y position
-      5 => to_unsigned(1,4) & to_unsigned(dummy_x1/256,4), -- touch ID
-                                                      -- MSB of X position
-      6 => to_unsigned(dummy_x1 mod 256,8),           -- LSB of X position
-      7 => x"00", -- touch pressure (unused?)
-      8 => x"00", -- touch area (unused?)
-
-      -- Touch event 2:
-      9 => "00" & "00" & to_unsigned(dummy_y2/256,4), -- touch is being held,
-                                                      -- 2 unused bits,
-                                                      -- MSB of Y position
-     10 => to_unsigned(dummy_y2 mod 256,8),           -- LSB of Y position
-     11 => to_unsigned(2,4) & to_unsigned(dummy_x2/256,4), -- touch ID
-                                                      -- MSB of X position
-     12 => to_unsigned(dummy_x2 mod 256,8),           -- LSB of X position
-     13 => x"00", -- touch pressure (unused?)
-      14 => x"00", -- touch area (unused?)
-
-      -- Extra item so we don't overrun
-    15 => x"FF"
-      );
-  
 begin
 
   i2c1: entity work.mega65r2_i2c port map (
@@ -82,25 +48,6 @@ begin
       
   end process;
 
-  process (clock50mhz) is
-  begin
-    if rising_edge(clock50mhz) then
-      if read_req = '1' then
-        -- data_to_master <= std_logic_vector(to_unsigned(next_value,8));
-        if next_value < 16 then
-          data_to_master <= std_logic_vector(dummy_touch_event(next_value));
-        else
-          data_to_master <= x"bd";
-        end if;
-        next_value <= next_value + 1;
-      elsif data_valid='1' then
-        -- write address to read from
-        next_value <= to_integer(unsigned(data_from_master));
-      end if;
-    end if;
-  end process;
-  
-  
   process (clock50mhz) is
   begin
     if rising_edge(clock50mhz) then
