30b2bace5d8c5467c12a0d64ccff1fdf5e537ef3
Paul Gardner-Stephen <paul@servalproject.org>
2022-06-13 16:50:33 +0930

    try to fix grove RTC I2C communications error

--- src/vhdl/grove_i2c.vhdl
+++ src/vhdl/grove_i2c.vhdl
@@ -145,6 +145,8 @@ begin
       if fastio_addr(7 downto 6) = "00" then
         report "reading buffered I2C data";
         fastio_rdata <= bytes(to_integer(fastio_addr(5 downto 0)));
+      elsif fastio_addr(7 downto 0) = x"fc" then
+        fastio_rdata <= to_unsigned(busy_count,8);
       elsif fastio_addr(7 downto 0) = x"fd" then
         fastio_rdata <= x"00";
         fastio_rdata(1) <= sda;
@@ -250,14 +252,8 @@ begin
           i2c1_address <= write_i2c_addr(7 downto 1); 
           i2c1_wdata <= x"00";
           i2c1_rw <= '0';
-        when 1 =>
-          -- Then switch to read, reading from register 0
-          command_en <= '1';
-          i2c1_address <= read_i2c_addr(7 downto 1); 
-          i2c1_wdata <= x"00";
-          i2c1_rw <= '1';
         when
-          2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 |
+          1 | 2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 |
           17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 |
           33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 |
           49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 |
