2c4255367b019afb416ac9fb8bd0cb1014114866
Paul Gardner-Stephen <paul@servalproject.org>
2022-09-17 17:38:45 +0930

    finish debugging pin assignments of te0725

--- src/vhdl/te0725.vhdl
+++ src/vhdl/te0725.vhdl
@@ -61,7 +61,7 @@ entity container is
          ----------------------------------------------------------------------
          -- Debug interfaces on TE0725
          ----------------------------------------------------------------------
-         led : inout std_logic := '1';
+         led : inout std_logic := '0';
 
          ----------------------------------------------------------------------
          -- UART monitor interface
@@ -74,55 +74,17 @@ end container;
 
 architecture Behavioral of container is
   
-  component fpgatemp is
-    Generic ( DELAY_CYCLES : natural := 480 ); -- 10us @ 48 Mhz
-    Port ( clk : in  STD_LOGIC;
-           rst : in  STD_LOGIC;
-           temp : out  STD_LOGIC_VECTOR (11 downto 0));
-  end component;
-
-  signal irq : std_logic := '1';
-  signal nmi : std_logic := '1';
-  signal restore_key : std_logic := '1';
-  signal reset_out : std_logic := '1';
-  signal cpu_game : std_logic := '1';
-  signal cpu_exrom : std_logic := '1';
-  
-  
   signal pixelclock : std_logic;
   signal cpuclock : std_logic;
-  signal clock240 : std_logic;
-  signal clock120 : std_logic;
-  signal clock100 : std_logic;
-  signal ethclock : std_logic;
-  signal clock200 : std_logic;
-
-  signal i2s_master_clk_int : std_logic := '0';
-  signal i2s_master_clk : std_logic := '0';
-  signal i2s_sync : std_logic := '0';
-  signal i2s_sync_int : std_logic := '0';
-  signal sample : unsigned(15 downto 0) := x"0001";
-  signal table_offset : integer range 0 to 255 := 0;
-  signal table_dir : std_logic := '1';
-  signal table_neg : std_logic := '0';
-  signal divisor : integer := 0;
-  constant divisor_max : integer := 40000000/(256*4)/1;  -- proceed it really slowly
-
-  constant max_table_value: integer := 255;
-  subtype table_value_type is integer range 0 to 255;
-
-  constant max_table_index: integer := 255;
-  subtype table_index_type is integer range 0 to 255;
-
-  subtype sine_vector_type is std_logic_vector( 8 downto 0 );
-
+  signal counter : integer := 0;
+  
 begin
   
   gen_pin1:
   for i in 1 to 100 generate
       pin1: entity work.pin_id
         port map (
-          clock => pixelclock,
+          clock => cpuclock,
           pin_number => to_unsigned(i,8),
           pin => fpga_pins(i)
           );
@@ -133,6 +95,22 @@ begin
                clock81p => pixelclock, -- 80MHz
                clock41 => cpuclock -- 40MHz
                );
+
+  process (cpuclock) is
+  begin
+    if rising_edge(cpuclock) then
+      if counter /= 40_500_000 then
+        counter <= counter + 1;
+      else
+        counter <= 0;
+        led <= '1';
+      end if;
+      if counter = 20_250_000 then
+        led <= '0';
+      end if;
       
+    end if;
+  end process;
+  
   
 end Behavioral;
