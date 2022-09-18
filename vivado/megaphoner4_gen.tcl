47d1262d0b456230f28399bb3ea2b69282764b12
Paul Gardner-Stephen <paul@servalproject.org>
2022-09-19 02:28:45 +0930

    megaphoner4 uses A100T FPGA, not A200T

--- vivado/megaphoner4_gen.tcl
+++ vivado/megaphoner4_gen.tcl
@@ -132,7 +132,7 @@ set files [list \
  "[file normalize "$origin_dir/src/vhdl/uart_rx_buffered.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/buffereduart.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/sid_6581.vhdl"]"\
- "[file normalize "$origin_dir/src/vhdl/shadowram-a200t.vhdl"]"\
+ "[file normalize "$origin_dir/src/vhdl/shadowram-a100t.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/sdcardio.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/audio_mixer.vhdl"]"\
  "[file normalize "$origin_dir/src/vhdl/audio_complex.vhdl"]"\
@@ -503,7 +503,7 @@ set file "vhdl/sid_6581.vhdl"
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 
-set file "vhdl/shadowram-a200t.vhdl"
+set file "vhdl/shadowram-a100t.vhdl"
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 
