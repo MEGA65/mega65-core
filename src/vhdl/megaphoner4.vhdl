63d5ea3c23c0eb8a931e563b3f4e59f7a1f038c1
Paul Gardner-Stephen <paul@servalproject.org>
2022-09-18 11:39:35 +0930

    begin putting MEGA65 back into megaphoner4 target

--- src/vhdl/megaphoner4.vhdl
+++ src/vhdl/megaphoner4.vhdl
@@ -163,21 +163,113 @@ architecture Behavioral of container is
   signal clock270 : std_logic;
   signal clock325 : std_logic;
 
-  signal dummy : std_logic_vector(2 downto 0);
-  signal sawtooth_phase : integer := 0;
-  signal sawtooth_counter : integer := 0;
-  signal sawtooth_level : integer := 0;
+  signal slow_access_request_toggle : std_logic;
+  signal slow_access_ready_toggle : std_logic;
+  signal slow_access_write : std_logic;
+  signal slow_access_address : unsigned(27 downto 0);
+  signal slow_access_wdata : unsigned(7 downto 0);
+  signal slow_access_rdata : unsigned(7 downto 0);
 
+  signal slow_prefetched_address : unsigned(26 downto 0);
+  signal slow_prefetched_data : unsigned(7 downto 0);
+  signal slow_prefetched_request_toggle : std_logic;
+
+  signal sector_buffer_mapped : std_logic;
+
+  signal kbd_datestamp : unsigned(13 downto 0)  := to_unsigned(0,14);
+  signal kbd_commit : unsigned(31 downto 0) := to_unsigned(0,32);
+
+  signal vgaredignore : unsigned(3 downto 0);
+  signal vgagreenignore : unsigned(3 downto 0);
+  signal vgablueignore : unsigned(3 downto 0);
+
+  signal porta_pins : std_logic_vector(7 downto 0) := (others => '1');
+  signal portb_pins : std_logic_vector(7 downto 0) := (others => '1');
+
+  signal cart_ctrl_dir : std_logic := 'Z';
+  signal cart_haddr_dir : std_logic := 'Z';
+  signal cart_laddr_dir : std_logic := 'Z';
+  signal cart_data_dir : std_logic := 'Z';
+  signal cart_phi2 : std_logic := 'Z';
+  signal cart_dotclock : std_logic := 'Z';
+  signal cart_reset : std_logic := 'Z';
+
+  signal cart_nmi : std_logic := 'Z';
+  signal cart_irq : std_logic := 'Z';
+  signal cart_dma : std_logic := 'Z';
+
+  signal cart_exrom : std_logic := 'Z';
+  signal cart_ba : std_logic := 'Z';
+  signal cart_rw : std_logic := 'Z';
+  signal cart_roml : std_logic := 'Z';
+  signal cart_romh : std_logic := 'Z';
+  signal cart_io1 : std_logic := 'Z';
+  signal cart_game : std_logic := 'Z';
+  signal cart_io2 : std_logic := 'Z';
+
+  signal cart_d : unsigned(7 downto 0) := (others => 'Z');
+  signal cart_d_read : unsigned(7 downto 0) := (others => 'Z');
+  signal cart_a : unsigned(15 downto 0) := (others => 'Z');
+
+  ----------------------------------------------------------------------
+  -- CBM floppy serial port
+  ----------------------------------------------------------------------
+  signal iec_clk_en : std_logic := 'Z';
+  signal iec_data_en : std_logic := 'Z';
+  signal iec_data_o : std_logic := 'Z';
+  signal iec_reset : std_logic := 'Z';
+  signal iec_clk_o : std_logic := 'Z';
+  signal iec_data_i : std_logic := '1';
+  signal iec_clk_i : std_logic := '1';
+  signal iec_atn : std_logic := 'Z';
+  signal smartcard_io_read : std_logic := '1';
+  signal smartcard_clk_read : std_logic := '1';
+  
   -- Dummy signals for stub / not yet implemented interfaces
   signal eth_mdio : std_logic := '0';
   signal c65uart_rx : std_logic := '1';
 
-  signal pin_number : integer;
-
-  signal dummypins : std_logic_vector(1 to 100) := (others => '0');
-
   signal qspi_clock : std_logic;
 
+  signal expansionram_read : std_logic;
+  signal expansionram_write : std_logic;
+  signal expansionram_rdata : unsigned(7 downto 0);
+  signal expansionram_wdata : unsigned(7 downto 0);
+  signal expansionram_address : unsigned(26 downto 0);
+  signal expansionram_data_ready_strobe : std_logic;
+  signal expansionram_busy : std_logic;
+
+  signal current_cache_line : cache_row_t := (others => (others => '0'));
+  signal current_cache_line_address : unsigned(26 downto 3) := (others => '0');
+  signal current_cache_line_valid : std_logic := '0';
+  signal expansionram_current_cache_line_next_toggle : std_logic := '0';
+
+  signal i2c_joya_fire : std_logic;
+  signal i2c_joya_left : std_logic;
+  signal i2c_joya_right : std_logic;
+  signal i2c_joya_up : std_logic;
+  signal i2c_joya_down : std_logic;
+  signal i2c_joyb_fire : std_logic;
+  signal i2c_joyb_left : std_logic;
+  signal i2c_joyb_right : std_logic;
+  signal i2c_joyb_up : std_logic;
+  signal i2c_joyb_down : std_logic;
+
+  signal i2c_button2 : std_logic;
+  signal i2c_button3 : std_logic;
+  signal i2c_button4 : std_logic;
+  signal i2c_black2 : std_logic;
+  signal i2c_black3 : std_logic;
+  signal i2c_black4 : std_logic;
+
+  signal widget_matrix_col : std_logic_vector(7 downto 0) := (others => '1');
+
+  signal hyper_addr : unsigned(18 downto 3) := (others => '0');
+  signal hyper_request_toggle : std_logic := '0';
+  signal hyper_data : unsigned(7 downto 0) := x"00";
+  signal hyper_data_strobe : std_logic := '0';
+  signal portp : unsigned(7 downto 0);
+  
   signal pcm_clk : std_logic := '0';
   signal pcm_rst : std_logic := '1';
   signal pcm_clken : std_logic := '0';
@@ -203,6 +295,8 @@ architecture Behavioral of container is
   signal dvi_select : std_logic := '1';
 
   signal TMDS_clk_q : std_logic := '0';
+
+
   
 begin
 
@@ -321,6 +415,357 @@ begin
             out_n   => TMDS_clk_n
         );    
 
+  hyperram0: entity work.hyperram
+    port map (
+      pixelclock => pixelclock,
+      clock163 => clock162,
+      clock325 => clock325,
+
+      -- XXX Debug by showing if expansion RAM unit is receiving requests or not
+--      request_counter => led,
+
+      viciv_addr => hyper_addr,
+      viciv_request_toggle => hyper_request_toggle,
+      viciv_data_out => hyper_data,
+      viciv_data_strobe => hyper_data_strobe,
+
+      -- reset => reset_out,
+      address => expansionram_address,
+      wdata => expansionram_wdata,
+      read_request => expansionram_read,
+      write_request => expansionram_write,
+      rdata => expansionram_rdata,
+      data_ready_strobe => expansionram_data_ready_strobe,
+      busy => expansionram_busy,
+
+      current_cache_line => current_cache_line,
+      current_cache_line_address => current_cache_line_address,
+      current_cache_line_valid => current_cache_line_valid,
+      expansionram_current_cache_line_next_toggle  => expansionram_current_cache_line_next_toggle,
+
+      hr_d => hr_d,
+      hr_rwds => hr_rwds,
+      hr_reset => hr_reset,
+      hr_clk_p => hr_clk_p,
+      hr_clk_n => hr_clk_n,
+
+      hr_cs0 => hr_cs0,
+      hr_cs1 => hr_cs1,
+
+      hr2_d => open,
+      hr2_rwds => open,
+      hr2_reset => open,
+      hr2_clk_p => open,
+      hr2_clk_n => open
+      );
+
+  slow_devices0: entity work.slow_devices
+    port map (
+      cpuclock => cpuclock,
+      pixelclock => pixelclock,
+      reset => reset_out,
+      cpu_exrom => cpu_exrom,
+      cpu_game => cpu_game,
+      sector_buffer_mapped => sector_buffer_mapped,
+
+      qspidb => qspidb,
+      qspicsn => qspicsn,
+      qspisck => qspi_clock,
+
+      pin_number => pin_number,
+
+      slow_access_request_toggle => slow_access_request_toggle,
+      slow_access_ready_toggle => slow_access_ready_toggle,
+      slow_access_write => slow_access_write,
+      slow_access_address => slow_access_address,
+      slow_access_wdata => slow_access_wdata,
+      slow_access_rdata => slow_access_rdata,
+
+      ----------------------------------------------------------------------
+      -- Expansion RAM interface (upto 127MB)
+      ----------------------------------------------------------------------
+      expansionram_read => expansionram_read,
+      expansionram_write => expansionram_write,
+      expansionram_rdata => expansionram_rdata,
+      expansionram_wdata => expansionram_wdata,
+      expansionram_address => expansionram_address,
+      expansionram_data_ready_strobe => expansionram_data_ready_strobe,
+      expansionram_busy => expansionram_busy,
+
+      slow_prefetched_address => slow_prefetched_address,
+      slow_prefetched_data => slow_prefetched_data,
+      slow_prefetched_request_toggle => slow_prefetched_request_toggle,
+
+      ----------------------------------------------------------------------
+      -- Expansion/cartridge port
+      ----------------------------------------------------------------------
+      cart_ctrl_dir => cart_ctrl_dir,
+      cart_haddr_dir => cart_haddr_dir,
+      cart_laddr_dir => cart_laddr_dir,
+      cart_data_dir => cart_data_dir,
+      cart_phi2 => cart_phi2,
+      cart_dotclock => cart_dotclock,
+      cart_reset => cart_reset,
+
+      cart_nmi => cart_nmi,
+      cart_irq => cart_irq,
+      cart_dma => cart_dma,
+
+      cart_exrom => cart_exrom,
+      cart_ba => cart_ba,
+      cart_rw => cart_rw,
+      cart_roml => cart_roml,
+      cart_romh => cart_romh,
+      cart_io1 => cart_io1,
+      cart_game => cart_game,
+      cart_io2 => cart_io2,
+
+      cart_d_in => cart_d_read,
+      cart_d => cart_d,
+      cart_a => cart_a
+      );
+
+  machine0: entity work.machine
+    generic map (cpu_frequency => 40500000,
+                 target => megaphoner1,
+                 hyper_installed => true -- For VIC-IV to know it can use
+                                         -- hyperram for full-colour glyphs
+                 )
+    port map (
+      pixelclock      => pixelclock,
+      cpuclock        => cpuclock,
+      uartclock       => cpuclock, -- Match CPU clock
+      clock162 => clock162,
+      clock200 => clock200,
+      clock27 => clock27,
+      clock50mhz      => ethclock,
+
+      flopled => flopled,
+      portp_out => portp,
+
+      -- No IEC bus on this hardware, so no need to slow CPU down for it.
+      iec_bus_active => '0',
+      iec_srq_external => '1',
+
+      kbd_datestamp => kbd_datestamp,
+      kbd_commit => kbd_commit,
+
+      btncpureset => '1',
+      reset_out => reset_out,
+      irq => irq,
+      nmi => nmi,
+      restore_key => restore_key,
+      joyswap_key => joyswap_key,
+      osk_toggle_key => osk_toggle_key,
+      sector_buffer_mapped => sector_buffer_mapped,
+
+      hyper_addr => hyper_addr,
+      hyper_request_toggle => hyper_request_toggle,
+      hyper_data => hyper_data,
+      hyper_data_strobe => hyper_data_strobe,
+
+      -- enable/disable cartridge with sw(8)
+      cpu_exrom => '1',
+      cpu_game => '1',
+
+      power_down => power_down,
+
+      pal50_select_out => pal50_select,
+
+      -- Wire up a dummy caps_lock key on switch 8
+      caps_lock_key => '1',
+
+      fa_fire => i2c_joya_fire,
+      fa_up => i2c_joya_up,
+      fa_left => i2c_joya_left,
+      fa_down => i2c_joya_down,
+      fa_right => i2c_joya_right,
+
+      fb_fire => i2c_joyb_fire,
+      fb_up => i2c_joyb_up,
+      fb_left => i2c_joyb_left,
+      fb_down => i2c_joyb_down,
+      fb_right => i2c_joyb_right,
+
+      fa_potx => '0',
+      fa_poty => '0',
+      fb_potx => '0',
+      fb_poty => '0',
+
+      i2c_joya_fire => i2c_joya_fire,
+      i2c_joya_up => i2c_joya_up,
+      i2c_joya_down => i2c_joya_down,
+      i2c_joya_left => i2c_joya_left,
+      i2c_joya_right => i2c_joya_right,
+      i2c_joyb_fire => i2c_joyb_fire,
+      i2c_joyb_up => i2c_joyb_up,
+      i2c_joyb_down => i2c_joyb_down,
+      i2c_joyb_left => i2c_joyb_left,
+      i2c_joyb_right => i2c_joyb_right,
+      -- XXX Come up with better button assignments
+      i2c_button2 => i2c_button2,
+      i2c_button3 => i2c_button3,
+      i2c_button4 => joyswap_key,
+      i2c_black3 => osk_toggle_key,
+      i2c_black4 => restore_key,
+
+      f_index => '1',
+      f_track0 => '1',
+      f_writeprotect => '1',
+      f_rdata => '1',
+      f_diskchanged => '1',
+
+      ----------------------------------------------------------------------
+      -- CBM floppy serial port stub
+      ----------------------------------------------------------------------
+      iec_clk_en => iec_clk_en,
+      iec_data_en => iec_data_en,
+      iec_data_o => iec_data_o,
+      iec_reset => iec_reset,
+      iec_clk_o => iec_clk_o,
+      iec_atn_o => iec_atn,
+      iec_data_external => smartcard_io_read,
+      iec_clk_external => smartcard_clk_read,
+
+      no_hyppo => '0',
+
+      vsync           => vga_vsync,
+      vga_hsync           => vga_hsync,
+      lcd_vsync => lcd_vsync,
+      lcd_hsync => lcd_hsync,
+      lcd_dataenable => lcd_display_enable,
+      vgared(7 downto 0)          => buffer_vgared,
+      vgagreen(7 downto 0)        => buffer_vgagreen,
+      vgablue(7 downto 0)         => buffer_vgablue,
+
+      porta_pins => porta_pins,
+      portb_pins => portb_pins,
+      keyleft => '0',
+      keyup => '0',
+
+
+      ---------------------------------------------------------------------------
+      -- IO lines to the mems and headphone microphone
+      ---------------------------------------------------------------------------
+      micData0 => micData0,
+      micData1 => micData1,
+      micClk => micClk,
+      headphone_mic => headphone_mic,
+
+      ---------------------------------------------------------------------------
+      -- IO lines to the ethernet controller (stub)
+      ---------------------------------------------------------------------------
+      eth_mdio => eth_mdio,
+      eth_rxd => "11",
+      eth_rxer => '1',
+      eth_rxdv => '0',
+      eth_interrupt => '1',
+
+      -------------------------------------------------------------------------
+      -- Lines for the SDcard interface itself
+      -------------------------------------------------------------------------
+      cs_bo => sdReset,
+      sclk_o => sdClock,
+      mosi_o => sdMOSI,
+      miso_i => sdMISO,
+      miso2_i => '1',
+
+      -- Accelerometer is connected via shared I2C bus, not a dedicated one
+      -- so just dummy plumb this for now
+      aclMISO => '1',
+      aclInt1 => '0',
+      aclInt2 => '0',
+
+      -- Audio output
+      ampPWM_l => headphone_left,
+      ampPWM_r => headphone_right,
+
+      i2s_master_clk => i2s_mclk,
+      i2s_master_sync => i2s_sync,
+      i2s_speaker_data_out => i2s_speaker,
+
+      -- No nexys4 temperature sensor
+      tmpInt => '0',
+      tmpCT => '0',
+
+      -- Touch screen
+      touchSDA => touch_SDA,
+      touchSCL => touch_scl,
+      lcdpwm =>  lcd_pwm,
+
+      i2c1sda => i2c1sda,
+      i2c1scl => i2c1scl,
+
+      -- Bluetooth source, I2S slave
+      -- XXX Connect when module has been designed
+      pcm_bluetooth_clk_in => '1',
+      pcm_bluetooth_sync_in => '1',
+      pcm_bluetooth_data_in => '1',
+      pcm_bluetooth_data_out => open,
+
+      -- This is for modem as PCM master:
+      pcm_modem_clk_in => modem1_pcm_clk_in,
+      pcm_modem_sync_in => modem1_pcm_sync_in,
+
+      pcm_modem1_data_out => modem1_pcm_data_out,
+      pcm_modem1_data_in => modem1_pcm_data_in,
+
+      ps2data =>      '1',
+      ps2clock =>     '1',
+
+      widget_matrix_col => (others => '1'),
+      widget_restore => '1',
+      widget_capslock => '1',
+      widget_joya => (others => '1'),
+      widget_joyb => (others => '1'),
+
+      -- C65 UART
+      uart_rx => c65uart_rx,
+--      uart_tx => jclo(2),
+
+      -- Buffered UARTs for cellular modems etc
+      buffereduart_rx(0) => modem1_uart_rx,
+      buffereduart_rx(1) => modem1_debug_uart_rx,
+      buffereduart_rx(2) => modem2_uart_rx,
+      buffereduart_rx(3) => modem2_debug_uart_rx,
+      buffereduart_rx(4) => wifi_uart_rx,
+      buffereduart_rx(5) => bluetooth_uart_rx,
+      buffereduart_rx(6) => lora1_uart_rx,
+      buffereduart_rx(7) => lora2_uart_rx,
+
+      buffereduart_tx(0) => modem1_uart_tx,
+      buffereduart_tx(1) => modem1_debug_uart_tx,
+      buffereduart_tx(2) => modem2_uart_tx,
+      buffereduart_tx(3) => modem2_debug_uart_tx,
+      buffereduart_tx(4) => wifi_uart_tx,
+      buffereduart_tx(5) => bluetooth_uart_tx,
+      buffereduart_tx(6) => lora1_uart_tx,
+      buffereduart_tx(7) => lora2_uart_tx,
+
+      -- XXX Plumb to input from the orange crab FPGA
+      buffereduart_ringindicate => (others => '0'),
+
+      slow_access_request_toggle => slow_access_request_toggle,
+      slow_access_ready_toggle => slow_access_ready_toggle,
+      slow_access_address => slow_access_address,
+      slow_access_write => slow_access_write,
+      slow_access_wdata => slow_access_wdata,
+      slow_access_rdata => slow_access_rdata,
+
+      cart_access_count => x"00",
+
+      fpga_temperature => fpga_temperature,
+
+      sw => (others => '0'),
+      btn => (others => '0'),
+
+      UART_TXD => monitor_tx,
+      RsRx => monitor_rx
+
+      );
+
+
+  
   lcd_display_enable <= lcd_dataenable;
   
   process (clock27,cpuclock)
