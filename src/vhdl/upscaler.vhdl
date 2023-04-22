87f49d516984f6896c8982790168c7bff6972fb1
Paul Gardner-Stephen <paul@m-e-g-a.org>
2023-04-22 15:45:47 +0930

    start implementing 720p output

--- src/vhdl/upscaler.vhdl
+++ src/vhdl/upscaler.vhdl
@@ -55,6 +55,15 @@ architecture hundertwasser of upscaler is
   signal hsync_in_prev : std_logic := '0';
 
   signal frame_start_toggle : std_logic := '0';
+
+  signal coeff0 : integer range 0 to 256 := 256;
+  signal coeff1 : integer range 0 to 256 := 0;
+  signal coeff2 : integer range 0 to 256 := 0;
+
+  signal x_count : integer := 0;
+  signal y_count : integer := 0;
+  signal pal50_int : std_logic := '1';
+  signal last_frame_start_toggle : std_logic := '0';
   
 begin
 
@@ -104,6 +113,97 @@ begin
 
     if rising_edge(clock74p22) then
       -- Generate 720p video frame, and extract data
+      -- PAL:  1280 	720 	50 Hz 	37.5 kHz 	ModeLine "1280x720" 74.25 1280 1720 1760 1980 720 725 730 750 +HSync +VSync
+      --       i.e., 1980 x 750 = 1485000 clocks per frame
+      -- NTSC: 1280 	720 	60 Hz 	45 kHz 	        ModeLine "1280x720" 74.25 1280 1390 1430 1650 720 725 730 750 +HSync +VSync
+      --       i.e., 1650 x 750 = 1237500 clocks per frame
+      -- However, as our clock is 74.2268MHz instead of 74.25MHz, we will be a
+      -- few clocks short per frame.  The difference is about 464 or 387 clocks
+      -- per frame depending on whether we are in PAL or NTSC.  That's about 1
+      -- clock tick per 2 raster lines, but not exactly.
+      -- Another approach is to just use the VSYNC synchronisation to reset the
+      -- vertical counter, but not the horizontal counter. That way every 3
+      -- frames or so we will skip/insert a raster. If the number of rasters from
+      -- the VSYNC pulse in the output to the start of video is constant, it should
+      -- look fine. We'll try that.
+      if frame_start_toggle /= last_frame_start_toggle then
+        -- Sync to input frame boundaries
+        y_count <= 724;
+      end if;
+      if pal50_int='1' then
+        if x_count < 1980 then
+          x_count <= x_count + 1;
+        else
+          x_count <= 0;
+          if y_count < 750 then
+            y_count <= y_count + 1;
+          else
+            y_count <= 0;
+          end if;
+        end if;
+        if x_count = 1720 then
+          hsync_up <= '1';
+        end if;
+        if x_count = 1760 then
+          hsync_up <= '0';
+        end if;
+      else
+        if x_count < 1650 then
+          x_count <= x_count + 1;
+        else
+          x_count <= 0;
+          if y_count < 750 then
+            y_count <= y_count + 1;
+          else
+            y_count <= 0;
+          end if;
+        end if;
+        if x_count = 1390 then
+          hsync_up <= '1';
+        end if;
+        if x_count = 1430 then
+          hsync_up <= '0';
+        end if;
+      end if;
+      if y_count = 725 then
+        vsync_up <= '1';
+      end if;
+      if y_count = 730 then
+        vsync_up <= '0';
+        pal50_int <= pal50_select;
+      end if;
+      if x_count < 1280 then
+        pixelvalid_up <= '1';
+      else
+        pixelvalid_up <= '0';
+      end if;
+      if x_count < ((1280 - 720) / 2) then
+        -- Left shoulder
+        red_up <= (others => '0');
+        green_up <= (others => '0');
+        blue_up <= (others => '0');
+        read_addr <= to_unsigned(0,10);
+      elsif x_count < (1280 - ((1280 - 720)/2)) then
+        -- Work out which X position we need to read from the raster buffers
+        read_addr <= to_unsigned(x_count - ((1280 - 720)/2),10);
+        -- Active pixel: Do mix of the rasters
+        red_up <= to_unsigned(to_integer(rdata(0)(7 downto 0)) * coeff0,16)(15 downto 8)
+                  + to_unsigned(to_integer(rdata(1)(7 downto 0)) * coeff1,16)(15 downto 8)
+                  + to_unsigned(to_integer(rdata(2)(7 downto 0)) * coeff2,16)(15 downto 8);
+        green_up <= to_unsigned(to_integer(rdata(0)(15 downto 8)) * coeff0,16)(15 downto 8)
+                  + to_unsigned(to_integer(rdata(1)(15 downto 8)) * coeff1,16)(15 downto 8)
+                  + to_unsigned(to_integer(rdata(2)(15 downto 8)) * coeff2,16)(15 downto 8);
+        blue_up <= to_unsigned(to_integer(rdata(0)(23 downto 16)) * coeff0,16)(15 downto 8)
+                  + to_unsigned(to_integer(rdata(1)(23 downto 16)) * coeff1,16)(15 downto 8)
+                  + to_unsigned(to_integer(rdata(2)(23 downto 16)) * coeff2,16)(15 downto 8);
+        blue_up <= x"ff";
+                  
+      else
+        -- Right shoulder / fly back
+        red_up <= (others => '0');
+        green_up <= (others => '0');
+        blue_up <= (others => '0');
+      end if;        
     end if;
   end process;
 
