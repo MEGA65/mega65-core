faff561f1fbf3b6d80f1e57b257154f45acdcb89
Paul Gardner-Stephen <paul@m-e-g-a.org>
2023-12-10 20:12:37 +1030

    invert IEC line controls for R5

--- src/vhdl/mega65r5.vhdl
+++ src/vhdl/mega65r5.vhdl
@@ -1403,9 +1403,9 @@ begin
       -- Finally, because we have the output value of 0 hard-wired
       -- on the output drivers, we need only gate the EN line.
       -- But we only do this if the DDR is set to output
-      iec_srq_en_n <= iec_srq_o_drive and iec_srq_en_drive;
-      iec_clk_en_n <= iec_clk_o_drive and iec_clk_en_drive;
-      iec_data_en_n <= iec_data_o_drive and iec_data_en_drive;
+      iec_srq_en_n <= not (iec_srq_o_drive and iec_srq_en_drive);
+      iec_clk_en_n <= not (iec_clk_o_drive and iec_clk_en_drive);
+      iec_data_en_n <= not (iec_data_o_drive and iec_data_en_drive);
 
       -- Reading pins is simple
       iec_srq_i_drive <= iec_srq_i;
