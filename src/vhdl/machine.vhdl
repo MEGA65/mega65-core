7ea990e191b379d255ea1431941c73d24b1e3c07
Paul Gardner-Stephen <paul@m-e-g-a.org>
2025-10-12 08:42:46 +1030

    add switch in $D63E.0 for TV intensity ramp selection

--- src/vhdl/machine.vhdl
+++ src/vhdl/machine.vhdl
@@ -184,6 +184,8 @@ entity machine is
          hdmi_sda : inout std_logic := '1';
          hpd_a : inout std_logic := '1';
 
+         dvi_tv_range : out std_logic;
+         
          porto_out : out unsigned(7 downto 0) := x"00";
          portp_out : out unsigned(7 downto 0) := x"00";
 
@@ -1640,6 +1642,8 @@ begin
       ethernet_cpu_arrest => ethernet_cpu_arrest,
       eth_hyperrupt => eth_hyperrupt,
 
+      dvi_tv_range => dvi_tv_range,
+      
       hw_errata_level => hw_errata_level,
       hw_errata_enable_toggle => hw_errata_enable_toggle,
       hw_errata_disable_toggle => hw_errata_disable_toggle,
