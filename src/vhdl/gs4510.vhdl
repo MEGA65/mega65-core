6bafc71d782a69079be68413cc1c790eb46dc1fa
Unkn0wn <teamlightning2018@outlook.com>
2026-01-14 11:10:38 -0600

    experimental fix for DMAgic line mode inconsistent startup states

--- src/vhdl/gs4510.vhdl
+++ src/vhdl/gs4510.vhdl
@@ -603,7 +603,7 @@ architecture Behavioural of gs4510 is
   signal reg_dmagic_line_mode : std_logic := '0';
   signal reg_dmagic_line_x_or_y : std_logic := '0';
   signal reg_dmagic_line_slope_negative : std_logic := '0';
-  signal reg_dmagic_line_mode_skip_pixels : std_logic_vector(1 downto 0) := "00";
+  signal reg_dmagic_line_mode_skip_pixels : integer range 0 to 3 := 0;
 
   signal reg_dmagic_s_x8_offset : unsigned(15 downto 0) := x"0000";
   signal reg_dmagic_s_y8_offset : unsigned(15 downto 0) := x"0000";
@@ -613,7 +613,7 @@ architecture Behavioural of gs4510 is
   signal reg_dmagic_s_line_mode : std_logic := '0';
   signal reg_dmagic_s_line_x_or_y : std_logic := '0';
   signal reg_dmagic_s_line_slope_negative : std_logic := '0';
-  signal reg_dmagic_s_line_mode_skip_pixels : std_logic_vector(1 downto 0) := "00";
+  signal reg_dmagic_s_line_mode_skip_pixels : integer range 0 to 3 := 0;
 
   signal dmagic_option_id : unsigned(7 downto 0) := x"00";
   signal reg_dmagic_draw_spiral : std_logic := '0';
@@ -3859,7 +3859,7 @@ begin
       reg_dmagic_line_slope_negative <= '0';
       dmagic_slope_overflow_toggle <= '0';
       reg_dmagic_line_mode <= '0';
-      reg_dmagic_line_mode_skip_pixels <= (others => '0');
+      reg_dmagic_line_mode_skip_pixels <= 0;
       reg_dmagic_line_x_or_y <= '0';
 
       reg_dmagic_s_x8_offset <= x"0000";
@@ -3869,7 +3869,7 @@ begin
       reg_dmagic_s_line_slope_negative <= '0';
       dmagic_s_slope_overflow_toggle <= '0';
       reg_dmagic_s_line_mode <= '0';
-      reg_dmagic_s_line_mode_skip_pixels <= (others => '0');
+      reg_dmagic_s_line_mode_skip_pixels <= 0;
       reg_dmagic_s_line_x_or_y <= '0';
 
       reg_dmagic_floppy_mode <= '0';
@@ -5834,7 +5834,6 @@ begin
                   when x"8d" => reg_dmagic_slope_fraction_start(7 downto 0) <= memory_read_value;
                   when x"8e" => reg_dmagic_slope_fraction_start(15 downto 8) <= memory_read_value;
                   when x"8f" => reg_dmagic_line_mode <= memory_read_value(7);
-                                reg_dmagic_line_mode_skip_pixels <= memory_read_value(7) & '0';
                                 reg_dmagic_line_x_or_y <= memory_read_value(6);
                                 reg_dmagic_line_slope_negative <= memory_read_value(5);
                   -- @ IO:GS $D705 - Enhanced DMAgic job option $90 $xx = Set bits 16 -- 23 of DMA length to allow DMA operations >64KB.
@@ -5856,7 +5855,6 @@ begin
                   when x"9d" => reg_dmagic_s_slope_fraction_start(7 downto 0) <= memory_read_value;
                   when x"9e" => reg_dmagic_s_slope_fraction_start(15 downto 8) <= memory_read_value;
                   when x"9f" => reg_dmagic_s_line_mode <= memory_read_value(7);
-                                reg_dmagic_s_line_mode_skip_pixels <= memory_read_value(7) & '0';
                                 reg_dmagic_s_line_x_or_y <= memory_read_value(6);
                                 reg_dmagic_s_line_slope_negative <= memory_read_value(5);
 
@@ -5991,6 +5989,22 @@ begin
               pre_dma_cpuport_bits <= cpuport_value(2 downto 0);
               cpuport_value(2 downto 1) <= "10";
 
+              -- setup delay states for destination line mode
+              -- slope accumulator takes a cycle to set up, and the first read cycle is bad anyways
+              if reg_dmagic_line_mode = '1' then
+                reg_dmagic_line_mode_skip_pixels <= 1;
+              else
+                reg_dmagic_line_mode_skip_pixels <= 0;
+              end if;
+
+              -- setup delay states for source line mode
+              -- one cycle for slope accumulator
+              if reg_dmagic_s_line_mode = '1' then
+                reg_dmagic_s_line_mode_skip_pixels <= 1;
+              else
+                reg_dmagic_s_line_mode_skip_pixels <= 0;
+              end if;
+
               case dmagic_cmd(1 downto 0) is
                 when "11" => -- fill
                   state <= DMAgicFill;
@@ -6003,7 +6017,7 @@ begin
                   -- Since line mode skips the first cycle to allow for slope accumulator
                   -- latency, the counter is decremented one too many times.
                   dmagic_first_read <= reg_dmagic_line_mode;
-                  
+
                 when "00" => -- copy
                   dmagic_first_read <= '1';
                   state <= DMagicCopyRead;
@@ -6041,8 +6055,12 @@ begin
               -- Fill memory at dmagic_dest_addr with dmagic_src_addr(7 downto 0)
 
               -- Clear first pixel of line flag
-              reg_dmagic_line_mode_skip_pixels <= reg_dmagic_line_mode_skip_pixels(0) & "0";
-              reg_dmagic_s_line_mode_skip_pixels <= reg_dmagic_s_line_mode_skip_pixels(0) & "0";
+              if reg_dmagic_line_mode_skip_pixels /= 0 then
+                reg_dmagic_line_mode_skip_pixels <= reg_dmagic_line_mode_skip_pixels - 1;
+              end if;
+              if reg_dmagic_s_line_mode_skip_pixels /= 0 then
+                reg_dmagic_s_line_mode_skip_pixels <= reg_dmagic_s_line_mode_skip_pixels - 1;
+              end if;
 
               -- Do memory write
               phi_add_backlog <= '1'; phi_new_backlog <= 1;
@@ -6092,7 +6110,7 @@ begin
               else
                 -- We are in line mode.
 
-                if reg_dmagic_line_mode_skip_pixels="00" then
+                if reg_dmagic_line_mode_skip_pixels = 0 then
                   -- Accumulate scale value, but only if the skip rate was manually specified.
                   -- A skip rate of $0000 is treated as skipping 1 byte per cycle (which
                   -- equates to not scaling down) since not moving the address forwards at all
@@ -6104,11 +6122,11 @@ begin
                     line_skip_accumulator := "01" & x"0000";
                   end if;
                 end if;
-                
+
                 -- Add fractional position, but only when DMAgic has waited for enough cycles, according
                 -- to the skip rate. The skip accumulators start at a value of $10000, so this will
                 -- always run in the first few cycles.
-                if line_skip_accumulator(17 downto 16) /= "00" and reg_dmagic_line_mode_skip_pixels /= "11" then
+                if line_skip_accumulator(17 downto 16) /= "00" and reg_dmagic_line_mode_skip_pixels <= 1 then
                   reg_dmagic_slope_fraction_start <= reg_dmagic_slope_fraction_start + reg_dmagic_slope;
                 end if;
 
@@ -6133,7 +6151,7 @@ begin
                 -- so handle this by not advancing the major axis on the first
                 -- pixel.  The first pixel will thus effectively be written to
                 -- twice.
-                if reg_dmagic_line_mode_skip_pixels="00" then
+                if reg_dmagic_line_mode_skip_pixels = 0 then
                   -- Only allow major axis movement when the skip accumulator
                   -- is greater than $10000.
                   if line_skip_accumulator(17 downto 16) /= "00" then
@@ -6290,11 +6308,11 @@ begin
                   state <= DMAgicTrigger;
                 end if;
               else
-                -- Skip the first decrement in line mode.
-                if dmagic_first_read = '0' then
+                dmagic_first_read <= '0';
+
+                -- Skip the first decrements in line mode.
+                if reg_dmagic_line_mode_skip_pixels = 0 then
                   dmagic_count <= dmagic_count - 1;
-                else
-                  dmagic_first_read <= '0';
                 end if;
 
                 if pending_dma_busy='1' then
@@ -6331,7 +6349,9 @@ begin
                                         -- and can read or write on every cycle.
                                         -- so we need to read the first byte now.
 
-              reg_dmagic_s_line_mode_skip_pixels <= reg_dmagic_s_line_mode_skip_pixels(0) & "0";
+              if reg_dmagic_s_line_mode_skip_pixels /= 0 then
+                reg_dmagic_s_line_mode_skip_pixels <= reg_dmagic_s_line_mode_skip_pixels - 1;
+              end if;
 
                                         -- Do memory read
               phi_add_backlog <= '1'; phi_new_backlog <= 1;
@@ -6345,7 +6365,7 @@ begin
               if reg_dmagic_s_line_mode='1' then
                 -- We are in line mode.
 
-                if reg_dmagic_s_line_mode_skip_pixels="00" then
+                if reg_dmagic_s_line_mode_skip_pixels = 0 then
                   if line_source_skip_rate_set = '1' and reg_dmagic_src_skip /= x"0000" then
                     line_source_skip_accumulator := line_source_skip_accumulator + ("00" & reg_dmagic_src_skip);
                   else
@@ -6353,7 +6373,7 @@ begin
                   end if;
                 end if;
 
-                if line_source_skip_accumulator(17 downto 16) /= "00" and reg_dmagic_s_line_mode_skip_pixels /= "11" then
+                if line_source_skip_accumulator(17 downto 16) /= "00" and reg_dmagic_s_line_mode_skip_pixels <= 1 then
                   -- Add fractional position
                   reg_dmagic_s_slope_fraction_start <= reg_dmagic_s_slope_fraction_start + reg_dmagic_s_slope;
                 end if;
@@ -6375,7 +6395,7 @@ begin
                   end if;
                 end if;
                 -- Also move major axis (which is always in the forward direction)
-                if reg_dmagic_s_line_mode_skip_pixels="00" then
+                if reg_dmagic_s_line_mode_skip_pixels = 0 then
                   if line_source_skip_accumulator(17 downto 16) /= "00" then
                     if reg_dmagic_s_line_x_or_y='0' then
                       line_x_move := '1';
@@ -6499,7 +6519,8 @@ begin
                     dmagic_src_addr <= dmagic_src_addr - (256*1) - (256*8);
                   end if;
                 end if;
-              elsif dmagic_src_hold='0' then
+              elsif dmagic_src_hold='0' and reg_dmagic_s_line_mode_skip_pixels = 0 then
+                -- only increment source address if there are no cycles to skip. reg_dmagic_s_line_mode_skip_pixels is zero when not in line mode.
                 if dmagic_src_direction='0' then
                   dmagic_src_addr(35 downto 0) <= dmagic_src_addr(35 downto 0) + reg_dmagic_src_skip;
                 else
@@ -6540,27 +6561,31 @@ begin
 
               phi_add_backlog <= '1'; phi_new_backlog <= 1;
 
-              if dmagic_first_read = '0' then
+              -- check if line mode will independently wait a cycle to avoid logic that makes my head hurt
+              if dmagic_first_read = '0' or reg_dmagic_line_mode_skip_pixels /= 0 then
                                         -- Update address and check for end of job.
                                         -- XXX Ignores modulus, whose behaviour is insufficiently defined
                                         -- in the C65 specifications document
 
-                reg_dmagic_line_mode_skip_pixels <= reg_dmagic_line_mode_skip_pixels(0) & "0";
-                
+                if reg_dmagic_line_mode_skip_pixels /= 0 then
+                  reg_dmagic_line_mode_skip_pixels <= reg_dmagic_line_mode_skip_pixels - 1;
+                end if;
+
                 if reg_dmagic_line_mode='1' then
                   -- Line mode with copy, so that textured lines can be drawn
                   -- We are in line mode.
 
-                  if reg_dmagic_line_mode_skip_pixels="00" then
+                  if reg_dmagic_line_mode_skip_pixels = 0 then
                     if line_dest_skip_rate_set = '1' and reg_dmagic_dst_skip /= x"0000" then
                       line_skip_accumulator := line_skip_accumulator + ("00" & reg_dmagic_dst_skip);
                     else
                       line_skip_accumulator := "01" & x"0000";
                     end if;
                   end if;
-                  
+
                   -- Add fractional position
-                  if line_skip_accumulator(17 downto 16) /= "00" and reg_dmagic_line_mode_skip_pixels /= "11" then
+                  -- Wait until the last skipped cycle to start updating the slope accumulator
+                  if line_skip_accumulator(17 downto 16) /= "00" and reg_dmagic_line_mode_skip_pixels <= 1 then
                     reg_dmagic_slope_fraction_start <= reg_dmagic_slope_fraction_start + reg_dmagic_slope;
                   end if;
 
@@ -6581,7 +6606,7 @@ begin
                     end if;
                   end if;
                   -- Also move major axis (which is always in the forward direction)
-                  if reg_dmagic_line_mode_skip_pixels="00" then
+                  if reg_dmagic_line_mode_skip_pixels = 0 then
                     if line_skip_accumulator(17 downto 16) /= "00" then
                       if reg_dmagic_line_x_or_y='0' then
                         line_x_move := '1';
@@ -6706,7 +6731,8 @@ begin
                     end if;
                   end if;
                   -- end of line mode case
-                elsif dmagic_dest_hold='0' then
+                elsif dmagic_dest_hold='0' and reg_dmagic_line_mode_skip_pixels = 0 then
+                  -- reg_dmagic_line_mode_skip_pixels is zero when line mode is disabled
                   if dmagic_dest_direction='0' then
                     dmagic_dest_addr(35 downto 0) <= dmagic_dest_addr(35 downto 0) + reg_dmagic_dst_skip;
                   else
@@ -6742,7 +6768,7 @@ begin
                     state <= DMAgicTrigger;
                   end if;
                 else
-                  if reg_dmagic_line_mode = '0' or reg_dmagic_line_mode_skip_pixels = "00" then
+                  if reg_dmagic_line_mode = '0' or reg_dmagic_line_mode_skip_pixels = 0 then
                     dmagic_count <= dmagic_count - 1;
                   end if;
                 end if;
